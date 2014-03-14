5b00653b222ecd68ec65cd35a14d155f092ffdef
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-03-14 14:01:29 +0200

    Remove redundant double space
    Change-Id: I75f494f84d05f128ce011985fefa3f1253bafee4

--- modules/translation/ext.cx.translation.js
+++ modules/translation/ext.cx.translation.js
@@ -75,7 +75,7 @@
 	};
 
 	ContentTranslationEditor.prototype.listen = function () {
-		mw.hook(  'mw.cx.translation.add' ).add( $.proxy( this.update, this ) );
+		mw.hook( 'mw.cx.translation.add' ).add( $.proxy( this.update, this ) );
 
 		this.$content.on( 'input', function () {
 			mw.hook( 'mw.cx.translation.change' ).fire();
