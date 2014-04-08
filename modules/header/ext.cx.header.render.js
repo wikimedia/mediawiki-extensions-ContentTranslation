d399384dc33ed08523935f513222126ef081c41a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2014-04-08 15:56:16 +0530

    Simplify the UI construction code, remove progress bar
    Progress bar will re-appear in tools column soon
    
    Change-Id: I6f7bdc9cefda505c7da41d201cc7d57b7e57c164

--- modules/header/ext.cx.header.render.js
+++ modules/header/ext.cx.header.render.js
@@ -12,39 +12,32 @@
 	'use strict';
 
 	mw.cx.ContentTranslationHeader.prototype.render = function () {
-		this.$container.addClass( 'cx-header' );
-		this.$container.append(
-			$( '<div>' )
-				.addClass( 'cx-header__logo' ),
-			$( '<div>' )
-				.addClass( 'cx-header__user-details' )
-				.append(
-					$( '<a>' )
-						.addClass( 'cx-header__user-details__user-name' )
-						.attr( 'href', mw.util.getUrl( 'User:' + mw.user.getName() ) )
-						.text( mw.user.getName() )
-				)
-		);
+		var $logo, $userName, $userDetails, $headerBar, $publishButton,
+			$translationCenterLink, $translationCenter;
 
-		this.$container.append(
-			$( '<div>' )
-				.addClass( 'cx-header__bar' )
-				.append(
-					$( '<div>' )
-						.addClass( 'cx-header__translation-center' )
-						.append(
-							$( '<a>' )
-								.text( mw.msg( 'cx-header-translation-center' ) )
-								.attr( 'href', '#' )
-						),
-					$( '<div>' )
-						.addClass( 'cx-header__progressbar' )
-						.cxProgressBar(),
-					$( '<button>' )
-						.addClass( 'cx-header__publish publish mw-ui-button mw-ui-constructive' )
-						.text( mw.msg( 'cx-publish-button' ) )
-				)
-		);
+		$logo = $( '<div>' ).addClass( 'cx-header__logo' );
+		$userName = $( '<a>' )
+			.addClass( 'cx-header__user-details__user-name' )
+			.attr( 'href', mw.util.getUrl( 'User:' + mw.user.getName() ) )
+			.text( mw.user.getName() );
+		$userDetails = $( '<div>' )
+			.addClass( 'cx-header__user-details' )
+			.append( $userName );
+		$publishButton = $( '<button>' )
+			.addClass( 'cx-header__publish publish mw-ui-button mw-ui-constructive' )
+			.text( mw.msg( 'cx-publish-button' ) );
+		$translationCenterLink = $( '<a>' )
+			.text( mw.msg( 'cx-header-translation-center' ) )
+			.attr( 'href', '#' );
+		$translationCenter = $( '<div>' )
+			.addClass( 'cx-header__translation-center' )
+			.append( $translationCenterLink );
+		$headerBar = $( '<div>' )
+			.addClass( 'cx-header__bar' )
+			.append( $translationCenter, $publishButton );
+		this.$container
+			.addClass( 'cx-header' )
+			.append( $logo, $userDetails, $headerBar );
 	};
 
 }( jQuery, mediaWiki ) );
