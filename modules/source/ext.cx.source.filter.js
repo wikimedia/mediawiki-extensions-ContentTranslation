c6cd59f72257e5c52cc321554c8cee510c623120
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-07-03 11:40:06 +0000

    Adapt Cita noticia (es) to Ref-notícia" (ca)
    Change-Id: I045fd1b683eb8a53ca6305b15cd211aae062d5e5

--- modules/source/ext.cx.source.filter.js
+++ modules/source/ext.cx.source.filter.js
@@ -30,47 +30,102 @@
 		this.listen();
 	}
 
+	/**
+	 * Filter the templates present in source article based on the configuration
+	 * @param {Object} configuration
+	 */
 	CXSourceFilter.prototype.filter = function ( configuration ) {
+		var self = this;
+
 		$( '[typeof="mw:Transclusion"]' ).each( function () {
-			var mwData, $template = $( this ),
-				templateName, templateFragments, templateConf;
+			var title, mwData, templateName, templateConf,
+				$template = $( this );
 
 			mwData = $template.data( 'mw' );
 			if ( !mwData ) {
+				mw.log( '[CX] Skipping template!' );
 				return;
 			}
+
 			templateName = mwData.parts[ 0 ].template.target.wt;
 			templateName = templateName.trim();
-			if ( configuration ) {
-				templateConf = configuration.templates && configuration.templates[ templateName ];
 
-				// See www.mediawiki.org/wiki/Parsoid/MediaWiki_DOM_spec#Transclusion_content
-				// Template information is stored in parsoid-data attribute with an element with
-				// mw:Transcusion type, while the template rendering can be a different html element.
-				// They are connected using 'about' attribute.
-				templateFragments = $template.attr( 'about' );
-				if ( !templateConf ) {
-					// No configuration for the given template name found. So delete.
-					mw.log( '[CX] Removing template:' + templateName );
-					$template.remove();
-					// Remove the associated template renderings too.
-					if ( templateFragments ) {
-						$( '[about="' + templateFragments + '"]' ).remove();
-					}
-					return;
-				}
-				mwData.parts[ 0 ].template.target.wt = templateConf.targetname;
-				$template.attr( 'data-mw', JSON.stringify( mwData ) );
-				if ( !templateConf.editable ) {
-					$template.attr( 'data-editable', false );
-				}
-				mw.log( '[CX] Keeping template:' + templateName );
+			// Normalize the name
+			title = mw.Title.newFromText( templateName );
+			if ( title ) {
+				templateName = title.getPrefixedText();
+			}
+
+			templateConf = configuration &&
+				configuration.templates &&
+				configuration.templates[ templateName ];
+
+			if ( templateConf ) {
+				mw.log( '[CX] Keeping template: ' + templateName );
+				self.adaptTemplate( $template, templateConf );
 			} else {
-				mw.log( '[CX] Removing template:' + templateName );
-				$template.remove();
+				mw.log( '[CX] Removing template: ' + templateName );
+				self.removeTemplate( $template );
 			}
 		} );
 		mw.hook( 'mw.cx.source.ready' ).fire();
+	};
+
+	/**
+	 * Remove a template
+	 * @param {jQuery} $template
+	 */
+	CXSourceFilter.prototype.removeTemplate = function ( $template ) {
+		var templateFragments;
+		// See https://www.mediawiki.org/wiki/Parsoid/MediaWiki_DOM_spec#Transclusion_content
+		// Template information is stored in data-parsoid attribute with an element with
+		// mw:Transcusion type, while the template rendering can be a different html element.
+		// They are connected using 'about' attribute.
+		templateFragments = $template.attr( 'about' );
+		// Remove the associated template renderings too.
+		if ( templateFragments ) {
+			$( '[about="' + templateFragments + '"]' ).remove();
+		}
+
+		$template.remove();
+	};
+
+	/**
+	 * Adapt a template
+	 * @param {jQuery} $template
+	 * @param {Object} mappings parameter mappings
+	 */
+	CXSourceFilter.prototype.adaptTemplate = function ( $template, mappings ) {
+		var targetParams = {},
+			data = $template.data( 'mw' ),
+			sourceParams = data.parts[ 0 ].template.params;
+
+		// Update the name of the template
+		data.parts[ 0 ].template.target.wt = mappings.targetname;
+
+		// Update the tempate parameters
+		$.each( sourceParams, function ( key, value ) {
+			// Drop empty parameters
+			if ( $.trim( value ) === '' ) {
+				return;
+			}
+			//delete data.parts[ 0 ].template.params[ key ];
+			// Copy over other parameters, but map known keys
+			if ( mappings.parameters[ key ] !== undefined ) {
+				key = mappings.parameters[ key ];
+			}
+
+			targetParams[ key ] = value;
+
+		} );
+
+		data.parts[ 0 ].template.params = targetParams;
+		$template.attr( 'data-mw', JSON.stringify( data ) );
+
+		// Make templates uneditable unless whitelisted
+		if ( !mappings.editable ) {
+			$template.data( 'editable', false );
+		}
 	};
 
 	CXSourceFilter.prototype.listen = function () {
