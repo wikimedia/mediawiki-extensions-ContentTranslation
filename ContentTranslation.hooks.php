wikimedia/mediawiki-extensions-ContentTranslation

commit f4a227becefd706a51d9039a14c760bb5976d8d0

Make all of Content Translation a beta feature
The existing beta feature for entry point has been absorbed into
a new beta feature that also covers the visibility of the special
pages. Using a combination of SpecialPage::isListed and check in
Special::execute to have the special page act as if they do not
exist for users for whom the feature is not enabled.

Added a global for the beta feature use, so that developers can
turn it off for example. As before, if BetaFeatures extensions
does not exist, acts as if the global was false.

Removed old beta feature images and added new ones.

Bug: T76175
Change-Id: Iff4ff583fe434ef7f15bc7af53e093ce1490a95c

ContentTranslation.hooks.php
@@ -10,8 +10,27 @@
 
 class ContentTranslationHooks {
 	/**
-	 * @param OutputPage $out
-	 * @param Skin $skin
+	 * Utility function that checks whether CX is enabled for a given user.
+	 * Currently it checks that if CX is a beta feature, whether the user has
+	 * enabled it. Otherwise it is always enabled.
+	 *
+	 * @param User $user
+	 * @return Boolean
+	 */
+	public static function isEnabledForUser( User $user ) {
+		global $wgContentTranslationAsBetaFeature;
+
+		if ( !$wgContentTranslationAsBetaFeature ) {
+			return true;
+		}
+
+		return
+			class_exists( 'BetaFeatures' ) &&
+			BetaFeatures::isFeatureEnabled( $user, 'cx' );
+	}
+
+
+	/**
 	 * Hook: BeforePageDisplay
 	 */
 	public static function addModules( OutputPage $out, Skin $skin ) {
@@ -20,22 +39,6 @@ class ContentTranslationHooks {
 		$title = $out->getTitle();
 		$user = $out->getUser();
 
-		$redlinkFeatureEnabled = class_exists( 'BetaFeatures' ) &&
-			BetaFeatures::isFeatureEnabled( $user, 'red-interlanguage-link' );
-
-		if ( $user->isLoggedIn() ) {
-			if ( $redlinkFeatureEnabled &&
-				$title->inNamespace( NS_MAIN ) &&
-				$out->getLanguage()->getCode() !== $title->getPageLanguage()->getCode()
-			) {
-				$out->addModules( 'ext.cx.redlink' );
-			}
-
-			if ( class_exists( 'GuidedTourHooks' ) ) {
-				$out->addModules( 'ext.guidedTour' );
-			}
-		}
-
 		// If EventLogging integration is enabled, load the schema module
 		// and the event logging functions module
 		if ( $wgContentTranslationEventLogging ) {
@@ -43,6 +46,23 @@ class ContentTranslationHooks {
 				'schema.ContentTranslation',
 				'ext.cx.eventlogging',
 			) );
+		}
+
+		// CX is currently restricted to only logged in users
+		if ( $user->isLoggedIn() ) {
+			return;
+		}
+
+		if (
+			self::isEnabledForUser( $user ) &&
+			$title->inNamespace( NS_MAIN ) &&
+			$out->getLanguage()->getCode() !== $title->getPageLanguage()->getCode()
+		) {
+			$out->addModules( 'ext.cx.redlink' );
+		}
+
+		if ( class_exists( 'GuidedTourHooks' ) ) {
+			$out->addModules( 'ext.guidedTour' );
 		}
 	}
 
@@ -54,14 +74,14 @@ class ContentTranslationHooks {
 	public static function getPreferences( User $user, array &$prefs ) {
 		global $wgExtensionAssetsPath;
 
-		$imageDir = "$wgExtensionAssetsPath/ContentTranslation/modules/entrypoint/images";
+		$imageDir = "$wgExtensionAssetsPath/ContentTranslation/images";
 
-		$prefs['red-interlanguage-link'] = array(
-			'label-message' => 'cx-red-interlanguage-link-preference',
-			'desc-message' => 'cx-red-interlanguage-link-preference-beta-desc',
+		$prefs['cx'] = array(
+			'label-message' => 'cx-beta',
+			'desc-message' => 'cx-beta-desc',
 			'screenshot' => array(
-				'ltr' => "$imageDir/translate-redlink-ltr.svg",
-				'rtl' => "$imageDir/translate-redlink-rtl.svg",
+				'ltr' => "$imageDir/cx-icon-ltr.svg",
+				'rtl' => "$imageDir/cx-icon-rtl.svg",
 			),
 			'info-link' => 'https://www.mediawiki.org/wiki/Extension:ContentTranslation',
 			'discussion-link' => 'https://www.mediawiki.org/wiki/Extension_talk:ContentTranslation',

ContentTranslation.php
@@ -135,3 +135,9 @@ $GLOBALS['wgContentTranslationSiteTemplates'] = array(
  * The default is to open Special:ContentTranslation on the same wiki.
  */
 $GLOBALS['wgContentTranslationTranslateInTarget'] = false;
+
+/**
+ * Whether to have CX available only as a beta feature and only shown to the
+ * users who have enabled the beta feature.
+ */
+$GLOBALS['wgContentTranslationAsBetaFeature'] = true;

images/cx-icon-ltr.svg
@@ -1,0 +1,1 @@
+<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 768" enable-background="new 0 0 1024 768"><path fill="#E5E5E5" d="M715.6 89.2h85.3v23.3h-85.3zM901.8 89.2h100.8v23.3H901.8zM684.6 89.2h23.3v23.3h-23.3zM808.7 89.2H894v23.3h-85.3z"/><path fill="none" stroke="#E5E5E5" stroke-width="4" stroke-miterlimit="10" d="M0 59.8v589.6l52 38.8 44.6-38.8 53.9 38.8 39.6-32.6 8.1-6.2 48.9 38.8 53.5-38.8 55.1 38.8 50.4-38.8 50.4 38.8 53.5-38.8 46.9 38.8 52-38.8 55.1 38.8 50.4-38.8 50.4 38.8 22.5-16.7 31.4-22.1 19 16.3 29.5 22.5 53.5-38.8 53.9 38.8 10.5-8.1v.4l38.8-28.7v-592H0z"/><path fill="#E5E5E5" d="M554.7 89.2H190.1v23.3h364.6V89.2zm-3.9 19.4H193.9V93.1h356.8v15.5zM626.4 89.2h50.4v23.3h-50.4zM34.9 89.2H159v23.3H34.9zM595.4 89.2h23.3v23.3h-23.3z"/><path fill="#E5E5E5" stroke="#E5E5E5" d="M7.8 126.1h1008.5"/><g fill="#D6D6D6"><path d="M38.5 161.1h335.8v47.6H38.5zM200.9 238.5v132.8h173.5V238.5H200.9zm154.3 105.3H218.9l40.7-52.6 6-2.4 38.3 40.7 20.3-12 31 26.3zM38.5 398.5h333.8v27.6H38.5zM38.5 445.7h333.8v27.6H38.5zM38.5 296.7h135.2v27.6H38.5zM38.5 343.9h135.2v27.6H38.5zM38.5 523.5h333.8v27.6H38.5zM38.5 570.8h333.8v27.6H38.5z"/></g><g fill="#D6D6D6"><path d="M420.2 161.1H756v47.6H420.2zM582.6 238.5v132.8H756V238.5H582.6zm154.3 105.3H600.5l40.7-52.6 6-2.4 38.3 40.7 20.3-12 31.1 26.3zM420.2 398.5H754v27.6H420.2zM420.2 445.7H754v27.6H420.2zM420.2 296.7h135.2v27.6H420.2zM420.2 343.9h135.2v27.6H420.2zM420.2 523.5H754v27.6H420.2zM420.2 570.8H754v27.6H420.2z"/></g><path fill="#D6D6D6" d="M808.7 657.9l10.1-7.1 19 16.3 29.5 22.5 53.5-38.8 53.9 38.8 10.5-8.1v.4l17.5-12.9V162.5h-194z"/><path fill="#347BFF" d="M549.2 551.8H117.8l45.4-148-45.4-147.6h431.4l125 147.8z"/><path fill="#fff" d="M386.6 420.8c-17.3-17-32.4-31.7-41.1-66h60.3v-25.6h-60.3v-33.5h-26v33.9h-60.3v25.6h60.3v8.7c-8.7 33.5-18.8 55-60.3 76.1l8.7 25.6c39.2-21.1 59.9-47.1 69-76.5 8.7 22.2 23.4 40 40 56.9l9.7-25.2zm79.9-83h-34.7l-60.3 170h26l17.3-50.9h68.6l17.3 50.9h26l-60.2-170zm-43.3 93.5l26-67.8 26 67.8h-52z"/></svg>

images/cx-icon-rtl.svg
@@ -1,0 +1,1 @@
+<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 768" enable-background="new 0 0 1024 768"><path fill="#E5E5E5" d="M223 89.2h85.3v23.3H223zM21.3 89.2h100.8v23.3H21.3zM316 89.2h23.3v23.3H316zM129.9 89.2h85.3v23.3h-85.3z"/><path fill="none" stroke="#E5E5E5" stroke-width="4" stroke-miterlimit="10" d="M0 59.8v592l38.8 28.7v-.4l10.5 8.1 53.9-38.8 53.5 38.8 29.5-22.5 19-16.3 31.4 22.1 22.5 16.7 50.4-38.8 50.4 38.8 55.1-38.8 52 38.8 46.9-38.8 53.5 38.8 50.4-38.8 50.4 38.8 55.1-38.8 53.5 38.8 48.9-38.8 8.1 6.2 39.6 32.6 53.9-38.8 44.6 38.8 52-38.8V59.8H0z"/><path fill="#E5E5E5" d="M469.2 112.5h364.6V89.2H469.2v23.3zm4-3.9V93.1H830v15.5H473.1h.1zM347.1 89.2h50.4v23.3h-50.4zM864.9 89.2H989v23.3H864.9zM405.2 89.2h23.3v23.3h-23.3z"/><path fill="#E5E5E5" stroke="#E5E5E5" d="M7.6 126.1h1008.5"/><g fill="#D6D6D6"><path d="M649.6 161.1h335.8v47.6H649.6zM649.5 238.5v132.8H823V238.5H649.5zm154.3 105.3H667.5l40.7-52.6 6-2.4 38.3 40.7 20.3-12 31 26.3zM651.6 398.5h333.8v27.6H651.6zM651.6 445.7h333.8v27.6H651.6zM850.2 296.7h135.2v27.6H850.2zM850.2 343.9h135.2v27.6H850.2zM651.6 523.5h333.8v27.6H651.6zM651.6 570.8h333.8v27.6H651.6z"/></g><g fill="#D6D6D6"><path d="M267.9 161.1h335.8v47.6H267.9zM267.9 238.5v132.8h173.4V238.5H267.9zm154.3 105.3H285.8l40.7-52.6 6-2.4 38.3 40.7 20.3-12 31.1 26.3zM269.9 398.5h333.8v27.6H269.9zM269.9 445.7h333.8v27.6H269.9zM468.5 296.7h135.2v27.6H468.5zM468.5 343.9h135.2v27.6H468.5zM269.9 523.5h333.8v27.6H269.9zM269.9 570.8h333.8v27.6H269.9z"/></g><path fill="#D6D6D6" d="M215.2 162.5h-194V669l17.5 12.9v-.4l10.5 8.1 53.9-38.8 53.5 38.8 29.5-22.5 19-16.3 10.1 7.1z"/><path fill="#347BFF" d="M349.7 404l125-147.8h431.4l-45.4 147.6 45.4 148H474.7z"/><path fill="#fff" d="M624.6 420.8c-17.3-17-32.4-31.7-41.1-66h60.3v-25.6h-60.3v-33.5h-26v33.9h-60.3v25.6h60.3v8.7c-8.7 33.5-18.8 55-60.3 76.1l8.7 25.6c39.2-21.1 59.9-47.1 69-76.5 8.7 22.2 23.4 40 40 56.9l9.7-25.2zm79.9-83h-34.7l-60.3 170h26l17.3-50.9h68.6l17.3 50.9h26l-60.2-170zm-43.3 93.5l26-67.8 26 67.8h-52z"/></svg>

modules/entrypoint/images/translate-redlink-ltr.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 264 162" enable-background="new 0 0 264 162"><path fill="#fff" d="M13.4 161.4l-12.9-9.6v-151.3h263v151.7l-9 6.7v-116.4h-39v112.6l-4.4-3.7-7.6 5.3v-114.2h-155v110.8l-9.8 8-13.9-10-11.4 10.1z"/><path fill="#E5E5E5" d="M263 1v151l-8 6v-116h-40v112l-3.3-2.8-.6-.5-.6.4-6.5 4.6v-113.7h-156v111.1l-9.3 7.7-13.3-9.6-.6-.5-.6.5-10.9 9.5-12.3-9.2v-150.5h262m1-1h-264v152l13.4 10 11.5-10 13.9 10 10.2-8.4v-110.6h154v114.7l8.1-5.7 4.9 4.2v-113.2h38v117l10-7.4v-152.6zM203 157.7v-114.7h-154v110.6l2.1-1.6 12.6 10 13.8-10 14.2 10 13-10 13 10 13.8-10 12.1 10 13.4-10 14.2 10 13-10 13 10 5.8-4.3zM11 36c0-7.7 6.3-14 14-14s14 6.3 14 14-6.3 14-14 14-14-6.3-14-14zM254 159.9v-116.9h-38v113.2l7.6 5.8 13.8-10 13.9 10 2.7-2.1zM38 107v-35h-25v35h25zM232.5 5h26v6h-26v-6zM208.5 5h22v6h-22v-6zM142 6v4h-92v-4h92m1-1h-94v6h94v-6zM184.5 5h22v6h-22v-6zM161.5 5h13v6h-13v-6zM176.5 5h6v6h-6v-6zM153.5 5h6v6h-6v-6zM9 5h32v6h-32v-6z"/><path fill="#E5E5E5" stroke="#E5E5E5" d="M2 14.5h260"/><path fill="#E5E5E5" d="M52 7h2v2h-2v-2zM38 59v-5h-25v5h25zM38 132v-35h-25v35h25z"/><path fill="#fff" d="M15.4 108.5h20.1c.1 0 .2 0 .2.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2l.2-.1zM15.4 113.7h20.1c.1 0 .2 0 .2.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2l.2-.1z"/><path fill="#D11D13" d="M15.4 103.3h20.1c.1 0 .2 0 .2.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2l.2-.1z"/><path fill="#fff" d="M15.4 118.9h20.1c.1 0 .2 0 .2.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2l.2-.1zM15.4 124.2h20.1c.1 0 .2 0 .2.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c.1-.1.1-.1.2-.1z"/><path fill="#347BFF" d="M54.5 57.2v34.9l-13.5 12.2 13.5 12.2v23.5h139v-82.8z"/><g fill="#fff"><path d="M122.3 103.7c-4.6-4.5-8.6-8.4-10.9-17.5h16v-6.8h-16v-8.9h-6.9v9h-16v6.8h16v2.3c-2.3 8.9-5 14.6-16 20.2l2.3 6.8c10.4-5.6 15.9-12.5 18.3-20.3 2.3 5.9 6.2 10.6 10.6 15.1l2.6-6.7zM143.5 81.7h-9.2l-16 45.1h6.9l4.6-13.5h18.2l4.6 13.5h6.9l-16-45.1zm-11.5 24.8l6.9-18 6.9 18h-13.8z"/></g></svg>

modules/entrypoint/images/translate-redlink-rtl.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 264 162" enable-background="new 0 0 264 162"><path fill="#fff" d="M239.2 151.4l-13.9 10-9.8-8v-110.9h-155v114.2l-7.6-5.3-4.4 3.7v-112.6h-39v116.5l-9-6.7v-151.8h263v151.2l-12.9 9.6-11.4-9.9z"/><path fill="#E5E5E5" d="M1 1h262v150.5l-12.3 9.2-10.9-9.5-.6-.5-.6.5-13.3 9.6-9.3-7.7v-111.1h-156v113.7l-6.5-4.6-.6-.4-.6.5-3.3 2.8v-112h-40v116l-8-6v-151m-1 151.5l10 7.4v-116.9h38v113.2l4.9-4.2 8.1 5.7v-114.7h154v110.6l10.2 8.4 13.9-10 11.5 10 13.4-10v-152h-264v152.5zM61 157.7l5.8 4.3 13-10 13 10 14.2-10 13.4 10 12.1-10 13.8 10 13-10 13 10 14.2-10 13.8 10 12.6-10 2.1 1.6v-110.6h-154v114.7zM239 50c-7.7 0-14-6.3-14-14s6.3-14 14-14 14 6.3 14 14-6.3 14-14 14zM12.7 162l13.9-10 13.8 10 7.6-5.8v-113.2h-38v116.9l2.7 2.1zM251 107v-35h-25v35h25zM31.5 5v6h-26v-6h26zM55.5 11h-22v-6h22v6zM122 6h92v4h-92v-4m-1 5h94v-6h-94v6zM79.5 11h-22v-6h22v6zM102.5 11h-13v-6h13v6zM87.5 11h-6v-6h6v6zM110.5 11h-6v-6h6v6zM255 11h-32v-6h32v6z"/><path fill="#E5E5E5" stroke="#E5E5E5" d="M2 14.5h260"/><path fill="#E5E5E5" d="M212 9h-2v-2h2v2zM251 59v-5h-25v5h25zM251 132v-35h-25v35h25z"/><path fill="#fff" d="M248.7 108.7v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c0-.1.1-.1.2-.1h20.1c.2-.1.2 0 .2.1zM248.7 113.9v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c0-.1.1-.1.2-.1h20.1c.2-.1.2 0 .2.1z"/><path fill="#D11D13" d="M248.7 103.5v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c0-.1.1-.1.2-.1h20.1c.2-.1.2 0 .2.1z"/><path fill="#fff" d="M248.7 119.1v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c0-.1.1-.1.2-.1h20.1c.2-.1.2 0 .2.1zM248.7 124.3v2c0 .1-.1.1-.2.1h-20.1c-.1 0-.2 0-.2-.1v-2c0-.1.1-.1.2-.1h20.1c.2 0 .2 0 .2.1z"/><path fill="#347BFF" d="M209.5 57.2v34.9l13.5 12.2-13.5 12.2v23.5h-139v-82.8z"/><g fill="#fff"><path d="M138.3 103.7c-4.6-4.5-8.6-8.4-10.9-17.5h16v-6.8h-16v-8.9h-6.9v9h-16v6.8h16v2.3c-2.3 8.9-5 14.6-16 20.2l2.3 6.8c10.4-5.6 15.9-12.5 18.3-20.3 2.3 5.9 6.2 10.6 10.6 15.1l2.6-6.7zM159.5 81.7h-9.2l-16 45.1h6.9l4.6-13.5h18.2l4.6 13.5h6.9l-16-45.1zm-11.5 24.8l6.9-18 6.9 18h-13.8z"/></g></svg>

specials/SpecialContentTranslation.php
@@ -21,10 +21,20 @@ class SpecialContentTranslation extends SpecialPage {
 		return $this->msg( 'cx' )->text();
 	}
 
+	public function isListed() {
+		return ContentTranslationHooks::isEnabledForUser( $this->getUser() );
+	}
+
 	public function execute( $parameters ) {
 		$out = $this->getOutput();
 		$skin = $this->getSkin();
 		$request = $this->getRequest();
+
+		// Direct access, isListed only affects Special:SpecialPages
+		if ( !ContentTranslationHooks::isEnabledForUser( $this->getUser() ) ) {
+			$out->showErrorPage( 'nosuchspecialpage', 'nospecialpagetext' );
+			return;
+		}
 
 		$out->addModuleStyles( 'mediawiki.ui.button' );
 		if ( $request->getVal( 'from' ) === null ||

specials/SpecialContentTranslationStats.php
@@ -20,8 +20,18 @@ class SpecialContentTranslationStats extends SpecialPage {
 		return $this->msg( 'cx-stats-title' )->text();
 	}
 
+	public function isListed() {
+		return ContentTranslationHooks::isEnabledForUser( $this->getUser() );
+	}
+
 	public function execute( $parameters ) {
 		$out = $this->getOutput();
+
+		// Direct access, isListed only affects Special:SpecialPages
+		if ( !ContentTranslationHooks::isEnabledForUser( $this->getUser() ) ) {
+			$out->showErrorPage( 'nosuchspecialpage', 'nospecialpagetext' );
+			return;
+		}
 
 		$this->setHeaders();
 		$this->outputHeader();

commit 7185ef117c8003dbf02d23fdc2f52bb9e178938b

Fix the anon user check logic for adding optional modules
Regression from f4a227be

Change-Id: I0989c7d6147fb417c1675d2b1843127d754d81b1

ContentTranslation.hooks.php
@@ -49,7 +49,7 @@ class ContentTranslationHooks {
 		}
 
 		// CX is currently restricted to only logged in users
-		if ( $user->isLoggedIn() ) {
+		if ( !$user->isLoggedIn() ) {
 			return;
 		}
 