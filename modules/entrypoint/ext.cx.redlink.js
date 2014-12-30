aa3d6f7d606738a4399dfc355b12bb0934f3aa82
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-12-30 13:00:39 +0200

    Show a red link only if the language is available as a target
    Bug: T85223
    Change-Id: I74c096f064dc72dc95e8476bbefff5adee15ef9c

--- modules/entrypoint/ext.cx.redlink.js
+++ modules/entrypoint/ext.cx.redlink.js
@@ -12,23 +12,65 @@
 	'use strict';
 
 	/**
-	 * Get list of target languages.
+	 * Get the list of target languages that should be suggested
+	 * to the current user.
 	 * For now, just the user interface language.
-	 * May include more languages in the future.
-	 * @return {Array}
+	 * This will probably include more languages in the future.
+	 * @param {String[]} availableTargetLanguages A list of target languages
+	 *     that are supported by this instance.
+	 * @return {string[]} target languages
 	 */
-	function getTargetLanguages() {
-		var targetLanguages = [];
+	function getSuggestedTargetLanguages( availableTargetLanguages ) {
+		var possibleTargetLanguages = [];
 
-		targetLanguages.push( mw.config.get( 'wgUserLanguage' ) );
+		possibleTargetLanguages.push( mw.config.get( 'wgUserLanguage' ) );
 
-		return targetLanguages;
+		return $.grep( possibleTargetLanguages, function ( language ) {
+			return ( $.inArray( language, availableTargetLanguages ) > -1 );
+		} );
+	}
+
+	/**
+	 * Get the list of target languages that this instance of CX supports.
+	 * @return {jQuery.Promise}
+	 */
+	function getAvailableTargetLanguages() {
+		var languagePairsAPIUrl, deferred;
+
+		// Optimization: creating the cxserver URL without sitemapper
+		// to avoid loading the sitemapper module just for this.
+		languagePairsAPIUrl =
+			mw.config.get( 'wgContentTranslationSiteTemplates' ).cx + '/languagepairs';
+
+		deferred = $.Deferred();
+
+		$.get( languagePairsAPIUrl )
+			.done( function ( response ) {
+				var sourceLanguage;
+
+				// For now, only consider the current content language as the source language.
+				// In the future we may allow translating from other languages.
+				sourceLanguage = mw.config.get( 'wgContentLanguage' );
+
+				deferred.resolve( response[ sourceLanguage ] || [] );
+			} )
+			.fail( function ( response ) {
+				mw.log(
+					'Error getting language pairs from ' + languagePairsAPIUrl + ' . ' +
+					response.statusText + ' (' + response.status + '). ' +
+					response.responseText
+				);
+
+				deferred.reject();
+			} );
+
+		return deferred.promise();
 	}
 
 	/**
 	 * Is there a page in the target language?
 	 * @param {string} code
-	 * @return {Boolean}
+	 * @return {boolean}
 	 */
 	function pageInLanguageExists( code ) {
 		return $( 'li.interlanguage-link.interwiki-' + code ).length === 1;
@@ -68,14 +110,18 @@
 		return $item;
 	}
 
-	function prepareRedInterLanguageLinks() {
-		var $newItem,
-			dependencies = [],
-			$pLangList = $( '#p-lang ul' ),
-			targetLanguages = getTargetLanguages();
+	function prepareRedInterLanguageLinks( availableTargetLanguages ) {
+		var $newItem, $pLangList, dependencies, suggestedTargetLanguages;
 
-		if ( !( targetLanguages.length === 1 &&
-			targetLanguages[ 0 ] === mw.config.get( 'wgUserLanguage' )
+		suggestedTargetLanguages = getSuggestedTargetLanguages( availableTargetLanguages );
+
+		if ( !suggestedTargetLanguages.length ) {
+			return;
+		}
+
+		dependencies = [];
+		if ( !( suggestedTargetLanguages.length === 1 &&
+			suggestedTargetLanguages[ 0 ] === mw.config.get( 'wgUserLanguage' )
 		) ) {
 			// Optimization: load jquery.uls.data
 			// only if we need more than the autonym of the user language
@@ -92,7 +138,8 @@
 				contentTextLeft + $contentText.width() + cxEntryPointDialogOffset :
 				contentTextLeft - cxEntryPointDialogOffset;
 
-			$.each( targetLanguages, function ( i, code ) {
+			$pLangList = $( '#p-lang ul' );
+			$.each( suggestedTargetLanguages, function ( i, code ) {
 				if ( !pageInLanguageExists( code ) ) {
 					$newItem = createRedInterlanguageItem( code );
 					$pLangList.prepend( $newItem );
@@ -106,6 +153,8 @@
 	}
 
 	$( function () {
-		prepareRedInterLanguageLinks();
+		getAvailableTargetLanguages().then( function ( availableTargetLanguages ) {
+			prepareRedInterLanguageLinks( availableTargetLanguages );
+		} );
 	} );
 }( jQuery, mediaWiki ) );
