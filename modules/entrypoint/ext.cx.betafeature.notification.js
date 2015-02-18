1db9269ec9b345aa6f8e6dc2df3d23cbff92af9d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2015-02-18 14:55:03 +0530

    Propagate CX beta feature option of user to target language wiki
    Propagate CX beta feature option of user from source language
    wiki to target target language wiki.
    
    Show a tipsy notification with the beta feature link in
    personal toolbar that the Content Translation is enabled.
    
    Bug: T89812
    Change-Id: If910cf88f66d35fcd5f8cb4a9dcbbb85425b7635

---
+++ modules/entrypoint/ext.cx.betafeature.notification.js
@@ -0,0 +1,27 @@
+/**
+ * ContentTranslation Tools
+ * A tool that allows editors to translate pages from one language
+ * to another with the help of machine translation and other translation tools
+ *
+ * @file
+ * @ingroup Extensions
+ * @copyright See AUTHORS.txt
+ * @license GPL-2.0+
+ */
+( function ( $, mw ) {
+	'use strict';
+
+	$( function () {
+		var $betaTrigger;
+
+		$betaTrigger = $( '#pt-betafeatures' );
+		if ( !$betaTrigger.length ) {
+			return;
+		}
+		$betaTrigger.tipsy( {
+			title: function () {
+				return mw.msg( 'cx-beta-feature-enabled-notification' );
+			}
+		} ).tipsy( 'show' );
+	} );
+}( jQuery, mediaWiki ) );
