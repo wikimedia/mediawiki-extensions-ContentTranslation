01ab4df7211eb6eb74a34250a6fcc90dae77d3b7
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2015-03-23 11:54:43 +0530

    Updated callout widget
    Change-Id: Ie8561c64f2c5f3d13a08d7b07fe830982155db0f

--- modules/widgets/callout/ext.cx.callout.js
+++ modules/widgets/callout/ext.cx.callout.js
@@ -8,10 +8,16 @@
 ( function ( $ ) {
 	'use strict';
 
-	function maybeCall( thing, ctx ) {
-		return ( typeof thing === 'function' ) ? ( thing.call( ctx ) ) : thing;
+	/**
+	 * Depending on type, return function call result or just the thing.
+	 */
+	function maybeCall( thing, context ) {
+		return $.isFunction( thing ) ? ( thing.call( context ) ) : thing;
 	}
 
+	/**
+	 * Callout class
+	 */
 	function Callout( element, options ) {
 		this.$element = $( element );
 		this.options = $.extend( {}, $.fn.callout.defaults, options );
@@ -19,6 +25,9 @@
 		this.listen();
 	}
 
+	/**
+	 * Show the callout
+	 */
 	Callout.prototype.show = function () {
 		var $dialog, content;
 
@@ -29,7 +38,7 @@
 
 		$dialog = this.dialog();
 		$dialog.find( '.cx-callout-content' ).empty().append( content );
-		$dialog[ 0 ].className = 'cx-callout'; // reset classname in case of dynamic gravity
+		$dialog[ 0 ].className = 'cx-callout'; // reset classname in case of dynamic type
 		if ( this.options.classes ) {
 			$dialog.addClass( this.options.classes );
 		}
@@ -59,8 +68,11 @@
 		this.shown = true;
 	};
 
+	/**
+	 * Position the callout
+	 */
 	Callout.prototype.position = function () {
-		var $dialog, pos, gravity, actualWidth, actualHeight, position;
+		var $dialog, pos, direction, actualWidth, actualHeight, position;
 
 		pos = $.extend( {}, this.$element.offset(), {
 			width: this.$element[ 0 ].offsetWidth,
@@ -68,67 +80,102 @@
 		} );
 
 		$dialog = this.dialog();
-		gravity = ( typeof this.options.gravity === 'function' ) ?
-			this.options.gravity.call( this.$element[ 0 ] ) :
-			this.options.gravity;
-
+		direction = $.isFunction( this.options.direction ) ? this.options.direction.call( this.$element[ 0 ] ) : this.options.direction;
 		// Attach css classes before checking height/width so they
 		// can be applied.
-		$dialog.addClass( 'cx-callout-' + gravity );
+		$dialog.removeClass().addClass( 'cx-callout cx-callout-' + direction );
 
 		actualWidth = $dialog[ 0 ].offsetWidth;
 		actualHeight = $dialog[ 0 ].offsetHeight;
-		switch ( gravity.charAt( 0 ) ) {
-		case 'n':
+		switch ( direction ) {
+		case '0':
+		case '12':
 			position = {
 				top: pos.top + pos.height + this.options.offset,
 				left: pos.left + pos.width / 2 - actualWidth / 2
 			};
 			break;
-		case 's':
+		case '1':
 			position = {
-				top: pos.top - actualHeight - this.options.offset,
-				left: pos.left + pos.width / 2 - actualWidth / 2
+				top: pos.top + pos.height + this.options.offset,
+				left: pos.left + pos.width - actualWidth
 			};
 			break;
-		case 'e':
+		case '11':
+			position = {
+				top: pos.top + pos.height + this.options.offset,
+				left: pos.left
+			};
+			break;
+		case '2':
+			position = {
+				top: pos.top,
+				left: pos.left - actualWidth - this.options.offset
+			};
+			break;
+		case '3':
 			position = {
 				top: pos.top + pos.height / 2 - actualHeight / 2,
 				left: pos.left - actualWidth - this.options.offset
 			};
 			break;
-		case 'w':
+		case '4':
+			position = {
+				top: pos.top + pos.height - actualHeight,
+				left: pos.left - actualWidth - this.options.offset
+			};
+			break;
+		case '5':
+			position = {
+				top: pos.top - actualHeight - this.options.offset,
+				left: pos.left + pos.width - actualWidth
+			};
+			break;
+		case '6':
+			position = {
+				top: pos.top - actualHeight - this.options.offset,
+				left: pos.left + pos.width / 2 - actualWidth / 2
+			};
+			break;
+		case '7':
+			position = {
+				top: pos.top - actualHeight - this.options.offset,
+				left: pos.left
+			};
+			break;
+		case '8':
+			position = {
+				top: pos.top + pos.height - actualHeight,
+				left: pos.left + pos.width + this.options.offset
+			};
+			break;
+		case '9':
 			position = {
 				top: pos.top + pos.height / 2 - actualHeight / 2,
 				left: pos.left + pos.width + this.options.offset
 			};
 			break;
+		case '10':
+			position = {
+				top: pos.top,
+				left: pos.left + pos.width + this.options.offset
+			};
+			break;
 		}
 
-		if ( gravity.length === 2 ) {
-			if ( gravity.charAt( 1 ) === 'w' ) {
-				if ( this.options.center ) {
-					position.left = pos.left + pos.width / 2 - 15;
-				} else {
-					position.left = pos.left;
-				}
-			} else {
-				if ( this.options.center ) {
-					position.left = pos.left + pos.width / 2 - actualWidth + 15;
-				} else {
-					position.left = pos.left + pos.width;
-				}
-			}
-		}
 		$dialog.css( position );
 	};
 
+	/**
+	 * Listen for event
+	 */
 	Callout.prototype.listen = function () {
 		var timer, self = this;
 
 		if ( this.options.trigger === 'auto' ) {
 			self.show();
 		}
+
 		if ( this.options.trigger === 'hover' ) {
 			this.$element.on( 'mouseenter', function () {
 				self.show();
@@ -146,6 +193,7 @@
 				}
 			} );
 		}
+
 		$( window ).resize( function () {
 			clearTimeout( timer );
 			timer = setTimeout( function () {
@@ -154,6 +202,9 @@
 		} );
 	};
 
+	/**
+	 * Hide the callout
+	 */
 	Callout.prototype.hide = function () {
 		if ( this.options.fade ) {
 			this.dialog().stop().fadeOut( 100, function () {
@@ -165,116 +216,112 @@
 		this.shown = false;
 	};
 
+	/**
+	 * Construct and return the dialog
+	 * @return {jQuery}
+	 */
 	Callout.prototype.dialog = function () {
 		if ( !this.$dialog ) {
-			this.$dialog = $( '<div class="cx-callout"></div>' ).html( '</div><div class="cx-callout-content"></div>' );
+			this.$dialog = $( '<div>' ).addClass( 'cx-callout' )
+				.append( $( '<div>' ).addClass( 'cx-callout-content' ) );
 		}
 		return this.$dialog;
 	};
 
-	Callout.prototype.validate = function () {
-		if ( !this.$element[ 0 ].parentNode ) {
-			this.hide();
-			this.$element = null;
-			this.options = null;
-		}
-	};
-
+	/**
+	 * The callout plugin
+	 */
 	$.fn.callout = function ( options ) {
-		if ( options === true ) {
-			return this.data( 'callout' );
-		} else if ( typeof options === 'string' ) {
-			var callout = this.data( 'callout' );
-			if ( callout ) {
-				callout[ options ]();
-			}
-			return this;
-		}
+		return this.each( function () {
+			var $this = $( this ),
+				data = $this.data( 'callout' );
 
-		function get( ele ) {
-			var callout = $.data( ele, 'callout' );
-			if ( !callout ) {
-				callout = new Callout( ele, $.fn.callout.elementOptions( ele, options ) );
-				$.data( ele, 'callout', callout );
+			if ( !data ) {
+				$this.data( 'callout', ( data = new Callout( this, options ) ) );
 			}
-			return callout;
-		}
 
-		this.each( function () {
-			get( this );
+			if ( typeof options === 'string' ) {
+				data[ options ].call( $this );
+			}
 		} );
-
-		return this;
-	};
-
-	$.fn.callout.defaults = {
-		gravity: 'n',
-		center: true,
-		offset: 10,
-		opacity: 1.0,
-		trigger: 'hover'
-	};
-
-	// Overwrite this method to provide options on a per-element basis.
-	// For example, you could store the gravity in a 'cx-callout-gravity' attribute:
-	// return $.extend({}, options, {gravity: $(ele).attr('cx-callout-gravity') || 'n' });
-	// (remember - do not modify 'options' in place!)
-	$.fn.callout.elementOptions = function ( ele, options ) {
-		return $.metadata ? $.extend( {}, options, $( ele ).metadata() ) : options;
-	};
-
-	$.fn.callout.autoNS = function () {
-		return $( this ).offset().top > ( $( document ).scrollTop() + $( window ).height() / 2 ) ? 's' : 'n';
-	};
-
-	$.fn.callout.autoNEW = function () {
-		return $( this ).offset().left > ( $( document ).scrollLeft() + $( window ).width() / 2 ) ? 'ne' : 'nw';
-	};
-
-	$.fn.callout.autoWE = function () {
-		return $( this ).offset().left > ( $( document ).scrollLeft() + $( window ).width() / 2 ) ? 'e' : 'w';
 	};
 
 	/**
-	 * yields a closure of the supplied parameters, producing a function that takes
-	 * no arguments and is suitable for use as an autogravity function like so:
+	 * Return a closure that can calculate a good direction based on the arguments.
 	 *
-	 * @param margin (int) - distance from the viewable region edge that an
-	 *        element should be before setting its tooltip's gravity to be away
-	 *        from that edge.
-	 * @param prefer (string, e.g. 'n', 'sw', 'w') - the direction to prefer
-	 *        if there are no viewable region edges effecting the tooltip's
-	 *        gravity. It will try to vary from this minimally, for example,
-	 *        if 'sw' is preferred and an element is near the right viewable
-	 *        region edge, but not the top edge, it will set the gravity for
-	 *        that element's tooltip to be 'se', preserving the southern
-	 *        component.
+	 * @param {string} prefer - the direction to prefer.  if there are no viewable region
+	 *     edges effecting the callouts's direction.  e.g. '3', '6', 9'
+	 * @return {function}
 	 */
-	$.fn.callout.autoBounds = function ( margin, prefer ) {
+	$.fn.callout.autoDirection = function ( prefer ) {
 		return function () {
-			var dir = {
-					ns: prefer[ 0 ],
-					ew: ( prefer.length > 1 ? prefer[ 1 ] : false )
-				},
-				boundTop = $( document ).scrollTop() + margin,
-				boundLeft = $( document ).scrollLeft() + margin,
+			var direction = prefer,
+				leftFlips, rightFlips, topFlips, bottomFlips,
+				boundTop = $( document ).scrollTop() + $( window ).height() / 2,
+				boundLeft = $( document ).scrollLeft() + $( window ).width() / 2,
 				$this = $( this );
 
+			leftFlips = {
+				'1': '11',
+				'2': '10',
+				'3': '9',
+				'4': '8',
+				'5': '7'
+			};
+			rightFlips = {
+				'11': '1',
+				'10': '2',
+				'9': '3',
+				'8': '4',
+				'7': '5'
+			};
+			topFlips = {
+				'3': '2',
+				'4': '2',
+				'5': '1',
+				'6': '0',
+				'7': '11',
+				'8': '10',
+				'9': '10'
+			};
+			bottomFlips = {
+				'0': '6',
+				'12': '6',
+				'1': '5',
+				'2': '4',
+				'3': '4',
+				'11': '7',
+				'10': '8',
+				'9': '8'
+			};
 			if ( $this.offset().top < boundTop ) {
-				dir.ns = 'n';
+				direction = topFlips[ direction ] || direction;
 			}
-			if ( $this.offset().left < boundLeft ) {
-				dir.ew = 'w';
+			if ( $this.offset().top > boundTop ) {
+				direction = bottomFlips[ direction ] || direction;
 			}
-			if ( $( window ).width() + $( document ).scrollLeft() - $this.offset().left < margin ) {
-				dir.ew = 'e';
+			if ( $( this ).offset().left < boundLeft ) {
+				direction = leftFlips[ direction ] || direction;
 			}
-			if ( $( window ).height() + $( document ).scrollTop() - $this.offset().top < margin ) {
-				dir.ns = 's';
+			if ( $( this ).offset().left > boundLeft ) {
+				direction = rightFlips[ direction ] || direction;
 			}
-
-			return dir.ns + ( dir.ew ? dir.ew : '' );
+			return direction;
 		};
+	};
+
+	$.fn.callout.defaults = {
+		// direction of callout. Imagine the callout is along with the direction of a
+		// clock handle with the tip as tip of handle.
+		// Possible directions are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12.
+		// It is also possible to give a preferred direction using $.fn.callout.autoDirection
+		// Example: $.fn.callout.autoDirection( 100, '0' ),
+		// Auto direction will change direction if screen space is not available.
+		direction: $.fn.callout.autoDirection( '0' ),
+		// Offset of dialog from the trigger
+		offset: 10,
+		opacity: 1.0,
+		trigger: 'hover'
 	};
 
 }( jQuery ) );
