ca088d23d836aa4c1b631a091b1f8a648ad329a7
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2015-06-23 12:08:39 +0530

    Support <poem> translations
    Adapt the poem (mw:Extension/poem) from the source article to the
    target article.
    
    Editing the poem text is not supported. If there is MT, the translation
    will be used for target article.
    
    Testplan:
    Translate an article with poem in it. Example:
    https://en.wikipedia.org/wiki/Puja_%28Hinduism%29
    After translating, ensure that translation has poem with content in it.
    
    Bug: T100385
    Change-Id: I98d29f889b9afd4497898a2edbfce5366e5e719e

---
+++ modules/tools/ext.cx.tools.poem.js
@@ -0,0 +1,38 @@
+/**
+ * ContentTranslation Tools
+ * A tool that allows editors to translate pages from one language
+ * to another with the help of machine translation and other translation tools
+ *
+ * @file
+ * @ingroup Extensions
+ * @copyright See AUTHORS.txt
+ * @license GPL-2.0+
+ */
+( function ( $, mw ) {
+	'use strict';
+
+	function adaptPoem( $section ) {
+		var $sourceSection, poemData, $poem;
+
+		$sourceSection = mw.cx.getSourceSection( $section.data( 'source' ) );
+		if ( $section.is( '[typeof*="mw:Extension/poem"]' ) ) {
+			$poem = $sourceSection;
+		} else {
+			$poem = $sourceSection.find( '[typeof*="mw:Extension/poem"]' );
+		}
+		if ( !$poem.length ) {
+			return;
+		}
+		poemData = $poem.data( 'mw' );
+		// TODO: Note that we are not retaining markup here.
+		poemData.body.extsrc = $poem.text().replace( /<br\s*\/?>/mg, '\n' );
+		// Copy the data-mw to target section.
+		$section.find( '[typeof*="mw:Extension/poem"]' ).attr( 'data-mw', JSON.stringify( poemData ) );
+		// Make the poem readonly. We can only adapt them. Not allowing edits.
+		$section.find( 'p' ).attr( 'contenteditable', false );
+	}
+
+	$( function () {
+		mw.hook( 'mw.cx.translation.postMT' ).add( adaptPoem );
+	} );
+}( jQuery, mediaWiki ) );
