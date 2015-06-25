c4cf862424433e80ab709dbd1ae43b3b3047b8f5
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-06-25 20:56:37 +0200

    Fix CX alias file loading
    When not using wfLoadExtension directly, aliases were ignored
    because they were using same array key as messages
    
    Bug: T103888
    Change-Id: I53b97221b87fe979a6169f0923637b19630db9e9

--- ContentTranslation.php
+++ ContentTranslation.php
@@ -3,7 +3,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'ContentTranslation' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['ContentTranslation'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['ContentTranslation'] = __DIR__ . '/ContentTranslation.alias.php';
+	$wgExtensionMessagesFiles['ContentTranslationAlias'] = __DIR__ . '/ContentTranslation.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for ContentTranslation extension. ' .
 		'Please use wfLoadExtension instead, ' .
