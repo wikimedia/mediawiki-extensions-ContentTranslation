5ecf38c7de3d873284d8662385393bcf2a054a34
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-12-28 09:19:47 +0200

    Move the language list from the deletion script to a separate file
    To make it reusable in other scripts.
    
    This also removes some language codes that don't belong
    to actual Wikipedias, like ten, test, etc.
    
    Change-Id: Ie997666e58b0dbe2ebb23735330d657ddd79e538

--- scripts/daily-stats/count_deletion.sh
+++ scripts/daily-stats/count_deletion.sh
@@ -9,8 +9,9 @@ echo "Deletions on $date"
 query="SELECT count(ar_id) FROM change_tag, archive, user WHERE ar_timestamp like '$date%' AND ar_namespace = 0 AND ct_tag = 'contenttranslation' AND ar_rev_id = ct_rev_id AND ar_user = user_id ORDER BY NULL;"
 
 total_deletions=0
+language_list=`cat language_list.txt`
 
-for language in aa ab ace af ak als am ang an arbcom_de arbcom_en arbcom_fi arbcom_nl arc ar arz ast as av ay az azb bar bat_smg ba bcl be_x_old be bg bh bi bjn bm bn bo bpy br bs bug bxr ca cbk_zam cdo ceb ce cho chr ch chy ckb co crh cr csb cs cu cv cy da de diq dsb dv dz ee el eml en eo es et eu ext fa ff fiu_vro fi fj fo frp frr fr fur fy gag gan ga gd glk gl gn gom got gu gv hak ha haw he hif hi ho hr hsb ht hu hy hz ia id ie ig ii ik ilo io is it iu ja jbo jv kaa kab ka kbd kg ki kj kk kl km kn koi ko krc kr ksh ks ku kv kw ky lad la lbe lb lez lg lij li lmo ln lo lrc ltg lt lv mai map_bms mdf mg mhr mh min mi mk ml mn mo mrj mr ms mt mus mwl myv my mzn nah nap na nds_nl nds ne new ng nl nn nov no nrm nso nv ny oc om or os pag pam pap pa pcd pdc pfl pih pi pl pms pnb pnt ps pt qu rm rmy rn roa_rup roa_tara ro rue ru rw sah sa scn sco sc sd se sg sh simple si sk sl sm sn so sq srn sr ss stq st su sv sw szl ta ten test test2 tet te tg th ti tk tl tn to tpi tr ts tt tum tw tyv ty udm ug uk ur uz vec vep ve vi vls vo war wa wg_en wo wuu xal xh xmf yi yo za zea zh_classical zh_min_nan zh_yue zh zu
+for language in $language_list
 do
 	db="${language}wiki"
 
@@ -24,4 +25,3 @@ do
 done
 
 echo "Total deletions for $date: $total_deletions"
-
