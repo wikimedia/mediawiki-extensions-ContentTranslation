d95dd64a6839ac165f4303b29f25613c74d596fc
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2016-11-25 12:37:51 +0530

    Use mw.widgets.TitleInputWidget for page selector
    Replaces the page selector in CX source page selector with a new
    OOJS-UI based selector.
    
    Bug: T151985
    Change-Id: I89a40152a4c93d8b78c4d7990a184ead7849acae

---
+++ modules/ui/widgets/mw.cx.ui.PageSelectorWidget.js
@@ -0,0 +1,73 @@
+/*!
+ * Content Translation UserInterface PageSelectorWidget class.
+ *
+ * @ingroup Extensions
+ * @copyright See AUTHORS.txt
+ * @license GPL-2.0+
+ */
+
+( function ( $, mw, OO ) {
+	'use strict';
+
+	/**
+	 * Creates an mw.cx.ui.PageSelectorWidget object.
+	 *
+	 * @class
+	 * @extends mw.cx.ui.PageSelectorWidget
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 * @cfg {number} [namespace] Namespace to prepend to queries. Defaults to main namespace.
+	 */
+	mw.cx.ui.PageSelectorWidget = function PageSelectorWidget( config ) {
+		config = $.extend( {}, {
+			namespace: mw.config.get( 'wgNamespaceIds' ).main,
+			showDescriptions: true,
+			showImages: true,
+			icon: 'search'
+		}, config );
+
+		this.siteMapper = config.siteMapper;
+		this.language = config.language || 'en';
+		// Parent constructor
+		mw.cx.ui.PageSelectorWidget.super.call( this, config );
+		// Initialization
+		this.$element.addClass( 'mw-cx-ui-PageSelectorWidget' );
+	};
+
+	/* Inheritance */
+
+	OO.inheritClass( mw.cx.ui.PageSelectorWidget, mw.widgets.TitleInputWidget );
+
+	/* Methods */
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.cx.ui.PageSelectorWidget.prototype.getApi = function () {
+		return this.siteMapper.getApi( this.language );
+	};
+
+	mw.cx.ui.PageSelectorWidget.prototype.setLanguage = function ( language ) {
+		this.language = language;
+		this.setDir( $.uls.data.getDir( language ) );
+
+		// Reset the requestCache of OO.ui.mixin.LookupElement
+		this.requestCache = {};
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.cx.ui.PageSelectorWidget.prototype.getOptionWidgetData = function ( title, data ) {
+		var optionWidgetData;
+
+		// Mixin method
+		optionWidgetData = mw.widgets.TitleWidget.prototype.getOptionWidgetData.call( this, title, data );
+		// Correct the URL so that it can point to the source language wiki.
+		optionWidgetData.url = this.siteMapper.getPageUrl( this.language, title );
+
+		return optionWidgetData;
+	};
+
+}( jQuery, mediaWiki, OO ) );
