9c3723c954dd3d0e3a085f342592824ce101eef6
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2017-02-03 12:37:45 +0530

    Add machine translation tool
    This is no-op now, just the tool card and drop down.
    
    Bug: T152586
    Change-Id: Idb7c30d4833d42901c66ac864578fa538f7d2d22

---
+++ modules/tools/mw.cx.tools.MachineTranslationTool.js
@@ -0,0 +1,79 @@
+/**
+ * Reference tool
+ *
+ * @class
+ * @extends mw.cx.tools.TranslationTool
+ * @constructor
+ * @param {mw.cx.ui.TranslationUnit} ui
+ * @param {Object} config
+ */
+mw.cx.tools.MachineTranslationTool = function CXMachineTranslationTool( ui, config ) {
+	config.title = mw.msg( 'cx-tools-mt-title' );
+	config.name = 'machinetranslation';
+	config.language = config.targetLanguage;
+	config.order = 5;
+	// Parent constructor
+	mw.cx.tools.MachineTranslationTool.super.call( this, ui, config );
+	this.ui.connect( this, {
+		click: 'showTool'
+	} );
+};
+
+/* Inheritance */
+OO.inheritClass( mw.cx.tools.MachineTranslationTool, mw.cx.tools.TranslationTool );
+
+mw.cx.tools.MachineTranslationTool.static.name = 'machinetranslation';
+mw.cx.tools.MachineTranslationTool.static.defaultProvider = null;
+
+/**
+ * @inheritDoc
+ */
+mw.cx.tools.MachineTranslationTool.prototype.getActions = function () {
+	this.keepDefault = new OO.ui.ButtonWidget( {
+		label: mw.msg( 'cx-tools-mt-set-default' ),
+		icon: 'check',
+		framed: false,
+		classes: [ 'cx-mt-set-default' ]
+	} );
+	this.actions = [
+		this.keepDefault
+	];
+	this.keepDefault.connect( this, {
+		click: 'setDefaultProvider'
+	} );
+	return this.actions;
+};
+
+mw.cx.tools.MachineTranslationTool.prototype.getContent = function () {
+	var noMT, useSource, resetMT;
+
+	noMT = new OO.ui.MenuOptionWidget( {
+		data: 'no-mt',
+		label: mw.msg( 'cx-tools-mt-dont-use' )
+	} );
+	useSource = new OO.ui.MenuOptionWidget( {
+		data: 'source-mt',
+		label: mw.msg( 'cx-tools-mt-use-source' )
+	} );
+	resetMT = new OO.ui.MenuOptionWidget( {
+		data: 'reset-mt',
+		label: mw.msg( 'cx-tools-mt-reset' )
+	} );
+	this.mtProviderSelector = new OO.ui.DropdownWidget( {
+		classes: [ 'card-mt-providers-menu' ],
+		menu: {
+			items: [ useSource, noMT, resetMT ]
+		}
+	} );
+	this.mtProviderSelector.getMenu().selectItemByData( 'source-mt' );
+
+	return this.mtProviderSelector.$element;
+};
+
+/**
+ * Remove the reference
+ */
+mw.cx.tools.MachineTranslationTool.prototype.setDefaultProvider = function () {};
+
+/* Register */
+mw.cx.tools.translationToolFactory.register( mw.cx.tools.MachineTranslationTool );
