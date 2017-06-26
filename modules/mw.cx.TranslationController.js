wikimedia/mediawiki-extensions-ContentTranslation

commit 61b74cafa029fbd475093ee2e898914277be41ea

Simplify code a bit
Removed get/setTranslationSource. The SectionTranslationUnit model
already handles this.

Change-Id: I159edf970ba207d412768f529d9826007a84c92d

modules/mw.cx.TranslationController.js
@@ -204,23 +204,27 @@ mw.cx.TranslationController.prototype.getContentToSave = function ( saveQueue ) 
 
 /**
  * Get the records for saving the translation unit.
- * @param {mw.cx.dm.translationUnit} translationUnit
+ * @param {mw.cx.dm.SectionTranslationUnit} translationUnit
  * @return {Object[]} Objects to save
  */
 mw.cx.TranslationController.prototype.getTranslationUnitData = function ( translationUnit ) {
 	var sequenceId, origin, translationSource, records = [],
 		validate;
 
+	if ( !( translationUnit instanceof mw.cx.dm.SectionTranslationUnit ) ) {
+		mw.log.error( '[CX] Trying to save a non-section: ' + translationUnit.getId() );
+	}
+
 	// XXX Section validation for abusefilter
 	validate = false;
 
 	sequenceId = translationUnit.sourceDocument.getAttribute( 'data-seqid' );
-	translationSource = translationUnit.getTranslationSource();
-	if ( translationSource === 'mt' ) {
-		origin = translationUnit.getMachineTranslationProvider();
-	} else {
+	// XXX should use the promise, but at this point the member variable should always be present
+	translationSource = translationUnit.MTProvider;
+	if ( translationSource === 'source' ) {
 		origin = 'user';
 	}
+
 	records.push( {
 		content: translationUnit.getTargetDocument().outerHTML,
 		sectionId: translationUnit.sourceDocument.id, // source section id is the canonical section id.

modules/dm/translationunits/mw.cx.dm.LinkTranslationUnit.js
@@ -86,7 +86,6 @@ mw.cx.dm.LinkTranslationUnit.prototype.findLinkTarget = function ( sourceLanguag
  * @return {jQuery.Promise}
  */
 mw.cx.dm.LinkTranslationUnit.prototype.adapt = function () {
-
 	if ( this.targetDocument ) {
 		this.redlink = this.targetDocument.classList.contains( 'new' );
 		mw.log.warn( '[CX] Adapting a link which looks already adapted: ' + this );

modules/dm/translationunits/mw.cx.dm.TranslationUnit.js
@@ -38,8 +38,6 @@ mw.cx.dm.TranslationUnit = function TranslationUnit( config, translation, source
 	this.sourceLanguage = config.sourceLanguage;
 	this.targetLanguage = config.targetLanguage;
 	this.parentTranslationUnit = null;
-	// Source of the translation. Can be source, user, Apertium, Yandex etc
-	this.translationSource = null;
 	// Parent constructor
 	mw.cx.dm.TranslationUnit.super.call( this );
 	this.connect( this, {
@@ -275,14 +273,6 @@ mw.cx.dm.TranslationUnit.prototype.getTranslationSectionId = function () {
 	}
 
 	return 'cx' + this.getSectionId();
-};
-
-mw.cx.dm.TranslationUnit.prototype.getTranslationSource = function () {
-	return this.translationSource;
-};
-
-mw.cx.dm.TranslationUnit.prototype.setTranslationSource = function ( translationSource ) {
-	this.translationSource = translationSource;
 };
 
 /**

commit aa459712877f2840f3aabe4b014fc22ce643a74e

CX2: Prevent save failures due to origin not set in translation units
Regression from 61b74cafa029f

Change-Id: I01bff3471971435bd51529733dd85b6e273d9cb4

modules/mw.cx.TranslationController.js
@@ -221,8 +221,10 @@ mw.cx.TranslationController.prototype.getTranslationUnitData = function ( transl
 	sequenceId = translationUnit.sourceDocument.getAttribute( 'data-seqid' );
 	// XXX should use the promise, but at this point the member variable should always be present
 	translationSource = translationUnit.MTProvider;
-	if ( translationSource === 'source' ) {
+	if ( translationSource === 'source' || translationSource === 'scratch' ) {
 		origin = 'user';
+	} else {
+		origin = translationSource;
 	}
 
 	records.push( {