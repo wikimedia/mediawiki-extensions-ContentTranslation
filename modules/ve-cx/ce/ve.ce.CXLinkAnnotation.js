915164b983754821963dca3d94041c4dae840bf4
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-03-08 15:37:28 +0530

    CX2: Don't use link cache for styling the links
    Show the links based on adaptation information supplied by cxserver.
    
    Bug: T188071
    Change-Id: I4decc5a01cf182abf68794478c623916209b6765

--- modules/ve-cx/ce/ve.ce.CXLinkAnnotation.js
+++ modules/ve-cx/ce/ve.ce.CXLinkAnnotation.js
@@ -1,5 +1,5 @@
 /**
- * Annotation representing an adapted link
+ * Annotation representing an adapted or unadapted link
  *
  * @class
  * @extends ve.ce.MWInternalLinkAnnotation
@@ -7,12 +7,17 @@
  * @param {ve.dm.CXLinkAnnotation} model
  */
 ve.ce.CXLinkAnnotation = function VeCeCXLinkAnnotation( model ) {
-	// Parent constructor
-	ve.ce.CXLinkAnnotation.super.apply( this, arguments );
+	this.model = model;
+	// Parent's parent constructor because the parent constructor also uses
+	// VE Link cache to style the links. CX can not use it.
+	ve.ce.MWInternalLinkAnnotation.super.apply( this, arguments );
 
 	this.$anchor
 		.addClass( 've-ce-cxLinkAnnotation' )
-		.data( 'linkid', model.getAttribute( 'linkid' ) );
+		.data( 'linkid', this.model.getAttribute( 'linkid' ) );
+
+	// Style the link based on the adaptation information.
+	this.styleAnchor( this.getAdaptationInfo() );
 };
 
 /* Inheritance */
@@ -22,6 +27,26 @@ OO.inheritClass( ve.ce.CXLinkAnnotation, ve.ce.MWInternalLinkAnnotation );
 /* Static Properties */
 
 ve.ce.CXLinkAnnotation.static.name = 'cxLink';
+
+/**
+ * Get the adaptation info supplied by cxserver
+ *
+ * @return {Object} adptationInfo
+ */
+ve.ce.CXLinkAnnotation.prototype.getAdaptationInfo = function () {
+	return this.model.getAttribute( 'cx' );
+};
+
+/**
+ * Style the link based on the adaptation information.
+ *
+ * @param {Object} adptationInfo
+ */
+ve.ce.CXLinkAnnotation.prototype.styleAnchor = function ( adptationInfo ) {
+	if ( adptationInfo && !adptationInfo.adapted ) {
+		this.$anchor.addClass( 'cx-target-link-unadapted' );
+	}
+};
 
 /* Registration */
 
