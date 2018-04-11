62f62abdc52ea806ca2a677edde7daa3d3fda07c
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-04-11 16:53:39 +0530

    CX2: Show introduction card only when relevant
    As per T188072, the tools column is going to have contextual and
    non-contextual tools. Contextual tools are VE tools. Instructions
    tool, Article level issues etc are non-contextual tools.
    
    When contextual tools are shown, we won't show non-contextual tools.
    
    This patch takes care of Instructions tools only, but if there are more
    non-contextual tools, we need to consider that too.
    
    Bug: T189869
    Change-Id: I74c1fb38192fbe0706675b0a279ebca2f50438ff

--- modules/ve-cx/ui/ve.ui.CXSurface.js
+++ modules/ve-cx/ui/ve.ui.CXSurface.js
@@ -12,9 +12,13 @@
 ve.ui.CXSurface = function VeUiCXSurface( dataOrDoc, toolsColumn, config ) {
 	// Parent constructor
 	ve.ui.CXSurface.super.call( this, dataOrDoc, config );
-
+	this.toolsContainer = toolsColumn.toolContainer;
 	// Move context to toolsContainer
-	toolsColumn.toolContainer.$element.append( this.context.$element );
+	this.toolsContainer.$element.append( this.context.$element );
+	this.getView().connect( this, {
+		focus: 'onFocus',
+		blur: 'onBlur'
+	} );
 };
 
 /* Inheritance */
@@ -29,4 +33,12 @@ OO.inheritClass( ve.ui.CXSurface, ve.ui.Surface );
  */
 ve.ui.CXSurface.prototype.createContext = function () {
 	return new ve.ui.CXDesktopContext( this );
+};
+
+ve.ui.CXSurface.prototype.onBlur = function () {
+	this.toolsContainer.$element.removeClass( 'cx-column-tools-container--contextual' );
+};
+
+ve.ui.CXSurface.prototype.onFocus = function () {
+	this.toolsContainer.$element.addClass( 'cx-column-tools-container--contextual' );
 };
