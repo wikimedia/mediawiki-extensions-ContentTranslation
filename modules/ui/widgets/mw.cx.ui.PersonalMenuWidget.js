7298dc92911d7c02c2dd32b15b00615653d8f68d
Ed Sanders <esanders@wikimedia.org>
2018-05-21 21:40:40 +0100

    Simplify width override code on personal menu
    Change-Id: I0bcde7a0d3efe6f169987c29029629d272c0d707

--- modules/ui/widgets/mw.cx.ui.PersonalMenuWidget.js
+++ modules/ui/widgets/mw.cx.ui.PersonalMenuWidget.js
@@ -18,16 +18,10 @@
  * @param {Object} [config] Configuration options
  */
 mw.cx.ui.PersonalMenuWidget = function PersonalMenuWidget( config ) {
-	this.isIdealMenuSizeSet = false;
-	this.idealSize = 'auto';
-
 	// Parent constructor
 	mw.cx.ui.PersonalMenuWidget.super.call( this, config );
 	// Mixin constructor
 	OO.ui.mixin.FlaggedElement.call( this, config );
-
-	// Listen for events
-	this.listen();
 
 	// Initialization
 	this.$element.addClass( 'mw-cx-ui-PersonalMenuWidget' );
@@ -48,50 +42,14 @@ OO.mixinClass( mw.cx.ui.PersonalMenuWidget, OO.ui.mixin.FlaggedElement );
 mw.cx.ui.PersonalMenuWidget.prototype.onMenuSelect = function ( item ) {
 	// Navigate browser to a new page
 	location.href = item.getData();
+	// Don't call parent to avoid changing handle
 };
 
 /**
- * Attach event listeners
+ * @inheritdoc
  */
-mw.cx.ui.PersonalMenuWidget.prototype.listen = function () {
-	this.menu.connect( this, { toggle: 'onMenuVisible' } );
-	// Resize handler
-	$( window ).on( 'resize', OO.ui.throttle( this.resize.bind( this ), 250 ) );
-};
-
-/**
- * Callback for menu toggle event. Sets ideal size for menu.
- */
-mw.cx.ui.PersonalMenuWidget.prototype.onMenuVisible = function () {
-	// Ideal menu size is calculated only on first menu opening.
-	if ( !this.isIdealMenuSizeSet ) {
-		this.setIdealMenuSize();
-		this.isIdealMenuSizeSet = true;
-	}
-
-	this.menu.setIdealSize( this.idealSize );
-};
-
-/**
- * If screen size gets changed, we need to recalculate ideal menu size.
- */
-mw.cx.ui.PersonalMenuWidget.prototype.resize = function () {
-	this.getMenu().toggle( false );
-	this.blur();
-
-	this.isIdealMenuSizeSet = false;
-	this.idealSize = 'auto';
-};
-
-/**
- * Calculates ideal size for personal menu. Called only once to initialize menu size,
- * and after screen size changes.
- */
-mw.cx.ui.PersonalMenuWidget.prototype.setIdealMenuSize = function () {
-	var handleWidth = this.$element.width(),
-		menuWidth = this.menu.$element.width();
-
-	if ( handleWidth > menuWidth ) {
-		this.idealSize = '100%';
-	}
+mw.cx.ui.PersonalMenuWidget.prototype.onMenuToggle = function () {
+	this.menu.$element.css( 'min-width', this.$element.outerWidth() );
+	// Parent method
+	mw.cx.ui.PersonalMenuWidget.super.prototype.onMenuToggle.apply( this, arguments );
 };
