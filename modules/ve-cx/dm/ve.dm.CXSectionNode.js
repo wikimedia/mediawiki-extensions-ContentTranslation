dd6d9f548441bc96d4769da368af6481c00d63f2
petarpetkovic <ppetkovic@wikimedia.org>
2018-05-25 14:50:01 +0200

    CX2: Don't save sections while restoring a draft
    Fixes regression from b625f8e883678698ff73083e9f8b08113e59ef31.
    
    Whole onUpdate() method was wrapped in ve.debounce, which caused
    condition from isTargetSection() to be satisfied before it needed
    to be (while restoring a draft). Only part that emits 'sectionChange'
    should be deferred from immediate execution, to have the same
    state as before refactoring done in b625f8e883678698.
    
    Bug: T193161
    Change-Id: Iab4f7d6bd1f7ca75e07489fb0c05fe17b6de8054

--- modules/ve-cx/dm/ve.dm.CXSectionNode.js
+++ modules/ve-cx/dm/ve.dm.CXSectionNode.js
@@ -13,13 +13,14 @@
 ve.dm.CXSectionNode = function VeDmCXSectionNode() {
 	// Parent constructor
 	ve.dm.CXSectionNode.super.apply( this, arguments );
+
 	// Mixin constructors
 	ve.dm.CXTranslationUnitModel.call( this );
 	ve.dm.CXLintableNode.call( this );
+
 	this.translation = ve.init.target.getTranslation();
-	// Update is triggered by a tree modification. Wait for the whole tree modification
-	// to finish, e.g. if there are relevant internal list changes to wait for.
-	this.connect( this, { update: ve.debounce( this.onUpdate.bind( this ), 0 ) } );
+
+	this.connect( this, { update: this.onUpdate.bind( this ) } );
 };
 
 /* Inheritance */
@@ -59,8 +60,14 @@ ve.dm.CXSectionNode.static.toDomElements = function ( dataElement ) {
 
 ve.dm.CXSectionNode.prototype.onUpdate = function () {
 	if ( this.isTargetSection() ) {
-		this.translation.emit( 'sectionChange', this.getSectionId() );
+		// Update is triggered by a tree modification. Wait for the whole tree modification
+		// to finish, e.g. if there are relevant internal list changes to wait for.
+		setTimeout( this.emitSectionChange.bind( this ) );
 	}
+};
+
+ve.dm.CXSectionNode.prototype.emitSectionChange = function () {
+	this.translation.emit( 'sectionChange', this.getSectionId() );
 };
 
 /**
