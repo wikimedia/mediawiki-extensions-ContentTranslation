78ed06e59defa97cee2171d3ee447d1010a2da76
petarpetkovic <ppetkovic@wikimedia.org>
2018-09-11 15:03:09 +0200

    Use HTTPS for GapFinder link
    Change-Id: I2939b100ca54b3bcb4e385199af1b73297235e4e

--- modules/dashboard/mw.cx.SuggestionList.js
+++ modules/dashboard/mw.cx.SuggestionList.js
@@ -755,7 +755,7 @@ mw.cx.CXSuggestionList.prototype.showEmptySuggestionList = function () {
 			.addClass( 'cx-suggestionlist-empty__recommend' )
 			.append( $( '<a>' )
 				.text( mw.msg( 'cx-suggestionlist-empty-desc-recommend-link-text' ) )
-				.prop( 'href', 'http://recommend.wmflabs.org' )
+				.prop( 'href', 'https://recommend.wmflabs.org' )
 			)
 		);
 
