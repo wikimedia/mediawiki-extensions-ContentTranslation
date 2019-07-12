46936316eae0b0123ec5a8c0c9faa6ebef61a4a4
Ed Sanders <esanders@wikimedia.org>
2019-07-12 14:19:26 +0100

    Fix context inheritance
    Remove duplicated code and replace with fewer overrides.
    
    Remove unused CSS rules.
    
    Change-Id: Ie86a37a76a7524190ccffe9ba3f800ca9cbbe203

--- modules/ve-cx/ui/ve.ui.CXDesktopContext.js
+++ modules/ve-cx/ui/ve.ui.CXDesktopContext.js
@@ -2,34 +2,21 @@
  * CX Context menu and inspectors.
  *
  * @class
- * @extends ve.ui.LinearContext
+ * @extends ve.ui.DesktopContext
  *
  * @constructor
  * @param {ve.ui.CXSurface} surface
+ * @param {Object} [config]
  */
-ve.ui.CXDesktopContext = function VeUiCXDesktopContext( surface ) {
+ve.ui.CXDesktopContext = function VeUiCXDesktopContext() {
 	// Parent constructor
 	ve.ui.CXDesktopContext.super.apply( this, arguments );
 
-	// Properties
-	this.toolsContainer = surface.toolsContainer;
-	this.transitioning = null;
-	this.suppressed = false;
-	this.$window = $( this.getElementWindow() );
-
-	// Events
-	this.surface.getView().connect( this, {
-		relocationStart: 'onSuppress',
-		relocationEnd: 'onUnsuppress',
-		blur: 'onSuppress',
-		focus: 'onUnsuppress'
-	} );
-	this.surface.getModel().connect( this, {
-		select: 'onModelSelect'
-	} );
+	this.popup.$element.remove();
 
 	// Initialization
 	this.$element.addClass( 've-ui-cxDesktopContext' ).append(
+		this.$group,
 		this.inspectors.$element
 			.addClass( 've-ui-cxDesktopContext-inspectors' )
 	);
@@ -37,7 +24,7 @@ ve.ui.CXDesktopContext = function VeUiCXDesktopContext( surface ) {
 
 /* Inheritance */
 
-OO.inheritClass( ve.ui.CXDesktopContext, ve.ui.LinearContext );
+OO.inheritClass( ve.ui.CXDesktopContext, ve.ui.DesktopContext );
 
 /* Methods */
 
@@ -49,107 +36,8 @@ ve.ui.CXDesktopContext.prototype.afterContextChange = function () {
 	ve.ui.CXDesktopContext.super.prototype.afterContextChange.call( this );
 
 	this.emit( 'afterContextChange' );
-
-	// Bypass while dragging
-	if ( this.suppressed ) {
-		return;
-	}
 };
 
-/**
- * Handle context suppression event.
- */
-ve.ui.CXDesktopContext.prototype.onSuppress = function () {
-	this.suppressed = true;
-	if ( this.isVisible() ) {
-		if ( !this.isEmpty() ) {
-			// Change state: menu -> closed
-			this.toggleMenu( false );
-			this.toggle( false );
-		} else if ( this.inspector ) {
-			// Change state: inspector -> closed
-			this.inspector.close();
-		}
-	}
-};
+ve.ui.CXDesktopContext.prototype.updateDimensions = function () {};
 
-/**
- * Handle context unsuppression event.
- */
-ve.ui.CXDesktopContext.prototype.onUnsuppress = function () {
-	this.suppressed = false;
-
-	if ( this.isInspectable() ) {
-		// Change state: closed -> menu
-		this.toggleMenu( true );
-		this.toggle( true );
-	}
-};
-
-/**
- * Handle model select event.
- */
-ve.ui.CXDesktopContext.prototype.onModelSelect = function () {
-	if ( this.isVisible() ) {
-		if ( this.inspector && this.inspector.isOpened() ) {
-			this.inspector.close();
-		}
-	}
-};
-
-/**
- * @inheritdoc
- */
-ve.ui.CXDesktopContext.prototype.createInspectorWindowManager = function () {
-	return new ve.ui.DesktopInspectorWindowManager( this.surface, {
-		factory: ve.ui.windowFactory,
-		overlay: this.surface.getLocalOverlay(),
-		modal: false
-	} );
-};
-
-/**
- * @inheritdoc
- */
-ve.ui.CXDesktopContext.prototype.toggle = function ( show ) {
-	var promise;
-
-	if ( this.transitioning ) {
-		return this.transitioning;
-	}
-	show = show === undefined ? !this.visible : !!show;
-	if ( show === this.visible ) {
-		return $.Deferred().resolve().promise();
-	}
-
-	this.transitioning = $.Deferred();
-	promise = this.transitioning.promise();
-
-	// Parent method
-	ve.ui.CXDesktopContext.super.prototype.toggle.call( this, show );
-
-	this.transitioning.resolve();
-	this.transitioning = null;
-	this.visible = show;
-
-	if ( show ) {
-		if ( this.inspector ) {
-			this.inspector.updateSize();
-		}
-	} else if ( this.inspector ) {
-		this.inspector.close();
-	}
-
-	return promise;
-};
-
-/**
- * @inheritdoc
- */
-ve.ui.CXDesktopContext.prototype.destroy = function () {
-	// Disconnect
-	this.surface.getView().disconnect( this );
-
-	// Parent method
-	return ve.ui.CXDesktopContext.super.prototype.destroy.call( this );
-};
+ve.ui.CXDesktopContext.prototype.setPopupSizeAndPosition = function () {};
