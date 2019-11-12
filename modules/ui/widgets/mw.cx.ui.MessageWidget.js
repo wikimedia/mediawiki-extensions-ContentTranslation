0b3db060c58216453ef3e22d66efe8cf953c8bd7
Ed Sanders <esanders@wikimedia.org>
2019-11-12 22:30:23 +0000

    Have mw.cx.ui.MessageWidget inherit from OO.ui.MessageWidget
    Bug: T225282
    Change-Id: I4ad0b05906498d6de28d93615b8b243e2a16c1f9

--- modules/ui/widgets/mw.cx.ui.MessageWidget.js
+++ modules/ui/widgets/mw.cx.ui.MessageWidget.js
@@ -2,41 +2,23 @@
  * CX Message widget (dismissible banners)
  *
  * @class
- * @extends OO.ui.Widget
- * @mixins OO.ui.mixin.ItemWidget
- * @mixins OO.ui.mixin.LabelElement
- * @mixins OO.ui.mixin.FlaggedElement
- * @mixins OO.ui.mixin.TabIndexedElement
+ * @extends OO.ui.MessageWidget
  *
  * @constructor
  * @param {Object} [config] Configuration options
- * @cfg {string} [type] Message type. One of 'success', 'error' or 'warning'.
  * @cfg {mediawiki.Message|string} [message] Main message.
  * @cfg {mediawiki.Message|string} [details] Additional details.
  * @cfg {OO.ui.ButtonWidget[]} [buttons] Array of additional buttons.
  */
 mw.cx.ui.MessageWidget = function CXMessageWidget( config ) {
-	var iconMap;
 	// Configuration initialization
 	config = config || {};
+
+	config.label = config.label || this.composeMessage( config.message, config.details );
 
 	// Parent constructor
 	mw.cx.ui.MessageWidget.super.call( this, config );
 
-	// Mixin constructors
-	OO.ui.mixin.ItemWidget.call( this );
-	OO.ui.mixin.LabelElement.call( this, {
-		label: this.composeMessage( config.message, config.details )
-	} );
-	OO.ui.mixin.FlaggedElement.call( this, config );
-	iconMap = {
-		success: 'check',
-		error: 'alert',
-		warning: 'notice'
-	};
-	OO.ui.mixin.IconElement.call( this, {
-		icon: iconMap[ config.type ] || config.type
-	} );
 	// Events
 	this.closeButton = new OO.ui.ButtonWidget( {
 		framed: false,
@@ -47,18 +29,15 @@ mw.cx.ui.MessageWidget = function CXMessageWidget( config ) {
 
 	// Initialization
 	this.$element
-		.addClass( 'cx-message-widget cx-message-' + config.type )
-		.append( this.$icon, this.$label, this.closeButton.$element );
+		.addClass( 'cx-message-widget' )
+		.append( this.closeButton.$element );
+
 	this.addButtons( config.buttons );
 };
 
 /* Setup */
 
-OO.inheritClass( mw.cx.ui.MessageWidget, OO.ui.Widget );
-OO.mixinClass( mw.cx.ui.MessageWidget, OO.ui.mixin.ItemWidget );
-OO.mixinClass( mw.cx.ui.MessageWidget, OO.ui.mixin.LabelElement );
-OO.mixinClass( mw.cx.ui.MessageWidget, OO.ui.mixin.IconElement );
-OO.mixinClass( mw.cx.ui.MessageWidget, OO.ui.mixin.FlaggedElement );
+OO.inheritClass( mw.cx.ui.MessageWidget, OO.ui.MessageWidget );
 
 /* Methods */
 
