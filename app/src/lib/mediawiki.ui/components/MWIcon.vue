dcef13cc20b86480bbe717717893c0dbbac3bd56
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-07-28 12:14:21 +0530

    UI Library: Set icon svg viewBox as 0 0 20 20
    SVG Icons in OOUI library are designed with that aspect ratio
    
    Change-Id: I6da66ced2822adfd975348194aec6bcabf1ca5c8

--- app/src/lib/mediawiki.ui/components/MWIcon.vue
+++ app/src/lib/mediawiki.ui/components/MWIcon.vue
@@ -4,7 +4,7 @@
       xmlns="http://www.w3.org/2000/svg"
       :width="width || size"
       :height="height || size"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       :aria-labelledby="iconName"
       aria-hidden="true"
       role="presentation"
