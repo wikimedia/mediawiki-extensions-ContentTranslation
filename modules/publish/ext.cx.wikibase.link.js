74a87184408937bcdb4a27f1f563bbbdff45cf97
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-08-17 11:39:42 +0530

    Publish: Fix broken wikidata linking
    Due to some recent refactoring in Wikibase library(T256054), wikibase.client
    global was removed, causing js error and broken wikidata linking after
    publishing a translation.
    
    wikibase.client.getMwApiForRepo was reimplemented with in CX code as it is
    just 3 lines of code.
    
    How to test:
    
    Find a CX Published article that was not linked to wikidata, open dev tool
    console. Get the required RL modules by
    
      mw.loader.using( ['ext.cx.publish', 'ext.cx.wikibase.link'] )
        .then( () => console.log('done') );
    
      mw.hook( 'mw.cx.translation.published' ).fire(
        'SourceLangCode', 'TargetLangCode', 'SourceTitle', 'TargetTitle' );
    
    Observe the js error. Now to validate the fix:
    
      wikibase.client={};
      getMwApiForRepo=// The function we added in this patch
      wikibase.client.getMwApiForRepo=getMwApiForRepo;
    
    Trigger the wikidata linking using
    
       mw.hook( 'mw.cx.translation.published' ).fire(
         'SourceLangCode', 'TargetLangCode', 'SourceTitle', 'TargetTitle' );
    
    Reload the page and observe links added.
    
    Bug: T249458
    Change-Id: I315eb7bb82ab77a5cde8e4d97aabf04b2e4648a8
    (cherry picked from commit 7d5111b5d0e2ffab3b7080ebab7e5e6ce0c6f828)

--- modules/publish/ext.cx.wikibase.link.js
+++ modules/publish/ext.cx.wikibase.link.js
@@ -9,6 +9,16 @@
 	'use strict';
 
 	/**
+	 * @return {mw.Api}
+	 */
+	function getMwApiForRepo() {
+		var repoConfig = mw.config.get( 'wbRepo' ),
+			repoApiEndpoint = repoConfig.url + repoConfig.scriptPath + '/api.php';
+
+		return wikibase.api.getLocationAgnosticMwApi( repoApiEndpoint );
+	}
+
+	/**
 	 * Link the source and target articles in the Wikibase repo
 	 *
 	 * @param {string} sourceLanguage
@@ -16,7 +26,7 @@
 	 * @param {string} sourceTitle
 	 * @param {string} targetTitle
 	 */
-	var addWikibaseLink = function ( sourceLanguage, targetLanguage, sourceTitle, targetTitle ) {
+	function addWikibaseLink( sourceLanguage, targetLanguage, sourceTitle, targetTitle ) {
 		var title, sourceApi;
 
 		// Link only pages in the main space
@@ -25,7 +35,7 @@
 			return;
 		}
 
-		sourceApi = mw.cx.siteMapper.getApi( mw.cx.sourceLanguage );
+		sourceApi = mw.cx.siteMapper.getApi( sourceLanguage );
 
 		// TODO: Use action=query&meta=wikibase API
 		// that expose siteid as per
@@ -37,7 +47,7 @@
 		} ).done( function ( result ) {
 			var repoApi, targetWikiId, sourceWikiId, pageConnector;
 
-			repoApi = new wikibase.api.RepoApi( wikibase.client.getMwApiForRepo() );
+			repoApi = new wikibase.api.RepoApi( getMwApiForRepo() );
 			targetWikiId = mw.config.get( 'wgWikiID' );
 			sourceWikiId = result.query.general.wikiid;
 
@@ -60,7 +70,7 @@
 				} );
 			} );
 		} );
-	};
+	}
 
 	$( function () {
 		mw.loader.using( [
