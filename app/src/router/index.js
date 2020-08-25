60e692b344b3b047c84a556c476d0432dcba062e
NikG <ngkountas@wikimedia.org>
2020-08-25 16:03:06 +0300

    CX vue routing: Add check if accessible route before redirection
    * Currently a user can visit any route in the workflow as first
    route (through refreshing or url enforcement). This leads to
    broken functionality, since the user should follow specified
    workflow for everything to work correctly.
    * Added a beforeEach route hook in order to redirect user to
    dashboard when user tries to begin the workflow from non-root
    route (dashboard, or 404 page).
    * When user has already started workflow properly but requests
    a redirection to non-subsequent workflow step, a check is
    performed to validate if this redirection is valid.
    * Basically, this check is validating that the previous required
    workflow step has already been visited. This is done by checking
    the difference between step values.
    * Decimal values for workflowStep property are now permitted
    for non-required routes (like SXQuickTutorial)
    
    Bug: T259689
    Change-Id: I83b25d2dca5cbb041f7c651db54b1ef5bc27d60e

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -45,14 +45,14 @@ const routes = [
     name: "sx-quick-tutorial",
     component: SXQuickTutorial,
     params: true,
-    meta: { workflowStep: 4 }
+    meta: { workflowStep: 3.5 }
   },
   {
     path: "/sx/sentence-selector",
     name: "sx-sentence-selector",
     component: SXSentenceSelector,
     params: true,
-    meta: { workflowStep: 5 }
+    meta: { workflowStep: 4 }
   },
   {
     path: "*",
@@ -66,6 +66,37 @@ const routes = [
 const router = new VueRouter({
   mode: "hash",
   routes
+});
+
+/**
+ * Checks before each redirect that redirect is coming
+ * from last required step. When so previous step exists
+ * it redirects user to the beginning of the workflow (dashboard)
+ */
+router.beforeEach((to, from, next) => {
+  const fromStep = from.meta.workflowStep;
+  const toStep = to.meta.workflowStep;
+  if (isNaN(fromStep) && toStep > 0) {
+    next({ name: "dashboard" });
+    return;
+  }
+
+  /**
+   * Optional intermediate steps (e.g. SXQuickTutorial) can have decimal values
+   * to indicate that there are not required for workflow. This way we only need
+   * to check if previous required step (which should have an integer value) has
+   * already been visited
+   */
+  const stepsDifference = Math.ceil(toStep) - Math.floor(fromStep);
+  if (stepsDifference > 1) {
+    const previousRequiredStep = Math.ceil(toStep) - 1;
+    const previousRequiredRoute = routes.find(
+      route => route.meta.workflowStep === previousRequiredStep
+    );
+    next({ name: previousRequiredRoute.name });
+    return;
+  }
+  next();
 });
 
 export default router;
