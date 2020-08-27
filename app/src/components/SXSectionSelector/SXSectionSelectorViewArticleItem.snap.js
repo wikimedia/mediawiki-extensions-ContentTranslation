13c4e0d0362d39a1e954656adea0597e5bc30123
NikG <ngkountas@wikimedia.org>
2020-08-27 13:57:15 +0300

    SXSectionSelector: Add first test for view article item
    Create first unit test for view article item component. Since this
    is a simple component, only match snapshot test was added
    
    Change-Id: Ib76f806194eb7de445895ccc85be06e51eac1257

---
+++ app/src/components/SXSectionSelector/SXSectionSelectorViewArticleItem.snap.js
@@ -0,0 +1,48 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`SXSectionSelector View Article Item Component output matches snapshot for specified props 1`] = `
+<li
+  class="ma-0 row items-center justify-start"
+>
+  <a
+    class="col justify-between py-3 px-4 mw-ui-button mw-ui-button--text"
+    href="my.test.url"
+    target="_blank"
+  >
+    <span
+      class="mw-ui-button__content"
+    >
+      <!---->
+       
+      <span
+        class="mw-ui-button__label"
+      >
+        cx-sx-section-selector-view-article-button-label
+      </span>
+       
+      <span
+        class="notranslate mw-ui-button__indicator mw-ui-icon"
+      >
+        <svg
+          aria-hidden="true"
+          height="12"
+          role="presentation"
+          viewBox="0 0 20 20"
+          width="12"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <!---->
+           
+          <g
+            fill="currentColor"
+          >
+            <path
+              d="M17 17H3V3h5V1H3a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-5h-2z M11 1l3.29 3.29-5.73 5.73 1.42 1.42 5.73-5.73L19 9V1z"
+            />
+          </g>
+        </svg>
+      </span>
+    </span>
+  </a>
+</li>
+`;
---
+++ app/src/components/SXSectionSelector/SXSectionSelectorViewArticleItem.test.js
@@ -0,0 +1,20 @@
+import { mount, createLocalVue } from "@vue/test-utils";
+import VueBananaI18n from "vue-banana-i18n";
+
+const localVue = createLocalVue();
+localVue.use(VueBananaI18n);
+
+import SXSectionSelectorViewArticleItem from "./SXSectionSelectorViewArticleItem";
+
+describe("SXSectionSelector View Article Item", () => {
+  it("Component output matches snapshot for specified props", () => {
+    const wrapper = mount(SXSectionSelectorViewArticleItem, {
+      localVue,
+      propsData: {
+        path: "my.test.url",
+        autonym: "English"
+      }
+    })
+    expect(wrapper.element).toMatchSnapshot();
+  })
+})
