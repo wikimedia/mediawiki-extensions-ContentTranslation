877faaae6f3ececb83ba914039578a0df05cc52e
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-09-15 17:12:29 +0530

    ContentTranslation Skin
    Skin name: contenttranslation
    
    Features:
    * Works without javascript. There is a 2 line javascript but
      progressive enhancement
    * Responsive, mobile friendly
    * Does not have sidebar or usual mediawiki screen components, just
      logo, and personal toolbar menu as a dropdown.
    
    Bug: T263093
    Depends-On: I333f6517dd639bb0e10194b3e641c5498aff9a1b
    Change-Id: Ic26074fbabb3b0906eb94d9bf1beeaf54840cd99

---
+++ skin/SkinContentTranslation.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Skin subclass for Content Translation
+ * @ingroup Skins
+ * @internal
+ */
+class SkinContentTranslation extends SkinMustache {
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct( $options = [] ) {
+		$options['templateDirectory'] = __DIR__ . '/templates';
+		parent::__construct( $options );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getTemplateData() : array {
+		$config = $this->getConfig();
+
+		$skinData = parent::getTemplateData() + [
+			'cx-version' => $config->get( 'ContentTranslationVersion' ),
+			'is-vue' => $config->get( 'ContentTranslationVueDashboard' ),
+			'main-page-href' => Skin::makeMainPageUrl(),
+			'user-name' => $this->getUser()->getName(),
+		] + $this->getNavigationTemplateData();
+
+		return $skinData;
+	}
+
+	/**
+	 * @return array of portlet data for navigation
+	 */
+	private function getNavigationTemplateData() : array {
+		$personalToolsUrls = $this->buildPersonalUrls();
+
+		// Extract some of the items to a separate list.
+		// Rest of them goes to a dropdown menu.
+		$ptoolPrimaryKeys = [ 'uls', 'notifications-alert', 'notifications-notice' ];
+		$personalToolsUrlsPrimary = [];
+		foreach ( $ptoolPrimaryKeys as $key ) {
+			if ( isset( $personalToolsUrls[ $key ] ) ) {
+				$personalToolsUrlsPrimary[ $key ] = $personalToolsUrls[ $key ];
+				unset( $personalToolsUrls[ $key ] );
+			}
+		}
+		return [
+			'data-personal' => $this->getPortletData( 'personal',
+				self::getPersonalToolsForMakeListItem( $personalToolsUrlsPrimary )
+			),
+			'data-personal-dropdown' => $this->getPortletData( 'personal',
+				self::getPersonalToolsForMakeListItem( $personalToolsUrls )
+			)
+		];
+	}
+}
