a895b89056ff7bf8b55a7fb0b85cbe8a1efc1728
NikG <ngkountas@wikimedia.org>
2020-10-11 21:00:56 +0300

    Section Translation: Set z-index for fullscreen class
    Currently, after integration of new contenttranslation skin,
    cx-header elements remain clickable even when not visible,
    hidden below fullscreen elements.
    
    See screenshot: https://imgur.com/a/CLZyaJQ
    
    To fix this issue: Set z-index for fullscreen class. While
    at it, move fullscreen class to App component from
    SXArticleSelector component.
    
    Change-Id: I7115d2172310386514f2847e695ed9ca9650dbc5

--- app/src/views/SXArticleSelector.vue
+++ app/src/views/SXArticleSelector.vue
@@ -29,20 +29,4 @@ export default {
 .sx-article-selector-view {
   background-color: @background-color-base;
 }
-
-// This should be moved to a different place
-.fullscreen {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  // height: 100vh doesn't work well for mobile screen as it leads to cropping of content at the bottom
-  // For browsers supporting -webkit attributes (iOS Safari, Chrome) -webkit-fill-available value fixes
-  // this issue. For the rest browsers height: 100% will be used and it is expected to work well too,
-  // provided that no parent element has height set to less than 100%.
-  // Fix explained here: https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/
-  height: -webkit-fill-available;
-  top: 0;
-  left: 0;
-  overflow-y: auto;
-}
 </style>
