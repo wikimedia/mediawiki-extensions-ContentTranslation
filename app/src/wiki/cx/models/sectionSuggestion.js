21b4cdb3e14495b051ec7430e6af022b1318a47f
NikG <ngkountas@wikimedia.org>
2020-11-02 17:49:01 +0200

    SX sectionSuggestion model: Add section order information
    This patch adds "sourceSections" and "targetSections"
    properties to SectionSuggestion model, which store
    information about section order inside source and target
    page respectively. The information was already existing
    in the suggestion api response but not used until now.
    A method that returns the section order number for a given
    section title is also added
    
    Change-Id: I59fdef8cf2f843c42eb6c6367d2119ba9d541fe8

--- app/src/wiki/cx/models/sectionSuggestion.js
+++ app/src/wiki/cx/models/sectionSuggestion.js
@@ -4,8 +4,25 @@ export default class SectionSuggestion {
     targetLanguage,
     sourceTitle,
     targetTitle,
+    /**
+     * Object that maps section titles in source article
+     * to already existing section titles in target article
+     * @param Object
+     */
     present,
-    missing
+    missing,
+    /**
+     * Array of all section titles in source article
+     * ordered by their order of appearance in the article
+     * @param String[]
+     */
+    sourceSections,
+    /**
+     * Array of all section titles in target article
+     * ordered by their order of appearance in the article
+     * @param String[]
+     */
+    targetSections
   } = {}) {
     this.sourceLanguage = sourceLanguage;
     this.targetLanguage = targetLanguage;
@@ -13,6 +30,8 @@ export default class SectionSuggestion {
     this.targetTitle = targetTitle;
     this.missingSections = missing;
     this.presentSections = present;
+    this.sourceSections = sourceSections;
+    this.targetSections = targetSections;
   }
 
   get missingSectionsCount() {
@@ -21,5 +40,18 @@ export default class SectionSuggestion {
 
   get presentSectionsCount() {
     return Object.keys(this.presentSections || {}).length;
+  }
+
+  /**
+   * For a given section source title it returns the order
+   * of this section inside target page if present or -1 elsewise
+   * @param sectionSourceTitle
+   * @return {Number}
+   */
+  getSectionNumber(sectionSourceTitle) {
+    const sectionTargetTitle = this.presentSections?.[sectionSourceTitle];
+    return this.targetSections.findIndex(
+      sectionTitle => sectionTitle === sectionTargetTitle
+    );
   }
 }
