44b08c6958ddede47395f92c737b32ffaee5c560
NikG <ngkountas@wikimedia.org>
2020-11-05 11:37:01 +0200

    SX PageSection model: Fix translationHtml to return only translated
    Currently section translation preview in "Preview and publish" step
    shows both translated and not translated text for the given section.
    Fix that by modifying pageSection model translationHtml getter to
    return only translated text.
    
    Bug: T259514
    Change-Id: Ie7b1006883436e243939d28efb2cfe4f25f9c6c5

--- app/src/wiki/cx/models/pageSection.js
+++ app/src/wiki/cx/models/pageSection.js
@@ -14,6 +14,7 @@ export default class PageSection {
       [MTProviderGroup.EMPTY_TEXT_PROVIDER_KEY]: ""
     };
     this.translatedTitle = "";
+    /** @type SectionSentence[] **/
     this.sentences = sentences;
   }
 
@@ -36,7 +37,7 @@ export default class PageSection {
 
   get translationHtml() {
     return this.sentences.reduce(
-      (htmlContent, sentence) => htmlContent + sentence.content,
+      (htmlContent, sentence) => htmlContent + sentence.translatedContent,
       ""
     );
   }
