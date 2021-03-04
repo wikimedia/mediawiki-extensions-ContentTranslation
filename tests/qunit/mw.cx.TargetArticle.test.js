e2a21625718a3207588af09107ad18c000d8df31
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-04 15:31:18 +0530

    CX2: Remove pathological transclusions before publishing
    Remove all pathological transclusions if any. Transclusion without any definition can cause
    Parsoid errors and hence failing the whole publishing workflow.
    
    Example:
     <span typeof="mw:Transclusion" data-mw="{}"
        data-cx="[{&quot;adapted&quot;:false}]" id="mwCH0"></span>
    These are not necessarily generated by CX adaptation, but resulted form editing workflow.
    
    Test included.
    
    Bug: T268872
    Change-Id: I50d8c05eddc529d2f259e6ae1d50044aced0402a

--- tests/qunit/mw.cx.TargetArticle.test.js
+++ tests/qunit/mw.cx.TargetArticle.test.js
@@ -18,6 +18,11 @@
 			inputHtml: '<section re="cx:Section"><table id="mwaA"><th id="9"><td id="10">T1<td></th><tr id="10"><td id="10">T1<td></tr></table><section>',
 			output: '<table><tbody><tr><th></th><td>T1</td><td></td></tr><tr><td>T1</td><td></td></tr></tbody></table>',
 			desc: 'Ids from table markup removed'
+		},
+		{
+			inputHtml: '<section rel="cx:Section" id="cxTargetSection112" data-mw-cx-source="Google"><span typeof="mw:Transclusion" data-mw="{}" data-cx="[{&quot;adapted&quot;:false}]" id="mwCH0"></span></section>',
+			output: '',
+			desc: 'Pathological template removed'
 		} ];
 		for ( i = 0; i < tests.length; i++ ) {
 			inputDoc = ve.createDocumentFromHtml( tests[ i ].inputHtml );
