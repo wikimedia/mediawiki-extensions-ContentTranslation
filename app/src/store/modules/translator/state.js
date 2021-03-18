7c91803629c8ea5347f0a275aa0b0dd7d7c0f683
NikG <ngkountas@wikimedia.org>
2021-03-18 13:52:15 +0200

    SX translator vuex state: Remove unused and invalid imports
    state file of translator vuex module has some invalid and unused
    imports, leftovers of previous refactoring, and master is currently
    broken as build doesn't work. Remove them to fix this issue.
    
    Change-Id: I25fa651fe814d078fb44f9eda5d0d7697897044f

--- app/src/store/modules/translator/state.js
+++ app/src/store/modules/translator/state.js
@@ -1,7 +1,3 @@
-import cxTranslatorApi from "../../../wiki/cx/api/translator";
-import PublishResult from "../../../wiki/cx/publishResult";
-import { getTitleForPublishOption } from "../../../utils/publishTitleFactory";
-
 export default {
   username: mw.config.get("wgUserName"),
   /** @type Translation[] */
