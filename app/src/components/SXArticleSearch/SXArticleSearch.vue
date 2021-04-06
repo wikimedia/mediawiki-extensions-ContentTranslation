806d7b143dd098f072562034babe3183a70ef795
NikG <ngkountas@wikimedia.org>
2021-04-06 14:55:13 +0300

    SXArticleSearch: add top search bar
    Add search input on top of the screen (only layout). Actual search
    functionality will be added in followup patch.
    
    Bug: T260141
    Change-Id: If4ae43fb532a3bd53f9b29a4da01f72585c1db4e

--- app/src/components/SXArticleSearch/SXArticleSearch.vue
+++ app/src/components/SXArticleSearch/SXArticleSearch.vue
@@ -1,9 +1,34 @@
 <template>
-  <section class="sx-article-search"></section>
+  <section class="sx-article-search">
+    <mw-input
+      v-model="searchInput"
+      :icon-size="20"
+      :outlined="false"
+      :icon="mwIconSearch"
+      :placeholder="$i18n('cx-sx-article-search-input-placeholder')"
+      type="search"
+      :indicator="mwIconClose"
+      :indicator-size="20"
+      @indicator-clicked="close"
+    />
+  </section>
 </template>
 
 <script>
+import { MwInput } from "@/lib/mediawiki.ui";
+import { mwIconSearch, mwIconClose } from "@/lib/mediawiki.ui/components/icons";
 export default {
-  name: "SxArticleSearch"
+  name: "SxArticleSearch",
+  components: { MwInput },
+  data: () => ({
+    mwIconSearch,
+    mwIconClose,
+    searchInput: ""
+  }),
+  methods: {
+    close() {
+      this.$router.go(-1);
+    }
+  }
 };
 </script>
