3df82380c3a2dde19bec4d7c482d298f0331d59d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-06-07 12:48:45 +0530

    SX Language selector: Keyboard support
    Languages can be selected using up, down, enter keys, without mouse
    
    * For language list used <ul>, <li> tags to make it more semantic
    * Improved the enter key logic to select a valid language from context.
      - select first search result if there is only one result
      - Make sure invalid search query is not selected as language when
        enter key is pressed
    
    Bug: T253303
    Change-Id: I793005fca780c62d6a68e2de69eec7261db0b6c6

--- app/src/components/MWLanguageSelector.vue
+++ app/src/components/MWLanguageSelector.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="mw-ui-language-selector">
+  <div ref="langSelectorContainer" class="mw-ui-language-selector">
     <slot name="search">
       <mw-input
-        ref="selectElement"
+        ref="searchInputElement"
         class="mw-ui-language-selector__search pa-4"
         :icon="mwIconSearch"
         :icon-size="20"
         :placeholder="placeholder"
         @input="onInput"
-        @keydown="onKeydown"
+        @keydown.enter.prevent="onEnter"
+        @keydown.down.prevent="next"
+        @keydown.up.prevent="prev"
+        @keydown.esc.prevent="close"
       />
     </slot>
     <slot v-if="suggestions.length && !searchQuery" name="suggestions">
@@ -17,18 +20,21 @@
           v-i18n:cx-sx-language-selector-suggestions
           class="results-header ps-8 pb-2"
         />
-        <section class="results-languages--suggestions">
-          <span
+        <ul class="results-languages--suggestions pa-0 ma-0">
+          <li
             v-for="language in suggestions"
             :key="language"
             class="language pa-1 ps-8"
             :lang="language"
             :dir="getDir(language)"
-            role="button"
+            :aria-selected="language === selectedLanguage"
+            :class="language === selectedLanguage ? 'language--selected' : ''"
+            tabindex="-1"
+            role="option"
             @click="select(language)"
             v-text="getAutonym(language)"
           />
-        </section>
+        </ul>
       </section>
     </slot>
     <slot v-if="searchResultsByScript.length" name="search">
@@ -38,23 +44,26 @@
           v-i18n:cx-sx-language-selector-all-languages
           class="results-header ps-8 pb-2"
         />
-        <section
+        <ul
           v-for="(chunk, cindex) in searchResultsByScript"
           :key="cindex"
-          class="results-languages mb-6"
+          class="results-languages pa-0 ma-0 mb-6"
           :class="resultsDisplayClass"
         >
-          <span
+          <li
             v-for="language in chunk"
             :key="language"
             class="language pa-1 ps-8"
             :lang="language"
             :dir="getDir(language)"
-            role="button"
+            role="option"
+            :aria-selected="language === selectedLanguage"
+            tabindex="-1"
+            :class="language === selectedLanguage ? 'language--selected' : ''"
             @click="select(language)"
             v-text="getAutonym(language)"
           />
-        </section>
+        </ul>
       </section>
     </slot>
     <slot v-else name="noresults">
@@ -127,6 +136,8 @@ export default {
     mwIconSearch,
     mwIconClose,
     searchQuery: "",
+    selectedLanguage: "",
+    selectedIndex: -1,
     searchResults: []
   }),
   computed: {
@@ -150,7 +161,11 @@ export default {
       if (resultsCount < 10) return "few-results";
       if (resultsCount < 30) return "some-results";
       if (resultsCount >= 30) return "many-results";
-    }
+    },
+    shownLanguages: vm =>
+      !!vm.searchQuery
+        ? vm.searchResults
+        : [...vm.suggestions, ...vm.searchResults]
   },
   watch: {
     /**
@@ -158,11 +173,23 @@ export default {
      */
     searchQuery: async function(query) {
       this.searchResults = await this.search(query);
+    },
+    selectedIndex: function() {
+      if (this.selectedIndex < 0) {
+        // Reset
+        this.selectedLanguage = "";
+
+        return;
+      }
+      this.selectedLanguage = this.shownLanguages[this.selectedIndex];
+      this.$refs.langSelectorContainer
+        .querySelectorAll(`.language[lang="${this.selectedLanguage}"]`)[0]
+        ?.scrollIntoView(false);
     }
   },
   mounted: async function() {
     if (this.autofocus) {
-      this.$refs.selectElement.focus();
+      this.$refs.searchInputElement.focus();
     }
     // Initialize with an empty search
     this.searchResults = await this.search();
@@ -175,19 +202,48 @@ export default {
     },
     onInput(value) {
       this.searchQuery = value;
+      this.selectedIndex = -1;
     },
-    onKeydown(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        // handle enter
-        this.select(event.target.value);
+    onEnter() {
+      // If the search value is a known language, select it
+      if (this.searchQuery && this.languages.includes(this.searchQuery)) {
+        this.select(this.searchQuery);
+
+        return;
       }
 
-      if (event.keyCode === 27) {
-        // handle escape
-        this.$emit("close");
+      // If there is an actively selected language, select it
+      if (this.selectedLanguage) {
+        this.select(this.selectedLanguage);
+
+        return;
+      }
+
+      // If there is only one search result, select it
+      if (this.searchResults.length === 1) {
+        this.select(this.searchResults[0]);
+
+        return;
       }
     },
+    close() {
+      this.$emit("close");
+    },
+    next() {
+      this.selectedIndex++;
+
+      if (this.selectedIndex >= this.shownLanguages.length) {
+        this.selectedIndex = 0;
+      }
+    },
+    prev() {
+      this.selectedIndex--;
+
+      if (this.selectedIndex < 0) {
+        this.selectedIndex = 0;
+      }
+    },
+
     /**
      * @param {string} [query]
      * @return {Promise<string[]>}
@@ -269,15 +325,6 @@ export default {
     padding: 1em;
     overflow-y: auto;
   }
-  &__result {
-    cursor: pointer;
-    &:hover {
-      background-color: @background-color-code;
-    }
-    &-code {
-      color: @color-base--subtle;
-    }
-  }
 
   .no-results,
   .results {
@@ -292,7 +339,9 @@ export default {
     text-align: left;
     .language {
       color: @wmui-color-base10;
-      &:hover {
+      &--selected,
+      &:hover,
+      &:focus {
         background-color: @wmui-color-base80;
       }
     }
