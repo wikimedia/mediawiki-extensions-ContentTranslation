de9c879cca33b76d2c99320127cb6676d6e5fd3d
NikG <ngkountas@wikimedia.org>
2021-09-21 21:45:01 +0300

    SXArticleLanguageSelector: Refactor to use composition api
    Change-Id: Ib7d52004aaafedee6e4f67fbfe0ca64172af0eef

--- app/src/components/SXArticleLanguageSelector.vue
+++ app/src/components/SXArticleLanguageSelector.vue
@@ -9,50 +9,54 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
 import SxTranslationListLanguageSelector from "./CXDashboard/SXTranslationListLanguageSelector";
+import useMediawikiState from "@/composables/useMediawikiState";
+import { computed } from "@vue/composition-api";
 
 export default {
   name: "SxArticleLanguageSelector",
   components: {
     SxTranslationListLanguageSelector
   },
-  computed: {
-    ...mapState({
-      supportedLanguageCodes: state =>
-        state.mediawiki.supportedLanguageCodes || [],
-      sourceLanguage: state => state.application.sourceLanguage,
-      targetLanguage: state => state.application.targetLanguage
-    }),
-    ...mapGetters({
-      currentLanguageTitleGroup: "application/getCurrentLanguageTitleGroup"
-    }),
+  setup(props, context) {
+    const { supportedLanguageCodes } = useMediawikiState();
+    const store = context.root.$store;
+
+    const currentLanguageTitleGroup = computed(
+      () => store.getters["application/getCurrentLanguageTitleGroup"]
+    );
+
     // titles are provided in the following format: { lang: "en", title: "Animal" }
     // so title.lang contains language code
-    availableSourceLanguages: vm =>
-      vm.currentLanguageTitleGroup?.titles.map(title => title.lang) || [],
-
+    const availableSourceLanguages = computed(
+      () =>
+        currentLanguageTitleGroup.value?.titles.map(title => title.lang) || []
+    );
     /**
      * If SectionTranslationTargetLanguages configuration parameter is set,
      * target language selection is limited to these languages
      *
      * @return {Object[]} - Array of available target language options
      */
-    targetLanguages: vm => {
+    const targetLanguages = computed(() => {
       const mwTargetLanguages = mw.config.get(
         "wgSectionTranslationTargetLanguages"
       );
 
-      return mwTargetLanguages || vm.supportedLanguageCodes;
-    }
-  },
-  methods: {
-    onSourceLanguageSelected(sourceLanguage) {
-      this.$store.dispatch("application/updateSourceLanguage", sourceLanguage);
-    },
-    onTargetLanguageSelected(targetLanguage) {
-      this.$store.dispatch("application/updateTargetLanguage", targetLanguage);
-    }
+      return mwTargetLanguages || supportedLanguageCodes.value;
+    });
+
+    const onSourceLanguageSelected = sourceLanguage =>
+      store.dispatch("application/updateSourceLanguage", sourceLanguage);
+    const onTargetLanguageSelected = targetLanguage =>
+      store.dispatch("application/updateTargetLanguage", targetLanguage);
+
+    return {
+      availableSourceLanguages,
+      targetLanguages,
+      onSourceLanguageSelected,
+      onTargetLanguageSelected
+    };
   }
 };
 </script>
