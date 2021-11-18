9e0b61c891724e46d929604f2e335fa3ace18d7d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-11-18 11:13:53 +0530

    MWDivider: Avoid thick line because of double border
    Currently the divider height is 2px because of top and bottom margins.
    
    Bug: T287870
    Change-Id: I1e7a02897d320e32d7785bb869dce04cb89dd5cc

--- app/src/lib/mediawiki.ui/components/MWDivider/MWDivider.vue
+++ app/src/lib/mediawiki.ui/components/MWDivider/MWDivider.vue
@@ -6,7 +6,7 @@
 @import "../../variables/wikimedia-ui-base.less";
 
 .mw-ui-divider {
-  border: @border-width-base @border-style-base @border-color-base;
+  border-top: @border-width-base @border-style-base @border-color-base;
   margin: 0 !important;
   padding: 0 !important;
 }
