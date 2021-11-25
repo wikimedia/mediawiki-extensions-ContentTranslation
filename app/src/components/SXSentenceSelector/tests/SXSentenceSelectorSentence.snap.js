e66c9dcb7fe6f1a329f5493dff3af31b0fe80ddf
NikG <ngkountas@wikimedia.org>
2021-11-25 17:03:10 +0200

    SX: Remove SXSentenceSelectorSentence SFC and tests
    SXSentenceSelectorSentence SFC is a leftover from previous
    implementation and is no longer used. Remove it with its
    related tests.
    
    Change-Id: I5692c0d7de226b25a9c9ac8a4ba639e26e359da6

--- app/src/components/SXSentenceSelector/tests/SXSentenceSelectorSentence.snap.js
+++
@@ -1,9 +0,0 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`SXSentenceSelector Sentence Component output matches snapshot 1`] = `
-<span
-  class="sx-sentence-selector__section-sentence py-1 me-1"
->
-  Test content
-</span>
-`;
