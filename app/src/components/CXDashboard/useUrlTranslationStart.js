490a627ad08b2e5edcde831b03aba0bb80569046
NikG <ngkountas@wikimedia.org>
2022-02-08 11:02:53 +0200

    SX: Instrumentation support for all entrypoints
    SX application properly supports the logging for the "dashboard_open"
    event, with the "event_source" parameter indicating whether the user
    is coming to SX from an entrypoint to the application. However, until
    now only events for "mflanguagesearcher" were logged.
    
    Fix this by adding a switch statement to also support the rest of the
    entrypoints.
    
    Bug: T301222
    Change-Id: I6698b72aa1a444caaaeac599f36eb593c90cbb14

--- app/src/components/CXDashboard/useUrlTranslationStart.js
+++ app/src/components/CXDashboard/useUrlTranslationStart.js
@@ -1,18 +1,25 @@
 import startSectionTranslation from "@/composables/useSectionTranslationStart";
 import { logEvent } from "@/plugins/eventlogging";
 
+/**
+ * @return {string|undefined} the event source based on the "campaign" URL param
+ */
 const getEventSourceFromUrlCampaign = () => {
+  const campaignEventSourcesMap = {
+    mflanguagesearcher: "content_language_selector",
+    mfrecenttranslation: "recent_translation",
+    mfrecentedit: "recent_edit",
+    mffrequentlanguages: "frequent_languages"
+  };
   const urlParams = new URLSearchParams(location.search);
   const campaign = urlParams.get("campaign");
 
-  if (campaign === "mflanguagesearcher") {
-    return "content_language_selector";
-  }
-
-  return null;
+  return campaignEventSourcesMap[campaign];
 };
 
 const startSectionTranslationFromUrl = pageTitle => {
+  // If no campaign exists inside the URL, then the user
+  // have preselected the page title inside the URL himself
   const eventSource = getEventSourceFromUrlCampaign() || "direct_preselect";
   logEvent({
     event_type: "dashboard_open",
