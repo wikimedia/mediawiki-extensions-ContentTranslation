wikimedia/mediawiki-extensions-ContentTranslation

commit 525f8595011d911d456bd656dd57978c0a0abd5d

CX: Rename Database class to LoadBalancer and register it as service
According to the related Mediawiki documentation, the service locator
pattern should be avoided. When a service needs access to another
service, it should ask for it in the constructor (actual dependency
injection). Documentation here:
https://www.mediawiki.org/wiki/Dependency_Injection

The ContentTranslation/Database class is a static factory using such
a hidden dependency on the load balancer factory. In order to follow
these best practices and use actual dependency injection, we should
refactor it to accept this dependency in the constructor and properly
register it as a custom service inside ServiceWiring.php file. This
way the Database service can be injected in the constructor of other
classes that need it.

This patch renames the Database class to LoadBalancer and refactors
it to follow these practices and replaces all occurrences of the
class usage with the updated syntax.

Disclaimer: The service locator pattern is present in several places
throughout ContentTranslation extension. The target of this patch is
not to eliminate all these cases, but only refactors this specific
class, so that we can start removing such bad practices step-by-step.

Change-Id: Icdd0f8d1c081101142262a68cf11dccd1941f411

includes/Translation.php
@@ -18,7 +18,8 @@ class Translation {
 	}
 
 	public function create( Translator $translator ) {
-		$dbw = Database::getConnection( DB_PRIMARY );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbw = $lb->getConnection( DB_PRIMARY );
 
 		$table = 'cx_translations';
 
@@ -62,7 +63,8 @@ class Translation {
 	 * @param Translator $translator
 	 */
 	public function update( ?array $options, Translator $translator ) {
-		$dbw = Database::getConnection( DB_PRIMARY );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbw = $lb->getConnection( DB_PRIMARY );
 
 		$table = 'cx_translations';
 
@@ -148,7 +150,8 @@ class Translation {
 			return null;
 		}
 
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 		$values = [
 			'translation_source_language' => $sourceLanguage,
 			'translation_target_language' => $targetLanguage,
@@ -190,7 +193,9 @@ class Translation {
 	 * @return Translation|null
 	 */
 	public static function findForTranslator( TranslationWork $work, Translator $translator ) {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
+
 		$values = [
 			'translation_source_language' => $work->getSourceLanguage(),
 			'translation_target_language' => $work->getTargetLanguage(),
@@ -212,7 +217,9 @@ class Translation {
 	 * @return Translation|null
 	 */
 	public static function findByPublishedTitle( string $publishedTitle, string $targetLanguage ): ?Translation {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
+
 		$conditions = [
 			'translation_target_language' => $targetLanguage,
 			'translation_target_title' => $publishedTitle
@@ -253,7 +260,8 @@ class Translation {
 	}
 
 	public static function delete( $translationId ) {
-		$dbw = Database::getConnection( DB_PRIMARY );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbw = $lb->getConnection( DB_PRIMARY );
 
 		$dbw->update(
 			'cx_translations',
@@ -279,7 +287,8 @@ class Translation {
 	 * @return array
 	 */
 	public static function getDraftStats() {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 
 		$rows = $dbr->select(
 			'cx_translations',
@@ -319,7 +328,8 @@ class Translation {
 	 * @return array
 	 */
 	public static function getPublishedStats() {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 
 		$rows = $dbr->select(
 			'cx_translations',
@@ -356,7 +366,8 @@ class Translation {
 	 * @return array<string,array>
 	 */
 	public static function getDeletionTrend( $interval ): array {
-		$dbr = wfGetDB( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 
 		$conditions = [
 			'ar_rev_id = ct_rev_id'
@@ -422,7 +433,8 @@ class Translation {
 	public static function getTrendByStatus(
 		$source, $target, $status, $interval, $translatorId
 	) {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 
 		$conditions = [];
 		if ( $status === 'published' ) {
@@ -517,7 +529,8 @@ class Translation {
 	 * @return array
 	 */
 	public static function getAllPublishedTranslations( $from, $to, $limit, $offset ) {
-		$dbr = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$dbr = $lb->getConnection( DB_REPLICA );
 		$conditions = [];
 		$conditions[] = self::getPublishedCondition( $dbr );
 

api/ApiContentTranslationSave.php
@@ -9,7 +9,6 @@ namespace ContentTranslation\ActionApi;
 use ApiBase;
 use ContentTranslation\AbuseFilterCheck;
 use ContentTranslation\CategoriesStorageManager;
-use ContentTranslation\Database;
 use ContentTranslation\RestbaseClient;
 use ContentTranslation\SiteMapper;
 use ContentTranslation\Translation;
@@ -21,6 +20,7 @@ use Deflate;
 use Exception;
 use FormatJson;
 use Language;
+use MediaWiki\MediaWikiServices;
 
 class ApiContentTranslationSave extends ApiBase {
 	/**
@@ -31,7 +31,8 @@ class ApiContentTranslationSave extends ApiBase {
 	public function execute() {
 		$params = $this->extractRequestParams();
 
-		if ( Database::getConnection( DB_PRIMARY )->isReadOnly() ) {
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		if ( $lb->getConnection( DB_PRIMARY )->isReadOnly() ) {
 			$this->dieReadOnly();
 		}
 

api/ApiQueryContentTranslation.php
@@ -13,7 +13,6 @@ use ApiPageSet;
 use ApiQuery;
 use ApiQueryGeneratorBase;
 use ContentTranslation\CorporaLookup;
-use ContentTranslation\Database;
 use ContentTranslation\Translation;
 use ContentTranslation\TranslationWork;
 use ContentTranslation\Translator;
@@ -195,7 +194,8 @@ class ApiQueryContentTranslation extends ApiQueryGeneratorBase {
 	 * @return array Associative array with 'translationUnits' and 'categories' data
 	 */
 	protected function getTranslationUnitsAndCategories( Translation $translation ) {
-		$db = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$db = $lb->getConnection( DB_REPLICA );
 
 		$lookup = new CorporaLookup( $db );
 		return $lookup->getByTranslationId( $translation->getTranslationId() );

api/ApiQueryContentTranslationCorpora.php
@@ -11,7 +11,7 @@ namespace ContentTranslation\ActionApi;
 use ApiBase;
 use ApiQueryBase;
 use ContentTranslation\CorporaLookup;
-use ContentTranslation\Database;
+use MediaWiki\MediaWikiServices;
 use Sanitizer;
 
 /**
@@ -28,7 +28,9 @@ class ApiQueryContentTranslationCorpora extends ApiQueryBase {
 		$params = $this->extractRequestParams();
 		$result = $this->getResult();
 
-		$db = Database::getConnection( DB_REPLICA );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$db = $lb->getConnection( DB_REPLICA );
+
 		$lookup = new CorporaLookup( $db );
 		$data = $lookup->getByTranslationId( $params['translationid'] );
 		$sections = $data[ 'sections' ];

includes/CategoriesStorageManager.php
@@ -10,6 +10,7 @@
 
 namespace ContentTranslation;
 
+use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\IDatabase;
 
 class CategoriesStorageManager {
@@ -96,7 +97,8 @@ class CategoriesStorageManager {
 	public static function save(
 		$translationId, $sourceCategories, $targetCategories, $newTranslation
 	) {
-		$db = Database::getConnection( DB_PRIMARY );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$db = $lb->getConnection( DB_PRIMARY );
 
 		$db->doAtomicSection(
 			__METHOD__,
@@ -126,7 +128,8 @@ class CategoriesStorageManager {
 	 * @return bool
 	 */
 	private static function exists( $translationId ) {
-		$db = Database::getConnection( DB_PRIMARY );
+		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
+		$db = $lb->getConnection( DB_PRIMARY );
 
 		$conditions = [ 'cxc_translation_id' => $translationId ] + self::$CATEGORIES_SECTION;
 

includes/Database.php
@@ -1,27 +1,0 @@
-<?php
-
-namespace ContentTranslation;
-
-use MediaWiki\MediaWikiServices;
-
-/**
- * ContentTranslation Database Connection abstraction
- */
-class Database {
-	/**
-	 * Gets a database connection to the ContentTranslation database
-	 * @param int $type Either DB_REPLICA or DB_PRIMARY
-	 * @return \Wikimedia\Rdbms\IDatabase
-	 */
-	public static function getConnection( $type ) {
-		global $wgContentTranslationDatabase, $wgContentTranslationCluster;
-
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		$lb = $wgContentTranslationCluster
-			? $lbFactory->getExternalLB( $wgContentTranslationCluster )
-			: $lbFactory->getMainLB( $wgContentTranslationDatabase );
-
-		return $lb->getConnectionRef( $type, [], $wgContentTranslationDatabase );
-	}
-
-}

includes/LoadBalancer.php
@@ -1,0 +1,58 @@
+<?php
+
+namespace ContentTranslation;
+
+use MediaWiki\Config\ServiceOptions;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\LBFactory;
+
+/**
+ * ContentTranslation Database Connection abstraction
+ */
+class LoadBalancer {
+
+	/** @var LBFactory */
+	private $lbFactory;
+
+	/**
+	 * The Database domain ID of the relevant wiki or false for the local wiki
+	 * @var string|false
+	 */
+	private $contentTranslationDatabase;
+
+	/**
+	 * The external Database cluster name where the database lives or false if not exists
+	 * @var string|false
+	 */
+	private $contentTranslationCluster;
+
+	/**
+	 * @internal For use by ServiceWiring
+	 */
+	public const CONSTRUCTOR_OPTIONS = [
+		'ContentTranslationDatabase',
+		'ContentTranslationCluster',
+	];
+
+	public function __construct( LBFactory $lbFactory, ServiceOptions $options ) {
+		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
+
+		$this->lbFactory = $lbFactory;
+		$this->contentTranslationDatabase = $options->get( 'ContentTranslationDatabase' );
+		$this->contentTranslationCluster = $options->get( 'ContentTranslationCluster' );
+	}
+
+	/**
+	 * Gets a database connection to the ContentTranslation database
+	 * @param int $type Either DB_REPLICA or DB_PRIMARY
+	 * @return IDatabase
+	 */
+	public function getConnection( int $type ): IDatabase {
+		$lb = $this->contentTranslationCluster
+			? $this->lbFactory->getExternalLB( $this->contentTranslationCluster )
+			: $this->lbFactory->getMainLB( $this->contentTranslationDatabase );
+
+		return $lb->getConnectionRef( $type, [], $this->contentTranslationDatabase );
+	}
+
+}

includes/ServiceWiring.php
@@ -1,0 +1,18 @@
+<?php
+
+declare( strict_types=1 );
+
+use ContentTranslation\LoadBalancer;
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\MediaWikiServices;
+
+/** @phpcs-require-sorted-array */
+return [
+	'ContentTranslation.LoadBalancer' =>
+		static function ( MediaWikiServices $services ): LoadBalancer {
+			return new LoadBalancer(
+				$services->getDBLoadBalancerFactory(),
+				new ServiceOptions( LoadBalancer::CONSTRUCTOR_OPTIONS, $services->getMainConfig() )
+			);
+		},
+];

commit 44050d363467ffea80141f8c3156686f66d9fcbb

Fix: contenttranslationlangtrend API fails with Internal DBQueryError
Regression from 525f8595011d911d456bd656dd57978c0a0abd5d

Translation::getDeletionTrend method uses mediawiki database and not
the shared CX database.

Bug: T302700
Change-Id: I10b57dfd84f8227200cbbd191e359e66866c34dd

includes/Translation.php
@@ -366,8 +366,7 @@ class Translation {
 	 * @return array<string,array>
 	 */
 	public static function getDeletionTrend( $interval ): array {
-		$lb = MediaWikiServices::getInstance()->getService( 'ContentTranslation.LoadBalancer' );
-		$dbr = $lb->getConnection( DB_REPLICA );
+		$dbr = wfGetDB( DB_REPLICA );
 
 		$conditions = [
 			'ar_rev_id = ct_rev_id'