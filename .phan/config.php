f3d2ea72a0763307cb882aa98ad521f83fe131fb
Timo Tijhof <krinkle@fastmail.com>
2022-03-05 16:55:14 +0000

    Move api and specials into PSR-4 directory structure
    The classes were already named this way. This change only removes a
    few redundant entries in the extension.json file, and moves the
    classes in a subdirectory matching their actual namespaced name.
    
    Change-Id: Ibb205231e8dff3a1255ac0382aa22d23dc00b02c

--- .phan/config.php
+++ .phan/config.php
@@ -5,8 +5,6 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
-		'api/',
-		'specials/',
 		'scripts/',
 		'includes/',
 		'skin/',
