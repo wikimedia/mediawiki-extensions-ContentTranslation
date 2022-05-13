1ddbc7b60db566b2d088f7e803c6f1df1586353f
Timo Tijhof <krinkle@fastmail.com>
2022-05-13 20:10:22 +0100

    tests: Simplify with built-in test.each() and assert.rejects()
    * Remove ad-hoc loops and inline assertion messages or inline
      comments, in favour of setting test case names in QUnit.test.each(),
      which provides more detailed test results, and more granular
      Re-run features.
    
    * Remove manual tracking of failed deferreds and async stacks
      by returning the promise to QUnit.test() callback for those
      that should resolve, or by using the `assert.rejects()`.
    
    Change-Id: I740164426b49c65334540dc73bf4244c99e5b79a

--- tests/qunit/mw.cx.MachineTranslationService.test.js
+++ tests/qunit/mw.cx.MachineTranslationService.test.js
@@ -8,125 +8,86 @@ QUnit.module( 'mw.cx.MachineTranslationService', QUnit.newMwEnvironment( {
 	}
 } ) );
 
-QUnit.test( 'fetchProviders', function ( assert ) {
-	var
-		done1 = assert.async(),
-		done2 = assert.async(),
-		done3 = assert.async();
-
-	// Success with results
+QUnit.test( 'fetchProviders [Success with results]', function ( assert ) {
 	this.server.respondWith( /list\/mt\/es\/ca/, [
 		200,
 		{ 'Content-Type': 'application/json' },
 		'{ "mt": [ "Provider1", "Provider2" ] }'
 	] );
-	new mw.cx.MachineTranslationService( 'es', 'ca', this.siteMapper )
-		.fetchProviders()
-		.done( function ( providers ) {
-			assert.deepEqual( providers, [ 'Provider1', 'Provider2' ], 'Correct providers are returned' );
-		} )
-		.fail( function () {
-			assert.true( false, 'Unexpected error' );
-		} )
-		.always( function () {
-			done1();
-		} );
 
-	// Success without results
+	return new mw.cx.MachineTranslationService( 'es', 'ca', this.siteMapper )
+		.fetchProviders()
+		.then( function ( providers ) {
+			assert.deepEqual( providers, [ 'Provider1', 'Provider2' ], 'Correct providers are returned' );
+		} );
+} );
+
+QUnit.test( 'fetchProviders [Success without results]', function ( assert ) {
 	this.server.respondWith( /list\/mt\/se\/ja/, [
 		200,
 		{ 'Content-Type': 'application/json' },
 		'{}'
 	] );
-	new mw.cx.MachineTranslationService( 'se', 'ja', this.siteMapper )
-		.fetchProviders()
-		.done( function ( providers ) {
-			assert.deepEqual( providers, [], 'Case of no providers is handled.' );
-		} )
-		.fail( function () {
-			assert.true( false, 'Unexpected error' );
-		} )
-		.always( function () {
-			done2();
-		} );
 
-	// Service is down
+	return new mw.cx.MachineTranslationService( 'se', 'ja', this.siteMapper )
+		.fetchProviders()
+		.then( function ( providers ) {
+			assert.deepEqual( providers, [], 'Case of no providers is handled.' );
+		} );
+} );
+
+QUnit.test( 'fetchProviders [Service is down]', function ( assert ) {
 	this.server.respondWith( /list\/fi\/sv\/ca/, [
 		500,
 		{ 'Content-Type': 'text/html' },
 		'Temporary failure'
 	] );
 
-	new mw.cx.MachineTranslationService( 'fi', 'sv', this.siteMapper )
-		.fetchProviders()
-		.done( function () {
-			assert.true( false, 'Unexpected success' );
-		} )
-		.fail( function () {
-			assert.true( true, 'Failure causes promise to be rejected' );
-		} )
-		.always( function () {
-			done3();
-		} );
+	assert.rejects(
+		new mw.cx.MachineTranslationService( 'fi', 'sv', this.siteMapper )
+			.fetchProviders(),
+		'Failure causes promise to be rejected'
+	);
 } );
 
-QUnit.test( 'getSuggestedDefaultProvider', function ( assert ) {
-	var
-		done1 = assert.async(),
-		done2 = assert.async(),
-		done3 = assert.async();
-
-	// Success with results
+QUnit.test( 'getSuggestedDefaultProvider [Success with results]', function ( assert ) {
 	this.server.respondWith( /list\/mt\/source\/target/, [
 		200,
 		{ 'Content-Type': 'application/json' },
 		'{ "mt": [ "Provider1", "Provider2" ] }'
 	] );
-	new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
-		.getSuggestedDefaultProvider()
-		.done( function ( provider ) {
-			assert.strictEqual( provider, 'Provider1', 'The first provider is suggested.' );
-		} )
-		.fail( function () {
-			assert.true( false, 'Unexpected error' );
-		} )
-		.always( function () {
-			done1();
-		} );
 
-	// Success without results
+	return new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
+		.getSuggestedDefaultProvider()
+		.then( function ( provider ) {
+			assert.strictEqual( provider, 'Provider1', 'The first provider is suggested.' );
+		} );
+} );
+
+QUnit.test( 'getSuggestedDefaultProvider [Success without results]', function ( assert ) {
 	this.server.respondWith( /list\/mt\/source\/target/, [
 		200,
 		{ 'Content-Type': 'application/json' },
 		'{}'
 	] );
-	new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
-		.getSuggestedDefaultProvider()
-		.done( function ( provider ) {
-			assert.strictEqual( provider, null, 'If no providers, no suggested provider.' );
-		} )
-		.fail( function () {
-			assert.true( false, 'Unexpected error' );
-		} )
-		.always( function () {
-			done2();
-		} );
 
-	// Success without source-mt in result
+	return new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
+		.getSuggestedDefaultProvider()
+		.then( function ( provider ) {
+			assert.strictEqual( provider, null, 'If no providers, no suggested provider.' );
+		} );
+} );
+
+QUnit.test( 'getSuggestedDefaultProvider [Success without source-mt in result]', function ( assert ) {
 	this.server.respondWith( /list\/mt\/source\/target/, [
 		200,
 		{ 'Content-Type': 'application/json' },
 		'{ "mt": [ "source-mt", "Provider1", "Provider2" ] }'
 	] );
-	new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
+
+	return new mw.cx.MachineTranslationService( 'source', 'target', this.siteMapper )
 		.getSuggestedDefaultProvider()
-		.done( function ( provider ) {
+		.then( function ( provider ) {
 			assert.strictEqual( provider, null, 'Source mt suggested by the server respected' );
-		} )
-		.fail( function () {
-			assert.true( false, 'Unexpected error' );
-		} )
-		.always( function () {
-			done3();
 		} );
 } );
--- tests/qunit/mw.cx.util.test.js
+++ tests/qunit/mw.cx.util.test.js
@@ -4,63 +4,50 @@ QUnit.module( 'mw.cx.util', QUnit.newMwEnvironment( {
 	}
 } ) );
 
-/* Tests */
-QUnit.test( 'Title evaluation with namespace change', function ( assert ) {
-	var tests, i;
-
-	tests = [
-		{
-			title: 'Oxygen',
-			namespace: 0,
-			targetTitle: 'Oxygen',
-			description: 'Main namespace to main namespace'
-		},
-		{
-			title: 'Oxygen',
-			namespace: 2,
-			targetTitle: 'User:TestUser/Oxygen',
-			description: 'User namespace.'
-		},
-		{
-			title: 'User:TestUser/Oxygen',
-			namespace: 0,
-			targetTitle: 'Oxygen',
-			description: 'User namespace to main'
-		},
-		{
-			title: 'User:TestUser/Oxygen',
-			namespace: 1,
-			targetTitle: 'Talk:Oxygen',
-			description: 'User namespace to Talk namespace'
-		},
-		{
-			title: 'User:TestUser/Oxygen',
-			namespace: 2,
-			targetTitle: 'User:TestUser/Oxygen',
-			description: 'User namespace to User namespace'
-		},
-		{
-			title: 'Talk:Oxygen',
-			namespace: 2,
-			targetTitle: 'User:TestUser/Oxygen',
-			description: 'Talk namespace to User namespace'
-		},
-		{
-			title: 'Talk:Oxygen',
-			namespace: 1,
-			targetTitle: 'Talk:Oxygen',
-			description: 'Talk namespace to Talk namespace'
-		}
-	];
-
-	for ( i = 0; i < tests.length; i++ ) {
-		assert.deepEqual(
-			mw.cx.getTitleForNamespace( tests[ i ].title, tests[ i ].namespace ),
-			tests[ i ].targetTitle,
-			tests[ i ].description
-		);
+QUnit.test.each( '.getTitleForNamespace()', {
+	'Main namespace to main namespace': {
+		title: 'Oxygen',
+		namespace: 0,
+		targetTitle: 'Oxygen'
+	},
+	'User namespace.': {
+		title: 'Oxygen',
+		namespace: 2,
+		targetTitle: 'User:TestUser/Oxygen'
+	},
+	'User namespace to main': {
+		title: 'User:TestUser/Oxygen',
+		namespace: 0,
+		targetTitle: 'Oxygen'
+	},
+	'User namespace to Talk namespace': {
+		title: 'User:TestUser/Oxygen',
+		namespace: 1,
+		targetTitle: 'Talk:Oxygen'
+	},
+	'User namespace to User namespace': {
+		title: 'User:TestUser/Oxygen',
+		namespace: 2,
+		targetTitle: 'User:TestUser/Oxygen'
+	},
+	'Talk namespace to User namespace': {
+		title: 'Talk:Oxygen',
+		namespace: 2,
+		targetTitle: 'User:TestUser/Oxygen'
+	},
+	'Talk namespace to Talk namespace': {
+		title: 'Talk:Oxygen',
+		namespace: 1,
+		targetTitle: 'Talk:Oxygen'
 	}
+}, function ( assert, data ) {
+	assert.deepEqual(
+		mw.cx.getTitleForNamespace( data.title, data.namespace ),
+		data.targetTitle
+	);
+} );
 
+QUnit.test( '.getTitleForNamespace() [invalid]', function ( assert ) {
 	assert.throws(
 		mw.cx.getTitleForNamespace.bind( this, '::', 0 ),
 		'Error thrown for invalid title'
