5cdf8c64062027ff528f808014243514cd02887b
NikG <ngkountas@wikimedia.org>
2022-10-10 12:18:18 +0300

    SX UI lib: Export colors to be available without injecting a plugin
    Change-Id: I49d04687f9d952134e667270a4c9e3a9dd01e8c3

--- app/src/lib/mediawiki.ui/plugins/colors.js
+++ app/src/lib/mediawiki.ui/plugins/colors.js
@@ -1,15 +1,5 @@
 // Copied from wikimedia-ui-base.less
-const colors = {
-  base10: "#202122",
-  base30: "#72777d",
-  base80: "#eaecf0",
-  green30: "#14866d",
-  green50: "#00af89",
-  red50: "#d33",
-  yellow30: "#ac6600",
-  yellow50: "#fc3",
-  primary: "#36c",
-};
+import colors from "../variables/colors";
 
 export default {
   install: (app) => {
---
+++ app/src/lib/mediawiki.ui/variables/colors.js
@@ -0,0 +1,12 @@
+export default {
+  base10: "#202122",
+  base30: "#72777d",
+  base70: "#c8ccd1",
+  base80: "#eaecf0",
+  green30: "#14866d",
+  green50: "#00af89",
+  red50: "#d33",
+  yellow30: "#ac6600",
+  yellow50: "#fc3",
+  primary: "#36c",
+};
