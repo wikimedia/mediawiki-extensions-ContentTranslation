cc398660eb608038a2cc17b51d40189a53cffaaa
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-02-01 10:29:47 +0530

    Make link parsing agnostic to baseURI, wgScript, wgArticlePath
    A recent change in VE link parsing started using baseURI for link parsing.
    
    The content provided by cxserver, whether it is article or machine
    translated sections, has links with href="Title". But when this `href`
    is parsed in the context of baseURI, it creates different URLs depending
    on baseURI, wgScript, wgArticlePath.
    For example, in a wiki where wgArticlePath is /wiki/$1, but baseURI based
    on current url is /w/index.php?title=Title, the `href` must be /wiki/Title
    for a valid URL. But cxserver cannot know this. And this can also happen
    in local development wiki which follows different wgArticlePath than a production
    wiki.
    To complicate this further, CX always works with two language wikis.
    
    For above mentioned factors, a valid internal mediawiki link may not be
    recognized as internal link. This will result cxLink nodes throwing errors.
    
    To fix this, have a defensive logic to construct the dataElement independent
    of baseURI, wgScript, wgArticlePath.
    
    Bug: T328143
    Change-Id: I945897a27db479986855002b389034a745bf9bef

--- modules/ve-cx/dm/ve.dm.CXLinkAnnotation.js
+++ modules/ve-cx/dm/ve.dm.CXLinkAnnotation.js
@@ -26,6 +26,20 @@ ve.dm.CXLinkAnnotation.static.matchFunction = function ( domElement ) {
 ve.dm.CXLinkAnnotation.static.toDataElement = function ( domElements, converter ) {
 	var dataCX,
 		dataElement = ve.dm.CXLinkAnnotation.super.static.toDataElement.call( this, domElements, converter );
+
+	if ( !dataElement ) {
+		// ve.dm.CXLinkAnnotation.super.static.toDataElement has assumptions about document.baseURI.
+		// baseURI can use URL patterns like /wiki/$1 or w/index.php?title=$1. It also uses
+		// current wikis 'wgScript', 'wgArticlePath' configuration values which can
+		// totally be different when running on a local dev wiki.
+		// Because of this dataElement can be null as toDataElement fails to parse an internal link
+		// So make this dataElement calculation agnostic of all of the above mentioned factors.
+		dataElement = ve.dm.CXLinkAnnotation.super.static.dataElementFromTitle.call(
+			this,
+			mw.Title.newFromText( domElements[ 0 ].getAttribute( 'title' ) )
+		);
+	}
+
 	dataElement.attributes.linkid = domElements[ 0 ].getAttribute( 'data-linkid' );
 
 	dataCX = domElements[ 0 ].getAttribute( 'data-cx' );
