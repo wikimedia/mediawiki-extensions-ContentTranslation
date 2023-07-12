083c7bcd7c83e6bb9941eaa0740fb79d4c6ffeb6
NikG <ngkountas@wikimedia.org>
2023-07-12 15:50:54 +0300

    SX: Scroll to selected segment when returning from mobile editor
    Currently, when user comes back to "Pick a sentence" step after
    editing the proposed translation, the contents are not scrolled
    at all, even when the selected translation segment is outside
    the viewport. This issue is caused because the watcher that is
    called when the selected translation unit is updated, and scrolls
    to the newly selected translation unit, is not called when the
    "Pick a sentence" step is mounted.
    
    This patch fixes the issue by setting the "immediate" property
    for the watcher and adding it inside the "onMounted" hook, so
    that the watcher is executed as soon as the step is mounted.
    
    Bug: T341685
    Change-Id: I3e1d1570cf58330011d5dc1bd8a58f11fe470771

--- app/src/components/SXSentenceSelector/SXSentenceSelector.vue
+++ app/src/components/SXSentenceSelector/SXSentenceSelector.vue
@@ -109,6 +109,7 @@ import { useEventLogging } from "@/plugins/eventlogging";
 import { replaceUrl } from "@/utils/urlHandler";
 import useInitializeSegmentSelection from "./useInitializeSegmentSelection";
 import useMTProvidersInitialize from "@/components/SXSentenceSelector/useMTProvidersInitialize";
+import useSegmentScroll from "./useSegmentScroll";
 
 export default {
   name: "SxSentenceSelector",
@@ -164,6 +165,8 @@ export default {
     const initializeMTProviders = useMTProvidersInitialize();
     initializeMTProviders();
 
+    const scrollToSegment = useSegmentScroll();
+
     onMounted(() => {
       watch(
         translationDataReady,
@@ -172,6 +175,20 @@ export default {
             // wait for next tick, so that page contents are rendered
             await nextTick();
             initializeSegmentSelection();
+
+            if (!selectedContentTranslationUnit.value) {
+              return;
+            }
+            const segmentId = selectedContentTranslationUnit.value.id;
+            const segment = isBlockTemplateSelected.value
+              ? document.getElementById(segmentId)
+              : document.querySelector(`[data-segmentid='${segmentId}']`);
+
+            if (!segment) {
+              return;
+            }
+
+            scrollToSegment(segment);
           }
         },
         { immediate: true }
@@ -278,43 +295,6 @@ export default {
         });
       }
     };
-
-    watch(selectedContentTranslationUnit, () => {
-      if (!selectedContentTranslationUnit.value) {
-        return;
-      }
-      const segmentId = selectedContentTranslationUnit.value.id;
-
-      const segment = isBlockTemplateSelected.value
-        ? document.getElementById(segmentId)
-        : document.querySelector(`[data-segmentid='${segmentId}']`);
-      // "segment" variable refers to multi-line inline elements (<span>).
-      // Such elements have multiple border boxes (one around each line).
-      // For this reason we need to check if all these border boxes are
-      // visible or not, and we need to use "getClientRects" method instead
-      // of "getBoundingClientRect" method
-      const isInView = Array.from(segment.getClientRects()).every(
-        // use "elementFromPoint" method to get the topmost element
-        // at the coordinates of the border box of each line.
-        // If the line of the segment is inside the viewport and not
-        // hidden by another element (e.g. the proposed translation card),
-        // the returned element should match the "segment" element.
-        // Note that we only check for the top-left corner of the rectangle, so
-        // if a small portion of a line is hidden, the line is still considered
-        // to be visible.
-        (rect) => document.elementFromPoint(rect.x, rect.y) === segment
-      );
-
-      if (isInView) {
-        return;
-      }
-      // Move the current selected translation unit to viewport
-      segment.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "nearest",
-      });
-    });
 
     const isBlockTemplateSelected = computed(
       () => selectedContentTranslationUnit.value instanceof SubSectionModel
