1719b6d4a0c88201e1db818c887e5022ced05376
NikG <ngkountas@wikimedia.org>
2023-07-13 11:47:53 +0300

    SX: Fix draft restoration after having started another SX
    When going back to dashboard from "Pick a sentence" step, the current
    section suggestion vuex state variable is not cleared. Until recently,
    when we didn't support draft translation restoration, this was not an
    issue, because when selecting a new page or section to translate, this
    state variable ("currentSectionSuggestion") is set to a new value.
    However, restoring a draft translation doesn't set the current section
    suggestion, and thus the "currentSectionSuggestion" variable holds the
    previously set value (if any) leading to wrong content being displayed
    to the user after restoration.
    
    To fix this issue, this patch clears the "currentSectionSuggestion"
    state variable when going back to dashboard. Additionally, it also
    clears the "currentSourceSection" state variable (even if it doesn't
    produce such issues), so that the state doesn't contain any outdated
    information after the user has left a translation.
    
    Bug: T341765
    Change-Id: Ib1dffe57737eb64e743bd054ebab35c50980b254

--- app/src/components/SXSentenceSelector/SXSentenceSelector.vue
+++ app/src/components/SXSentenceSelector/SXSentenceSelector.vue
@@ -217,15 +217,21 @@ export default {
       doGoToDashboard();
     };
 
-    const doGoToDashboard = () => {
+    const doGoToDashboard = async () => {
       store.dispatch("translator/fetchTranslations");
       // Remove URL params so that section translation doesn't restart, leading to endless loop
       replaceUrl(null);
       store.dispatch("application/clearPendingSaveTranslationRequests");
-      router.push({ name: "dashboard" });
+      // wait for the redirection to dashboard before resetting variables needed in this route
+      await router.push({ name: "dashboard" });
       currentPageSection.value.reset();
+      store.commit("application/setCurrentSourceSection", null);
+      // we also need to clear the current section suggestion, so that the current page getter
+      // returns null
+      store.commit("application/setCurrentSectionSuggestion", null);
 
       const { currentTranslation } = store.state.application;
+
       if (!currentTranslation) {
         return;
       }
