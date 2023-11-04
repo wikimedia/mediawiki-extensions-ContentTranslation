7c12456f3d5fae5ca27fd575546218cef3df832a
Abijeet <abijeetpatro@gmail.com>
2023-11-04 12:05:24 +0530

    e2e: Add webdriver.io
    Following configuratons were choosen:
    * Configured for e2e web apps with Chrome and Firefox
    * Used local machine for automation backend
    * Framework: mocha
    * Compiler: None
    * Used page objects
    * Reporters used: junit, spec
    
    Generated some default tests using webdriver.io. These tests can be
    run using "npm run wdio"
    
    Eslint config file has been added from the WikiLambda repo
    
    Bug: T312543
    Change-Id: Ifd46b339a375b78c89bdcae458461d6ef1377475

---
+++ tests/selenium/pageobjects/secure.page.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { $ } = require( '@wdio/globals' );
+const Page = require( './page' );
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class SecurePage extends Page {
+	/**
+	 * define selectors using getter methods
+	 */
+	get flashAlert() {
+		return $( '#flash' );
+	}
+}
+
+module.exports = new SecurePage();
