68c194361e2653c62af480fd10413b21f1734252
NikG <ngkountas@wikimedia.org>
2023-12-05 18:52:31 +0200

    SX: Refactor CXTranslationList SFC to use <script setup> syntax
    Change-Id: I371583393413b9d477f32c97982d7fb7e2c96dd8

--- app/src/components/CXDashboard/CXTranslationList.vue
+++ app/src/components/CXDashboard/CXTranslationList.vue
@@ -1,3 +1,125 @@
+<script setup>
+import CxTranslationWorkDraft from "./CXTranslationWorkDraft.vue";
+import CxTranslationWorkPublished from "./CXTranslationWorkPublished.vue";
+import { MwSpinner, MwCard } from "@/lib/mediawiki.ui";
+import SxConfirmTranslationDeletionDialog from "./SXConfirmTranslationDeletionDialog.vue";
+import SxConfirmTranslationStartDialog from "./SXConfirmTranslationStartDialog.vue";
+import SxTranslationListLanguageSelector from "./SXTranslationListLanguageSelector.vue";
+import { ref, computed } from "vue";
+import useMediawikiState from "@/composables/useMediawikiState";
+import { useStore } from "vuex";
+import useDraftTranslationStart from "@/components/CXDashboard/useDraftTranslationStart";
+
+const props = defineProps({
+  activeStatus: {
+    type: String,
+    required: true,
+    validator: (value) => {
+      // The value must match one of these strings
+      return ["suggestions", "published", "draft"].indexOf(value) !== -1;
+    },
+  },
+  translationStatus: {
+    type: String,
+    required: true,
+    validator: (value) => {
+      // The value must match one of these strings
+      return ["published", "draft"].indexOf(value) !== -1;
+    },
+  },
+});
+
+const selectedSourceLanguage = ref("all");
+const selectedTargetLanguage = ref("all");
+
+const store = useStore();
+
+const loaded = computed(
+  () => store.state.translator.translationsLoaded[props.translationStatus]
+);
+
+const { enabledTargetLanguages } = useMediawikiState();
+
+const deletionDialogOn = ref(false);
+const translationConfirmationDialogOn = ref(false);
+const translationToDelete = ref(null);
+const translationToStart = ref(null);
+const isDraftTranslationList = computed(
+  () => props.translationStatus === "draft"
+);
+const translations = computed(() =>
+  store.getters["translator/getTranslationsByStatus"](props.translationStatus)
+);
+
+const isActiveForAllSourceLanguages = computed(
+  () => selectedSourceLanguage.value === "all"
+);
+const isActiveForAllTargetLanguages = computed(
+  () => selectedTargetLanguage.value === "all"
+);
+
+// sort translations, so that latest translations always show up on top
+const activeTranslations = computed(() =>
+  translations.value
+    .filter(
+      (translation) =>
+        (isActiveForAllSourceLanguages.value ||
+          translation.sourceLanguage === selectedSourceLanguage.value) &&
+        (isActiveForAllTargetLanguages.value ||
+          translation.targetLanguage === selectedTargetLanguage.value)
+    )
+    .sort((a, b) => a.lastUpdatedTimestamp < b.lastUpdatedTimestamp)
+);
+
+const availableTargetLanguages = computed(() => {
+  let translationTargetLanguages = translations.value.map(
+    (translation) => translation.targetLanguage
+  );
+
+  // If SectionTranslationTargetLanguages configuration parameter is set,
+  // target language selection is limited to these languages
+  if (!!enabledTargetLanguages.value) {
+    translationTargetLanguages = translationTargetLanguages.filter((language) =>
+      enabledTargetLanguages.value.includes(language)
+    );
+  }
+
+  return [...new Set(translationTargetLanguages)];
+});
+
+/**
+ * @type {ComputedRef<string[]>} array of language codes
+ */
+const availableSourceLanguages = computed(() => {
+  const translationSourceLanguages = translations.value.map(
+    (translation) => translation.sourceLanguage
+  );
+
+  return [...new Set(translationSourceLanguages)];
+});
+
+const askDeletionConfirmation = (translation) => {
+  translationToDelete.value = translation;
+  deletionDialogOn.value = true;
+};
+
+const isActive = computed(() => props.activeStatus === props.translationStatus);
+
+const doStartDraftTranslation = useDraftTranslationStart();
+
+/**
+ * @param {DraftTranslation} translation
+ */
+const startDraftTranslation = (translation) => {
+  if (translation.isArticleTranslation) {
+    translationToStart.value = translation;
+    translationConfirmationDialogOn.value = true;
+  } else {
+    doStartDraftTranslation(translation);
+  }
+};
+</script>
+
 <template>
   <mw-card
     v-show="isActive"
@@ -45,159 +167,3 @@
     />
   </mw-card>
 </template>
-
-<script>
-import CxTranslationWorkDraft from "./CXTranslationWorkDraft.vue";
-import CxTranslationWorkPublished from "./CXTranslationWorkPublished.vue";
-import { MwSpinner, MwCard } from "@/lib/mediawiki.ui";
-import SxConfirmTranslationDeletionDialog from "./SXConfirmTranslationDeletionDialog.vue";
-import SxConfirmTranslationStartDialog from "./SXConfirmTranslationStartDialog.vue";
-import SxTranslationListLanguageSelector from "./SXTranslationListLanguageSelector.vue";
-import { ref, computed } from "vue";
-import useMediawikiState from "@/composables/useMediawikiState";
-import { useStore } from "vuex";
-import useDraftTranslationStart from "@/components/CXDashboard/useDraftTranslationStart";
-
-export default {
-  name: "CxTranslationList",
-  components: {
-    CxTranslationWorkDraft,
-    CxTranslationWorkPublished,
-    MwCard,
-    MwSpinner,
-    SxConfirmTranslationDeletionDialog,
-    SxConfirmTranslationStartDialog,
-    SxTranslationListLanguageSelector,
-  },
-  props: {
-    activeStatus: {
-      type: String,
-      required: true,
-      validator: (value) => {
-        // The value must match one of these strings
-        return ["suggestions", "published", "draft"].indexOf(value) !== -1;
-      },
-    },
-    translationStatus: {
-      type: String,
-      required: true,
-      validator: (value) => {
-        // The value must match one of these strings
-        return ["published", "draft"].indexOf(value) !== -1;
-      },
-    },
-  },
-  setup(props) {
-    const selectedSourceLanguage = ref("all");
-    const selectedTargetLanguage = ref("all");
-
-    const store = useStore();
-
-    const loaded = computed(
-      () => store.state.translator.translationsLoaded[props.translationStatus]
-    );
-
-    const { enabledTargetLanguages } = useMediawikiState();
-
-    const deletionDialogOn = ref(false);
-    const translationConfirmationDialogOn = ref(false);
-    const translationToDelete = ref(null);
-    const translationToStart = ref(null);
-    const isDraftTranslationList = computed(
-      () => props.translationStatus === "draft"
-    );
-    const translations = computed(() =>
-      store.getters["translator/getTranslationsByStatus"](
-        props.translationStatus
-      )
-    );
-
-    const isActiveForAllSourceLanguages = computed(
-      () => selectedSourceLanguage.value === "all"
-    );
-    const isActiveForAllTargetLanguages = computed(
-      () => selectedTargetLanguage.value === "all"
-    );
-
-    // sort translations, so that latest translations always show up on top
-    const activeTranslations = computed(() =>
-      translations.value
-        .filter(
-          (translation) =>
-            (isActiveForAllSourceLanguages.value ||
-              translation.sourceLanguage === selectedSourceLanguage.value) &&
-            (isActiveForAllTargetLanguages.value ||
-              translation.targetLanguage === selectedTargetLanguage.value)
-        )
-        .sort((a, b) => a.lastUpdatedTimestamp < b.lastUpdatedTimestamp)
-    );
-
-    const availableTargetLanguages = computed(() => {
-      let translationTargetLanguages = translations.value.map(
-        (translation) => translation.targetLanguage
-      );
-
-      // If SectionTranslationTargetLanguages configuration parameter is set,
-      // target language selection is limited to these languages
-      if (!!enabledTargetLanguages.value) {
-        translationTargetLanguages = translationTargetLanguages.filter(
-          (language) => enabledTargetLanguages.value.includes(language)
-        );
-      }
-
-      return [...new Set(translationTargetLanguages)];
-    });
-
-    /**
-     * @type {ComputedRef<string[]>} array of language codes
-     */
-    const availableSourceLanguages = computed(() => {
-      const translationSourceLanguages = translations.value.map(
-        (translation) => translation.sourceLanguage
-      );
-
-      return [...new Set(translationSourceLanguages)];
-    });
-
-    const askDeletionConfirmation = (translation) => {
-      translationToDelete.value = translation;
-      deletionDialogOn.value = true;
-    };
-
-    const isActive = computed(
-      () => props.activeStatus === props.translationStatus
-    );
-
-    const doStartDraftTranslation = useDraftTranslationStart();
-
-    /**
-     * @param {DraftTranslation} translation
-     */
-    const startDraftTranslation = (translation) => {
-      if (translation.isArticleTranslation) {
-        translationToStart.value = translation;
-        translationConfirmationDialogOn.value = true;
-      } else {
-        doStartDraftTranslation(translation);
-      }
-    };
-
-    return {
-      activeTranslations,
-      availableSourceLanguages,
-      availableTargetLanguages,
-      askDeletionConfirmation,
-      deletionDialogOn,
-      isActive,
-      isDraftTranslationList,
-      loaded,
-      selectedSourceLanguage,
-      selectedTargetLanguage,
-      startDraftTranslation,
-      translationConfirmationDialogOn,
-      translationToDelete,
-      translationToStart,
-    };
-  },
-};
-</script>
