214566169271db62e971b5d837299078ace4ef52
NikG <ngkountas@wikimedia.org>
2024-01-24 15:28:37 +0200

    SXSentenceSelector: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: Ib32a358ebd7285159760a7f568ad057e14cd47de

--- app/src/components/SXSentenceSelector/SXSentenceSelector.vue
+++ app/src/components/SXSentenceSelector/SXSentenceSelector.vue
@@ -1,3 +1,192 @@
+<script setup>
+import { MwButton, MwRow, MwCol, MwSpinner } from "@/lib/mediawiki.ui";
+import { mwIconArrowPrevious } from "@/lib/mediawiki.ui/components/icons";
+import { getDir } from "@wikimedia/language-data";
+import SxConfirmBackNavigationDialog from "./SXConfirmBackNavigationDialog.vue";
+import SxTranslationSelector from "./SXTranslationSelector.vue";
+import SxSentenceSelectorContentHeader from "./SXSentenceSelectorContentHeader.vue";
+import ProposedTranslationCard from "./ProposedTranslationCard.vue";
+import SubSection from "./SubSection.vue";
+import BlockTemplateAdaptationCard from "./BlockTemplateAdaptationCard.vue";
+import TranslatedSegmentCard from "./TranslatedSegmentCard.vue";
+import SubSectionModel from "@/wiki/cx/models/subSection";
+import { computed, nextTick, onMounted, ref, watch } from "vue";
+import useApplicationState from "@/composables/useApplicationState";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { useEventLogging } from "@/plugins/eventlogging";
+import { replaceUrl } from "@/utils/urlHandler";
+import useInitializeSegmentSelection from "./useInitializeSegmentSelection";
+import useMTProvidersInitialize from "@/components/SXSentenceSelector/useMTProvidersInitialize";
+import useSelectedContentTranslationUnitScroll from "./useSelectedContentTranslationUnitScroll";
+import useTranslationsFetch from "@/composables/useTranslationsFetch";
+
+const isTranslationOptionsActive = ref(false);
+const shouldProposedTranslationBounce = ref(false);
+const screenHeight = ref("100%");
+
+const store = useStore();
+
+const {
+  currentSourcePage,
+  currentTargetPage,
+  currentSourceSection: currentPageSection,
+  selectedContentTranslationUnit,
+  currentMTProvider,
+  sourceLanguage,
+  targetLanguage,
+} = useApplicationState(store);
+
+const translationDataReady = computed(
+  () => store.state.application.translationDataLoadingCounter === 0
+);
+
+const isSelectedTranslationUnitTranslated = computed(
+  () => currentPageSection.value?.isSelectedTranslationUnitTranslated
+);
+
+const subSections = computed(() => currentPageSection.value?.subSections);
+
+const originalSegmentContent = computed(
+  () => currentPageSection.value?.selectedTranslationUnitOriginalContent
+);
+
+const sentenceSelectorStyle = computed(() =>
+  isNaN(screenHeight.value) ? screenHeight.value : `${screenHeight.value}px`
+);
+const logEvent = useEventLogging();
+const initializeSegmentSelection = useInitializeSegmentSelection();
+const initializeMTProviders = useMTProvidersInitialize();
+initializeMTProviders();
+
+const scrollToTranslationUnit = useSelectedContentTranslationUnitScroll();
+
+onMounted(() => {
+  watch(
+    translationDataReady,
+    async () => {
+      if (translationDataReady.value) {
+        // wait for next tick, so that page contents are rendered
+        await nextTick();
+        initializeSegmentSelection();
+
+        // scroll to already selected translation unit if any
+        scrollToTranslationUnit();
+      }
+    },
+    { immediate: true }
+  );
+  screenHeight.value = window.innerHeight;
+});
+
+watch(selectedContentTranslationUnit, scrollToTranslationUnit);
+
+const skipTranslation = () =>
+  store.dispatch("application/selectNextTranslationUnit");
+const selectPreviousTranslationUnit = () =>
+  store.dispatch("application/selectPreviousTranslationUnit");
+
+const applyTranslation = () => {
+  logEvent({
+    event_type: "editor_segment_add",
+    translation_source_language: sourceLanguage.value,
+    translation_target_language: targetLanguage.value,
+  });
+  store.dispatch(
+    "application/applyProposedTranslationToSelectedTranslationUnit"
+  );
+};
+
+const bounceTranslation = () => {
+  shouldProposedTranslationBounce.value = true;
+  setTimeout(() => {
+    shouldProposedTranslationBounce.value = false;
+  }, 100);
+};
+const router = useRouter();
+
+const goToDashboard = () => {
+  const { autoSavePending } = store.state.application;
+
+  if (autoSavePending) {
+    verifyBackNavigationDialogOn.value = true;
+
+    return;
+  }
+
+  doGoToDashboard();
+};
+
+const { fetchTranslationsByStatus } = useTranslationsFetch();
+
+const doGoToDashboard = async () => {
+  fetchTranslationsByStatus("draft");
+  // Remove URL params so that section translation doesn't restart, leading to endless loop
+  replaceUrl(null);
+  store.dispatch("application/clearPendingSaveTranslationRequests");
+  // wait for the redirection to dashboard before resetting variables needed in this route
+  await router.push({ name: "dashboard" });
+  currentPageSection.value.reset();
+  store.commit("application/setCurrentSourceSection", null);
+  // we also need to clear the current section suggestion, so that the current page getter
+  // returns null
+  store.commit("application/setCurrentSectionSuggestion", null);
+
+  const { currentTranslation } = store.state.application;
+
+  if (!currentTranslation) {
+    return;
+  }
+
+  store.commit("application/setCurrentTranslationRestored", false);
+  // always clear current translation, if any, when going back to dashboard
+  store.commit("application/setCurrentTranslation", null);
+};
+
+const configureTranslationOptions = () => {
+  // Disable MT provider change when block template is selected
+  if (isBlockTemplateSelected.value) {
+    return;
+  }
+  isTranslationOptionsActive.value = true;
+  store.dispatch("application/translateSelectedTranslationUnitForAllProviders");
+};
+
+const editTranslation = (content, isInitialEdit) => {
+  router.push({
+    name: "sx-editor",
+    state: {
+      content,
+      originalContent: originalSegmentContent.value,
+      title: currentTargetPage.value?.title || currentSourcePage.value.title,
+      isInitialEdit: isInitialEdit || null,
+    },
+  });
+};
+
+const previewTranslation = () => router.push({ name: "sx-publisher" });
+
+const retryTranslation = async () => {
+  if (!selectedContentTranslationUnit.value) {
+    await store.dispatch("application/translateTranslationUnitById", {
+      id: 0,
+      provider: currentMTProvider.value,
+    });
+  } else {
+    await store.dispatch("application/translateTranslationUnitById", {
+      id: selectedContentTranslationUnit.value.id,
+      provider: currentMTProvider.value,
+    });
+  }
+};
+
+const isBlockTemplateSelected = computed(
+  () => selectedContentTranslationUnit.value instanceof SubSectionModel
+);
+
+const verifyBackNavigationDialogOn = ref(false);
+</script>
+
 <template>
   <section
     class="sx-sentence-selector fill-height column ma-0 no-wrap"
@@ -88,241 +277,6 @@
     />
   </section>
 </template>
-
-<script>
-import { MwButton, MwRow, MwCol, MwSpinner } from "@/lib/mediawiki.ui";
-import { mwIconArrowPrevious } from "@/lib/mediawiki.ui/components/icons";
-import { getDir } from "@wikimedia/language-data";
-import SxConfirmBackNavigationDialog from "./SXConfirmBackNavigationDialog.vue";
-import SxTranslationSelector from "./SXTranslationSelector.vue";
-import SxSentenceSelectorContentHeader from "./SXSentenceSelectorContentHeader.vue";
-import ProposedTranslationCard from "./ProposedTranslationCard.vue";
-import SubSection from "./SubSection.vue";
-import BlockTemplateAdaptationCard from "./BlockTemplateAdaptationCard.vue";
-import TranslatedSegmentCard from "./TranslatedSegmentCard.vue";
-import SubSectionModel from "@/wiki/cx/models/subSection";
-import { computed, nextTick, onMounted, ref, watch } from "vue";
-import useApplicationState from "@/composables/useApplicationState";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import { useEventLogging } from "@/plugins/eventlogging";
-import { replaceUrl } from "@/utils/urlHandler";
-import useInitializeSegmentSelection from "./useInitializeSegmentSelection";
-import useMTProvidersInitialize from "@/components/SXSentenceSelector/useMTProvidersInitialize";
-import useSelectedContentTranslationUnitScroll from "./useSelectedContentTranslationUnitScroll";
-import useTranslationsFetch from "@/composables/useTranslationsFetch";
-
-export default {
-  name: "SxSentenceSelector",
-  components: {
-    SxConfirmBackNavigationDialog,
-    BlockTemplateAdaptationCard,
-    TranslatedSegmentCard,
-    ProposedTranslationCard,
-    SubSection,
-    SxSentenceSelectorContentHeader,
-    MwRow,
-    MwSpinner,
-    MwCol,
-    SxTranslationSelector,
-    MwButton,
-  },
-  setup() {
-    const isTranslationOptionsActive = ref(false);
-    const shouldProposedTranslationBounce = ref(false);
-    const screenHeight = ref("100%");
-
-    const store = useStore();
-
-    const {
-      currentSourcePage,
-      currentTargetPage,
-      currentSourceSection: currentPageSection,
-      selectedContentTranslationUnit,
-      currentMTProvider,
-      sourceLanguage,
-      targetLanguage,
-    } = useApplicationState(store);
-
-    const translationDataReady = computed(
-      () => store.state.application.translationDataLoadingCounter === 0
-    );
-
-    const isSelectedTranslationUnitTranslated = computed(
-      () => currentPageSection.value?.isSelectedTranslationUnitTranslated
-    );
-
-    const subSections = computed(() => currentPageSection.value?.subSections);
-
-    const originalSegmentContent = computed(
-      () => currentPageSection.value?.selectedTranslationUnitOriginalContent
-    );
-
-    const sentenceSelectorStyle = computed(() =>
-      isNaN(screenHeight.value) ? screenHeight.value : `${screenHeight.value}px`
-    );
-    const logEvent = useEventLogging();
-    const initializeSegmentSelection = useInitializeSegmentSelection();
-    const initializeMTProviders = useMTProvidersInitialize();
-    initializeMTProviders();
-
-    const scrollToTranslationUnit = useSelectedContentTranslationUnitScroll();
-
-    onMounted(() => {
-      watch(
-        translationDataReady,
-        async () => {
-          if (translationDataReady.value) {
-            // wait for next tick, so that page contents are rendered
-            await nextTick();
-            initializeSegmentSelection();
-
-            // scroll to already selected translation unit if any
-            scrollToTranslationUnit();
-          }
-        },
-        { immediate: true }
-      );
-      screenHeight.value = window.innerHeight;
-    });
-
-    watch(selectedContentTranslationUnit, scrollToTranslationUnit);
-
-    const skipTranslation = () =>
-      store.dispatch("application/selectNextTranslationUnit");
-    const selectPreviousTranslationUnit = () =>
-      store.dispatch("application/selectPreviousTranslationUnit");
-
-    const applyTranslation = () => {
-      logEvent({
-        event_type: "editor_segment_add",
-        translation_source_language: sourceLanguage.value,
-        translation_target_language: targetLanguage.value,
-      });
-      store.dispatch(
-        "application/applyProposedTranslationToSelectedTranslationUnit"
-      );
-    };
-
-    const bounceTranslation = () => {
-      shouldProposedTranslationBounce.value = true;
-      setTimeout(() => {
-        shouldProposedTranslationBounce.value = false;
-      }, 100);
-    };
-    const router = useRouter();
-
-    const goToDashboard = () => {
-      const { autoSavePending } = store.state.application;
-
-      if (autoSavePending) {
-        verifyBackNavigationDialogOn.value = true;
-
-        return;
-      }
-
-      doGoToDashboard();
-    };
-
-    const { fetchTranslationsByStatus } = useTranslationsFetch();
-
-    const doGoToDashboard = async () => {
-      fetchTranslationsByStatus("draft");
-      // Remove URL params so that section translation doesn't restart, leading to endless loop
-      replaceUrl(null);
-      store.dispatch("application/clearPendingSaveTranslationRequests");
-      // wait for the redirection to dashboard before resetting variables needed in this route
-      await router.push({ name: "dashboard" });
-      currentPageSection.value.reset();
-      store.commit("application/setCurrentSourceSection", null);
-      // we also need to clear the current section suggestion, so that the current page getter
-      // returns null
-      store.commit("application/setCurrentSectionSuggestion", null);
-
-      const { currentTranslation } = store.state.application;
-
-      if (!currentTranslation) {
-        return;
-      }
-
-      store.commit("application/setCurrentTranslationRestored", false);
-      // always clear current translation, if any, when going back to dashboard
-      store.commit("application/setCurrentTranslation", null);
-    };
-
-    const configureTranslationOptions = () => {
-      // Disable MT provider change when block template is selected
-      if (isBlockTemplateSelected.value) {
-        return;
-      }
-      isTranslationOptionsActive.value = true;
-      store.dispatch(
-        "application/translateSelectedTranslationUnitForAllProviders"
-      );
-    };
-
-    const editTranslation = (content, isInitialEdit) => {
-      router.push({
-        name: "sx-editor",
-        state: {
-          content,
-          originalContent: originalSegmentContent.value,
-          title:
-            currentTargetPage.value?.title || currentSourcePage.value.title,
-          isInitialEdit: isInitialEdit || null,
-        },
-      });
-    };
-
-    const previewTranslation = () => router.push({ name: "sx-publisher" });
-
-    const retryTranslation = async () => {
-      if (!selectedContentTranslationUnit.value) {
-        await store.dispatch("application/translateTranslationUnitById", {
-          id: 0,
-          provider: currentMTProvider.value,
-        });
-      } else {
-        await store.dispatch("application/translateTranslationUnitById", {
-          id: selectedContentTranslationUnit.value.id,
-          provider: currentMTProvider.value,
-        });
-      }
-    };
-
-    const isBlockTemplateSelected = computed(
-      () => selectedContentTranslationUnit.value instanceof SubSectionModel
-    );
-
-    const verifyBackNavigationDialogOn = ref(false);
-
-    return {
-      applyTranslation,
-      bounceTranslation,
-      configureTranslationOptions,
-      currentPageSection,
-      doGoToDashboard,
-      editTranslation,
-      getDir,
-      goToDashboard,
-      isBlockTemplateSelected,
-      isSelectedTranslationUnitTranslated,
-      isTranslationOptionsActive,
-      mwIconArrowPrevious,
-      previewTranslation,
-      retryTranslation,
-      selectPreviousTranslationUnit,
-      sentenceSelectorStyle,
-      shouldProposedTranslationBounce,
-      skipTranslation,
-      sourceLanguage,
-      subSections,
-      translationDataReady,
-      verifyBackNavigationDialogOn,
-    };
-  },
-};
-</script>
 
 <style lang="less">
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
