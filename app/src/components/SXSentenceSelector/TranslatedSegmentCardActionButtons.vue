6ced2f41be1430314d9dc2609b95e2310463db1d
NikG <ngkountas@wikimedia.org>
2024-02-19 20:52:40 +0200

    TranslatedSegmentCardActionButtons: Use CdxButton and CdxIcon components
    This patch also fixes the missing border betweeen the action buttons,
    to match design specifications.
    
    Before fix: https://phabricator.wikimedia.org/F41986166
    After fix: https://phabricator.wikimedia.org/F41986186
    
    Design: https://phabricator.wikimedia.org/F41985465
    
    Bug: T340259
    Change-Id: Ief087c6c7700002b4733ddfe03d36818fdb240d9

--- app/src/components/SXSentenceSelector/TranslatedSegmentCardActionButtons.vue
+++ app/src/components/SXSentenceSelector/TranslatedSegmentCardActionButtons.vue
@@ -1,12 +1,10 @@
 <script setup>
-import { MwRow, MwButton } from "@/lib/mediawiki.ui";
-import {
-  mwIconArrowForward,
-  mwIconPrevious,
-} from "@/lib/mediawiki.ui/components/icons";
+import { MwRow } from "@/lib/mediawiki.ui";
 import useApplicationState from "@/composables/useApplicationState";
 import { computed } from "vue";
 import { useStore } from "vuex";
+import { CdxButton, CdxIcon } from "@wikimedia/codex";
+import { cdxIconNext, cdxIconPrevious } from "@wikimedia/codex-icons";
 
 defineEmits(["select-previous-segment", "skip-translation"]);
 
@@ -20,20 +18,41 @@ const isLastTranslationUnit = computed(
 </script>
 
 <template>
-  <mw-row class="sx-sentence-selector__translation-action-buttons ma-0">
-    <mw-button
-      :icon="mwIconPrevious"
-      type="icon"
-      class="sx-sentence-selector__previous-sentence-button col pa-4"
+  <mw-row
+    class="sx-sentence-selector__translated-segment-card__action-buttons ma-0"
+  >
+    <cdx-button
+      class="sx-sentence-selector__translated-segment-card__previous-button col pa-4"
+      weight="quiet"
       :disabled="isSectionTitleSelected"
       @click="$emit('select-previous-segment')"
-    />
-    <mw-button
-      type="icon"
-      :icon="mwIconArrowForward"
-      class="sx-sentence-selector__skip-translation-button col pa-4 items-start"
+    >
+      <cdx-icon :icon="cdxIconPrevious" />
+    </cdx-button>
+    <cdx-button
+      class="sx-sentence-selector__translated-segment-card__next-button col pa-4"
+      weight="quiet"
       :disabled="isLastTranslationUnit"
       @click="$emit('skip-translation')"
-    />
+    >
+      <cdx-icon :icon="cdxIconNext" />
+    </cdx-button>
   </mw-row>
 </template>
+
+<style lang="less">
+@import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
+
+.sx-sentence-selector__translated-segment-card {
+  &__action-buttons {
+    border-top: @border-subtle;
+  }
+  &__action-buttons button {
+    border-radius: 0;
+    border-block: none;
+  }
+  div&__action-buttons button&__next-button:last-of-type {
+    border-inline-start-color: @border-color-subtle;
+  }
+}
+</style>
