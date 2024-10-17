a2c3f657e03151af06d9f3edb61298d43dc63286
NikG <ngkountas@wikimedia.org>
2024-10-17 12:34:35 +0300

    SXArticleLanguageSelector: Migrate to use <script setup> syntax
    Bug: T355780
    Bug: T374886
    Change-Id: I16b48d289a2a9a0499aa4eb9b6c0665e01ffba27

--- app/src/components/SXArticleLanguageSelector.vue
+++ app/src/components/SXArticleLanguageSelector.vue
@@ -1,3 +1,47 @@
+<script setup>
+import SxTranslationListLanguageSelector from "./CXDashboard/SXTranslationListLanguageSelector.vue";
+import useMediaWikiState from "@/composables/useMediaWikiState";
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { useArticleLanguagePairUpdate } from "@/composables/useLanguageHelper";
+import useLanguageTitleGroup from "@/composables/useLanguageTitleGroup";
+import useURLHandler from "@/composables/useURLHandler";
+
+const { supportedLanguageCodes, enabledTargetLanguages } = useMediaWikiState();
+const store = useStore();
+
+const {
+  sourceLanguageURLParameter: sourceLanguage,
+  targetLanguageURLParameter: targetLanguage,
+} = useURLHandler();
+const { currentLanguageTitleGroup } = useLanguageTitleGroup();
+
+/**
+ * Titles are provided in the following format: { lang: "en", title: "Animal" }
+ * so title.lang contains language code
+ *
+ * @type {ComputedRef<string[]>} array of language codes
+ */
+const availableSourceLanguages = computed(
+  () => currentLanguageTitleGroup.value?.titles.map((title) => title.lang) || []
+);
+/**
+ * If enabledTargetLanguages are set,
+ * target language selection is limited to these languages
+ *
+ * @return {ComputedRef<string[]>} - Array of available target language codes
+ */
+const targetLanguages = computed(
+  () => enabledTargetLanguages.value || supportedLanguageCodes.value
+);
+
+const updateLanguagePair = useArticleLanguagePairUpdate();
+const onSourceLanguageSelected = (newSourceLanguage) =>
+  updateLanguagePair(newSourceLanguage, targetLanguage.value);
+const onTargetLanguageSelected = (newTargetLanguage) =>
+  updateLanguagePair(sourceLanguage.value, newTargetLanguage);
+</script>
+
 <template>
   <sx-translation-list-language-selector
     class="sx-article-language-selector"
@@ -9,65 +53,6 @@
     @update:selected-target-language="onTargetLanguageSelected"
   />
 </template>
-
-<script>
-import SxTranslationListLanguageSelector from "./CXDashboard/SXTranslationListLanguageSelector.vue";
-import useMediaWikiState from "@/composables/useMediaWikiState";
-import useApplicationState from "@/composables/useApplicationState";
-import { computed } from "vue";
-import { useStore } from "vuex";
-import { useArticleLanguagePairUpdate } from "@/composables/useLanguageHelper";
-import useLanguageTitleGroup from "@/composables/useLanguageTitleGroup";
-
-export default {
-  name: "SxArticleLanguageSelector",
-  components: {
-    SxTranslationListLanguageSelector,
-  },
-  setup() {
-    const { supportedLanguageCodes, enabledTargetLanguages } =
-      useMediaWikiState();
-    const store = useStore();
-    const { sourceLanguage, targetLanguage } = useApplicationState(store);
-    const { currentLanguageTitleGroup } = useLanguageTitleGroup();
-
-    /**
-     * Titles are provided in the following format: { lang: "en", title: "Animal" }
-     * so title.lang contains language code
-     *
-     * @type {ComputedRef<string[]>} array of language codes
-     */
-    const availableSourceLanguages = computed(
-      () =>
-        currentLanguageTitleGroup.value?.titles.map((title) => title.lang) || []
-    );
-    /**
-     * If enabledTargetLanguages are set,
-     * target language selection is limited to these languages
-     *
-     * @return {ComputedRef<string[]>} - Array of available target language codes
-     */
-    const targetLanguages = computed(
-      () => enabledTargetLanguages.value || supportedLanguageCodes.value
-    );
-
-    const updateLanguagePair = useArticleLanguagePairUpdate();
-    const onSourceLanguageSelected = (newSourceLanguage) =>
-      updateLanguagePair(newSourceLanguage, targetLanguage.value);
-    const onTargetLanguageSelected = (newTargetLanguage) =>
-      updateLanguagePair(sourceLanguage.value, newTargetLanguage);
-
-    return {
-      availableSourceLanguages,
-      targetLanguages,
-      onSourceLanguageSelected,
-      onTargetLanguageSelected,
-      sourceLanguage,
-      targetLanguage,
-    };
-  },
-};
-</script>
 
 <style lang="less">
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
