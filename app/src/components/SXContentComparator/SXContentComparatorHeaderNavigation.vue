70adcb02dfbf27d90cc7724786467d62322e2b92
eamedina <eamedina@wikimedia.org>
2024-11-14 12:17:37 -0400

    SXContentComparatorHeaderNavigation: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: I296f536d89a531ba001dfac782fe1bc2dc97babb

--- app/src/components/SXContentComparator/SXContentComparatorHeaderNavigation.vue
+++ app/src/components/SXContentComparator/SXContentComparatorHeaderNavigation.vue
@@ -1,3 +1,46 @@
+<script setup>
+import { MwCol, MwButton } from "@/lib/mediawiki.ui";
+import {
+  mwIconArrowForward,
+  mwIconPrevious,
+} from "@/lib/mediawiki.ui/components/icons";
+import { computed } from "vue";
+import usePageSectionSelect from "@/composables/usePageSectionSelect";
+import useURLHandler from "@/composables/useURLHandler";
+
+const props = defineProps({
+  sectionSourceTitles: {
+    type: Array,
+    required: true,
+  },
+});
+
+const { sectionURLParameter } = useURLHandler();
+
+const currentTitleIndex = computed(() =>
+  props.sectionSourceTitles.indexOf(sectionURLParameter.value)
+);
+
+const { selectPageSectionByTitle } = usePageSectionSelect();
+
+const goToPreviousSection = () => {
+  const previousIndex =
+    (currentTitleIndex.value - 1 + props.sectionSourceTitles.length) %
+    props.sectionSourceTitles.length;
+
+  const previousSectionTitle = props.sectionSourceTitles[previousIndex];
+  selectPageSectionByTitle(previousSectionTitle);
+};
+
+const goToNextSection = () => {
+  const nextIndex =
+    (currentTitleIndex.value + 1) % props.sectionSourceTitles.length;
+
+  const nextSectionTitle = props.sectionSourceTitles[nextIndex];
+  selectPageSectionByTitle(nextSectionTitle);
+};
+</script>
+
 <template>
   <mw-col class="justify-end" align="center">
     <mw-button
@@ -14,61 +57,3 @@
     />
   </mw-col>
 </template>
-
-<script>
-import { MwCol, MwButton } from "@/lib/mediawiki.ui";
-import {
-  mwIconArrowForward,
-  mwIconPrevious,
-} from "@/lib/mediawiki.ui/components/icons";
-import { computed } from "vue";
-import usePageSectionSelect from "@/composables/usePageSectionSelect";
-import useURLHandler from "@/composables/useURLHandler";
-
-export default {
-  name: "SxContentComparatorHeaderNavigation",
-  components: {
-    MwCol,
-    MwButton,
-  },
-  props: {
-    sectionSourceTitles: {
-      type: Array,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { sectionURLParameter } = useURLHandler();
-
-    const currentTitleIndex = computed(() =>
-      props.sectionSourceTitles.indexOf(sectionURLParameter.value)
-    );
-
-    const { selectPageSectionByTitle } = usePageSectionSelect();
-
-    const goToPreviousSection = () => {
-      const previousIndex =
-        (currentTitleIndex.value - 1 + props.sectionSourceTitles.length) %
-        props.sectionSourceTitles.length;
-
-      const previousSectionTitle = props.sectionSourceTitles[previousIndex];
-      selectPageSectionByTitle(previousSectionTitle);
-    };
-
-    const goToNextSection = () => {
-      const nextIndex =
-        (currentTitleIndex.value + 1) % props.sectionSourceTitles.length;
-
-      const nextSectionTitle = props.sectionSourceTitles[nextIndex];
-      selectPageSectionByTitle(nextSectionTitle);
-    };
-
-    return {
-      goToNextSection,
-      goToPreviousSection,
-      mwIconPrevious,
-      mwIconArrowForward,
-    };
-  },
-};
-</script>
