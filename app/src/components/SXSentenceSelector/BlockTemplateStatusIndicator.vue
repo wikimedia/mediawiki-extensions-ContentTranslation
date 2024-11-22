962b7ed83be9aa0b35c85f32931f16d9665e0a2e
eamedina <eamedina@wikimedia.org>
2024-11-22 14:44:42 -0400

    BlockTemplateStatusIndicator: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: Ie7b56101f290ed852fdf7232865e4e1eaf970fd3

--- app/src/components/SXSentenceSelector/BlockTemplateStatusIndicator.vue
+++ app/src/components/SXSentenceSelector/BlockTemplateStatusIndicator.vue
@@ -1,3 +1,33 @@
+<script setup>
+import { computed } from "vue";
+import { mwIconAlert, mwIconCheck } from "@/lib/mediawiki.ui/components/icons";
+import { MwIcon, MwCircleProgressBar } from "@/lib/mediawiki.ui";
+
+const props = defineProps({
+  isTemplateAdapted: {
+    type: Boolean,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  percentage: {
+    type: Number,
+    required: true,
+  },
+  strokeWidth: {
+    type: Number,
+    required: true,
+  },
+});
+
+const cssVars = computed(() => ({ "--size": props.size }));
+const statusIcon = computed(() =>
+  !props.isTemplateAdapted || props.percentage === 0 ? mwIconAlert : mwIconCheck
+);
+</script>
+
 <template>
   <div class="block-template-status-indicator" :style="cssVars">
     <mw-circle-progress-bar
@@ -15,50 +45,6 @@
     />
   </div>
 </template>
-
-<script>
-import { computed } from "vue";
-import { mwIconAlert, mwIconCheck } from "@/lib/mediawiki.ui/components/icons";
-import { MwIcon, MwCircleProgressBar } from "@/lib/mediawiki.ui";
-export default {
-  name: "BlockTemplateStatusIndicator",
-  components: {
-    MwCircleProgressBar,
-    MwIcon,
-  },
-  props: {
-    isTemplateAdapted: {
-      type: Boolean,
-      required: true,
-    },
-    size: {
-      type: Number,
-      required: true,
-    },
-    percentage: {
-      type: Number,
-      required: true,
-    },
-    strokeWidth: {
-      type: Number,
-      required: true,
-    },
-  },
-  setup(props) {
-    const cssVars = computed(() => ({ "--size": props.size }));
-    const statusIcon = computed(() =>
-      !props.isTemplateAdapted || props.percentage === 0
-        ? mwIconAlert
-        : mwIconCheck
-    );
-
-    return {
-      cssVars,
-      statusIcon,
-    };
-  },
-};
-</script>
 
 <style lang="less">
 .block-template-status-indicator {
