af1436d336fc8a07b59e2756d8109c64357c3d7a
eamedina <eamedina@wikimedia.org>
2024-11-22 14:39:30 -0400

    SXEditorOriginalContent: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: I58066d1a1702f9577b9577173339b7c7325717ab

--- app/src/components/SXEditor/SXEditorOriginalContent.vue
+++ app/src/components/SXEditor/SXEditorOriginalContent.vue
@@ -1,3 +1,53 @@
+<script setup>
+import { MwExpandableContent } from "@/lib/mediawiki.ui";
+import { ref, onMounted } from "vue";
+import { siteMapper } from "@/utils/mediawikiHelper";
+
+const props = defineProps({
+  originalContent: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  dir: {
+    type: String,
+    required: true,
+  },
+});
+
+/**
+ * Make all links open in new tabs and fix href to point to the source wiki
+ *
+ * @param {Element} containerEl
+ * @param {string} language
+ */
+const fixLinkTargets = (containerEl, language) => {
+  const links = containerEl.getElementsByTagName("a");
+
+  for (const link of links) {
+    link.href = siteMapper.getPageUrl(language, link.title);
+    link.target = "_blank";
+  }
+};
+
+const originalContentRef = ref(null);
+const twoLinesHeight = ref(0);
+const getLineHeight = () =>
+  parseFloat(
+    document.defaultView
+      .getComputedStyle(originalContentRef.value, null)
+      .getPropertyValue("line-height")
+  );
+
+onMounted(() => {
+  twoLinesHeight.value = 2 * getLineHeight();
+  fixLinkTargets(originalContentRef.value, props.language);
+});
+</script>
+
 <template>
   <section class="px-4 pt-3 pb-2 sx-editor__original-content-panel">
     <h5
@@ -17,65 +67,6 @@
     </mw-expandable-content>
   </section>
 </template>
-
-<script>
-import { MwExpandableContent } from "@/lib/mediawiki.ui";
-import { ref, onMounted } from "vue";
-import { siteMapper } from "@/utils/mediawikiHelper";
-
-/**
- * Make all links open in new tabs and fix href to point to the
- * source wiki
- * @param {Element} containerEl
- */
-function fixLinkTargets(containerEl, language) {
-  const links = containerEl.getElementsByTagName("a");
-
-  for (const link of links) {
-    link.href = siteMapper.getPageUrl(language, link.title);
-    link.target = "_blank";
-  }
-}
-
-export default {
-  name: "SxEditorOriginalContent",
-  components: { MwExpandableContent },
-  props: {
-    originalContent: {
-      type: String,
-      required: true,
-    },
-    language: {
-      type: String,
-      required: true,
-    },
-    dir: {
-      type: String,
-      required: true,
-    },
-  },
-  setup(props) {
-    const originalContentRef = ref(null);
-    const twoLinesHeight = ref(0);
-    const getLineHeight = () =>
-      parseFloat(
-        document.defaultView
-          .getComputedStyle(originalContentRef.value, null)
-          .getPropertyValue("line-height")
-      );
-
-    onMounted(() => {
-      twoLinesHeight.value = 2 * getLineHeight();
-      fixLinkTargets(originalContentRef.value, props.language);
-    });
-
-    return {
-      originalContentRef,
-      twoLinesHeight,
-    };
-  },
-};
-</script>
 
 <style lang="less">
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
