58dec88dad8612c33012792dc1127d19083e1b96
eamedina <eamedina@wikimedia.org>
2024-11-22 14:26:28 -0400

    SourceVsTargetSelector: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: Ia803d45dc459fc4fb6f2563d7220281509d81a06

--- app/src/components/SXContentComparator/SourceVsTargetSelector.vue
+++ app/src/components/SXContentComparator/SourceVsTargetSelector.vue
@@ -1,3 +1,43 @@
+<script setup>
+import { MwButtonGroup } from "@/lib/mediawiki.ui";
+import { watch } from "vue";
+import useListSelector from "@/components/SXContentComparator/useListSelector";
+
+const props = defineProps({
+  selection: {
+    type: String,
+    required: true,
+  },
+  isMappedSection: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:selection"]);
+
+const updateSelection = (selection) => emit("update:selection", selection);
+
+const listSelector = useListSelector(props);
+
+/**
+ * Watch for isMappedSection prop so that we can update
+ * sourceVsTarget selection accordingly, when isMappedSection
+ * is updated and previous sourceVsTarget selection is no
+ * longer a valid option.
+ */
+watch(
+  () => props.isMappedSection,
+  () => {
+    if (
+      !listSelector.value.map((item) => item.value).includes(props.selection)
+    ) {
+      updateSelection(listSelector.value[0].value);
+    }
+  }
+);
+</script>
+
 <template>
   <div class="sx-content-comparator__source-target-selector">
     <mw-button-group
@@ -7,57 +47,6 @@
     />
   </div>
 </template>
-
-<script>
-import { MwButtonGroup } from "@/lib/mediawiki.ui";
-import { watch } from "vue";
-import useListSelector from "@/components/SXContentComparator/useListSelector";
-
-export default {
-  name: "SxContentComparatorSourceVsTargetSelector",
-  components: { MwButtonGroup },
-  props: {
-    selection: {
-      type: String,
-      required: true,
-    },
-    isMappedSection: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  emits: ["update:selection"],
-  setup(props, { emit }) {
-    const updateSelection = (selection) => emit("update:selection", selection);
-
-    const listSelector = useListSelector(props);
-
-    /**
-     * Watch for isMappedSection prop so that we can update
-     * sourceVsTarget selection accordingly, when isMappedSection
-     * is updated and previous sourceVsTarget selection is no
-     * longer a valid option.
-     */
-    watch(
-      () => props.isMappedSection,
-      () => {
-        if (
-          !listSelector.value
-            .map((item) => item.value)
-            .includes(props.selection)
-        ) {
-          updateSelection(listSelector.value[0].value);
-        }
-      }
-    );
-
-    return {
-      listSelector,
-      updateSelection,
-    };
-  },
-};
-</script>
 
 <style lang="less">
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
