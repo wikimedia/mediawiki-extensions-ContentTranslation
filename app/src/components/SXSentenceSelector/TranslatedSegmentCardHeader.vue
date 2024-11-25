9b6f9b06ce152f9768307f6594fbc52023c16c77
eamedina <eamedina@wikimedia.org>
2024-11-25 16:22:19 -0400

    TranslatedSegmentCardHeader: Migrate to use <script setup> syntax
    Bug: T355780
    Change-Id: Ie2fe8463bd02a22b31905654b6b431f84ee7c7c9

--- app/src/components/SXSentenceSelector/TranslatedSegmentCardHeader.vue
+++ app/src/components/SXSentenceSelector/TranslatedSegmentCardHeader.vue
@@ -1,3 +1,50 @@
+<script setup>
+import { MwButtonGroup } from "@/lib/mediawiki.ui";
+import { computed } from "vue";
+import { useI18n } from "vue-banana-i18n";
+import useCurrentPageSection from "@/composables/useCurrentPageSection";
+
+const props = defineProps({
+  selection: {
+    type: String,
+    required: true,
+    validator: (value) => ["sentence", "paragraph"].includes(value),
+  },
+});
+
+const emit = defineEmits(["update:selection"]);
+
+const { isSectionTitleSelected } = useCurrentPageSection();
+const bananaI18n = useI18n();
+
+const scopeOptions = computed(() => [
+  {
+    value: "sentence",
+    props: {
+      label: bananaI18n.i18n(
+        "cx-sx-sentence-selector-translated-segment-sentence-option"
+      ),
+      type: "text",
+      class: "px-0 py-4 mx-4",
+    },
+  },
+
+  {
+    value: "paragraph",
+    props: {
+      label: bananaI18n.i18n(
+        "cx-sx-sentence-selector-translated-segment-paragraph-option"
+      ),
+      type: "text",
+      class: "px-0 py-4 mx-4",
+      disabled: isSectionTitleSelected.value,
+    },
+  },
+]);
+
+const updateSelection = (selection) => emit("update:selection", selection);
+</script>
+
 <template>
   <div class="translated-segment-card-header">
     <mw-button-group
@@ -7,59 +54,6 @@
     />
   </div>
 </template>
-
-<script>
-import { MwButtonGroup } from "@/lib/mediawiki.ui";
-import { computed } from "vue";
-import { useI18n } from "vue-banana-i18n";
-import useCurrentPageSection from "@/composables/useCurrentPageSection";
-
-export default {
-  name: "TranslatedSegmentCardHeader",
-  components: { MwButtonGroup },
-  props: {
-    selection: {
-      type: String,
-      required: true,
-      validator: (value) => ["sentence", "paragraph"].includes(value),
-    },
-  },
-  emits: ["update:selection"],
-  setup(props, { emit }) {
-    const { isSectionTitleSelected } = useCurrentPageSection();
-    const bananaI18n = useI18n();
-
-    const scopeOptions = computed(() => [
-      {
-        value: "sentence",
-        props: {
-          label: bananaI18n.i18n(
-            "cx-sx-sentence-selector-translated-segment-sentence-option"
-          ),
-          type: "text",
-          class: "px-0 py-4 mx-4",
-        },
-      },
-
-      {
-        value: "paragraph",
-        props: {
-          label: bananaI18n.i18n(
-            "cx-sx-sentence-selector-translated-segment-paragraph-option"
-          ),
-          type: "text",
-          class: "px-0 py-4 mx-4",
-          disabled: isSectionTitleSelected.value,
-        },
-      },
-    ]);
-
-    const updateSelection = (selection) => emit("update:selection", selection);
-
-    return { scopeOptions, updateSelection };
-  },
-};
-</script>
 
 <style lang="less">
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
