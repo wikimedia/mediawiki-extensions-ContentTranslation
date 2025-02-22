03f28a40dd1aa8cfe772485ce748919fd11f9e2d
Andrea Denisse Gómez-Martínez <adenisse@wikimedia.org>
2025-02-21 19:23:57 -0600

    mediawiki.cx: Migrate campaign counter to Prometheus
    This patch migrates the `MediaWiki.cx.campaign.accept`  metric to the new
    Prometheus format while keeping the legacy Graphite metric for backwards
    compatibility.
    
    Bug: T383650
    Bug: T359237
    Change-Id: I9745f8f0e74ae18c6c1919224ecf32d4f9957516

--- modules/eventlogging/ext.cx.eventlogging.campaigns.js
+++ modules/eventlogging/ext.cx.eventlogging.campaigns.js
@@ -11,6 +11,7 @@
 	$( () => {
 		mw.hook( 'mw.cx.cta.accept' ).add( ( campaign ) => {
 			mw.track( 'counter.MediaWiki.cx.campaign.' + campaign + '.accept', 1 );
+			mw.track( 'stats.mediawiki_cx_cta_responses_total', 1, { response: 'accept', campaign } );
 		} );
 	} );
 }() );
