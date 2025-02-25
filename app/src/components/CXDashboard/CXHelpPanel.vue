93b07db523a135dc2bc30a853666f13a55b2bcd1
Sbisson <sbisson@wikimedia.org>
2025-02-25 15:41:48 -0500

    Unified dashboard: add link to old dashboard in help panel
    Also replace mw-icon with cdx-icon
    
    Bug: T386118
    Change-Id: I1f24340c0b69f61763410768baefca7fc495266a

--- app/src/components/CXDashboard/CXHelpPanel.vue
+++ app/src/components/CXDashboard/CXHelpPanel.vue
@@ -1,31 +1,46 @@
 <script setup>
 import {
-  mwIconInfoFilled,
-  mwIconChart,
-  mwIconFeedback,
-} from "@/lib/mediawiki.ui/components/icons";
-import { MwIcon } from "@/lib/mediawiki.ui";
+  cdxIconInfoFilled,
+  cdxIconChart,
+  cdxIconFeedback,
+  cdxIconSpecialPages,
+} from "@wikimedia/codex-icons";
+import { CdxIcon } from "@wikimedia/codex";
 import { useI18n } from "vue-banana-i18n";
+import { getUrl } from "@/utils/mediawikiHelper";
+import { isDesktopSite } from "@/utils/mediawikiHelper";
 
 const bananaI18n = useI18n();
 
 const listItems = [
   {
-    icon: mwIconInfoFilled,
+    icon: cdxIconInfoFilled,
     label: bananaI18n.i18n("cx-sx-dashboard-help-panel-more-info-label"),
     href: "https://www.mediawiki.org/wiki/Special:MyLanguage/Content_translation",
+    target: "_blank",
   },
   {
-    icon: mwIconChart,
+    icon: cdxIconChart,
     label: bananaI18n.i18n("cx-sx-dashboard-help-panel-stats-label"),
     href: mw.util.getUrl("Special:ContentTranslationStats"),
+    target: "_blank",
   },
   {
-    icon: mwIconFeedback,
+    icon: cdxIconFeedback,
     label: bananaI18n.i18n("cx-sx-dashboard-help-panel-feedback-label"),
     href: "https://www.mediawiki.org/wiki/Talk:Content_translation",
+    target: "_blank",
   },
 ];
+
+if (isDesktopSite) {
+  listItems.push({
+    icon: cdxIconSpecialPages,
+    label: bananaI18n.i18n("cx-sx-dashboard-banner-access-previous"),
+    href: getUrl("Special:ContentTranslation", { "cx-dashboard": "desktop" }),
+    target: "_self",
+  });
+}
 </script>
 
 <template>
@@ -33,8 +48,8 @@ const listItems = [
     <h5 v-i18n:cx-sx-dashboard-help-panel-title />
     <ul class="cx-help-panel__item-list mt-6 ps-2">
       <li v-for="(item, index) in listItems" :key="index" class="mt-4">
-        <a :href="item.href" target="_blank">
-          <mw-icon class="me-2" :icon="item.icon" />
+        <a :href="item.href" :target="item.target">
+          <cdx-icon class="me-2" :icon="item.icon" />
           <span v-text="item.label" />
         </a>
       </li>
@@ -52,6 +67,11 @@ const listItems = [
       list-style: none;
       font-weight: @font-weight-bold;
       font-size: 0.875rem;
+      a {
+        .cdx-icon {
+          color: inherit;
+        }
+      }
     }
   }
 }
