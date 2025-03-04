wikimedia/mediawiki-extensions-ContentTranslation

commit 0d30ff16e219edf862c81fda1d67cd187165f2f4

mw.cx: Move var declarations inline
Change-Id: I024c006f1d02b16c0b53f89cee9f738d444d6fc5

modules/stats/ext.cx.stats.js
@@ -22,10 +22,9 @@
 	}
 
 	CXStats.prototype.init = function () {
-		var self = this,
-			$spinner;
+		var self = this;
 
-		$spinner = mw.cx.widgets.spinner();
+		var $spinner = mw.cx.widgets.spinner();
 		this.$highlights = $( '<div>' ).addClass( 'cx-stats-highlights' );
 		this.$container.append( $spinner, this.$highlights );
 
@@ -84,27 +83,21 @@
 	 * Render the boxes at the top with the most interesting recent data.
 	 */
 	CXStats.prototype.renderHighlights = function () {
-		var getTrend, info, infoLanguage, localLanguage,
-			$total, $weeklyStats,
-			weekLangTrendText, weekTrendText, weekTrendClass,
-			$parenthesizedTrend, $trendInLanguage,
-			fmt = mw.language.convertNumber; // Shortcut
+		var fmt = mw.language.convertNumber; // Shortcut
 
-		getTrend = function ( data ) {
-			var thisWeek, total, trend,
-				oneWeekAgoDelta, twoWeeksAgoDelta;
-
+		var getTrend = function ( data ) {
 			if ( data.length < 3 ) {
 				return;
 			}
 
-			thisWeek = data.length - 1;
+			var thisWeek = data.length - 1;
 
-			total = data[ thisWeek ].count;
+			var total = data[ thisWeek ].count;
 
-			oneWeekAgoDelta = data[ thisWeek - 1 ].delta;
-			twoWeeksAgoDelta = data[ thisWeek - 2 ].delta;
+			var oneWeekAgoDelta = data[ thisWeek - 1 ].delta;
+			var twoWeeksAgoDelta = data[ thisWeek - 2 ].delta;
 
+			var trend;
 			if ( twoWeeksAgoDelta ) {
 				trend = Math.round( ( oneWeekAgoDelta - twoWeeksAgoDelta ) / twoWeeksAgoDelta * 100 );
 			} else {
@@ -118,15 +111,15 @@
 			};
 		};
 
-		localLanguage = $.uls.data.getAutonym( mw.config.get( 'wgContentLanguage' ) );
-		info = getTrend( this.totalTranslationTrend );
-		infoLanguage = getTrend( this.languageTranslationTrend );
+		var localLanguage = $.uls.data.getAutonym( mw.config.get( 'wgContentLanguage' ) );
+		var info = getTrend( this.totalTranslationTrend );
+		var infoLanguage = getTrend( this.languageTranslationTrend );
 
 		if ( !info || !infoLanguage ) {
 			return;
 		}
 
-		$total = $( '<div>' )
+		var $total = $( '<div>' )
 			.addClass( 'cx-stats-box' )
 			.append(
 				$( '<div>' )
@@ -144,13 +137,14 @@
 					) )
 			);
 
-		weekLangTrendText = mw.msg( 'percent', fmt( infoLanguage.trend ) );
+		var weekLangTrendText = mw.msg( 'percent', fmt( infoLanguage.trend ) );
 		if ( infoLanguage.trend >= 0 ) {
 			// Add the plus sign to make clear that it's an increase
 			weekLangTrendText = '+' + weekLangTrendText;
 		}
 
-		weekTrendText = mw.msg( 'percent', fmt( info.trend ) );
+		var weekTrendText = mw.msg( 'percent', fmt( info.trend ) );
+		var weekTrendClass;
 		if ( info.trend >= 0 ) {
 			// Add the plus sign to make clear that it's an increase
 			weekTrendText = '+' + weekTrendText;
@@ -159,11 +153,11 @@
 			weekTrendClass = 'cx-stats-trend-decrease';
 		}
 
-		$parenthesizedTrend = $( '<span>' )
+		var $parenthesizedTrend = $( '<span>' )
 			// This is needed to show the plus or minus sign on the correct side
 			.prop( 'dir', 'ltr' )
 			.text( weekLangTrendText );
-		$trendInLanguage = $( '<div>' )
+		var $trendInLanguage = $( '<div>' )
 			.addClass( 'cx-stats-box__localtotal' )
 			.text( mw.msg(
 				'cx-stats-local-published',
@@ -176,7 +170,7 @@
 			$parenthesizedTrend.get( 0 ).outerHTML
 		) );
 
-		$weeklyStats = $( '<div>' )
+		var $weeklyStats = $( '<div>' )
 			.addClass( 'cx-stats-box' )
 			.append(
 				$( '<div>' )
@@ -344,13 +338,12 @@
 	 * @param {boolean} expandable
 	 */
 	CXStats.prototype.createTabs = function ( tabGroupId, items, expandable ) {
-		var $tabContainer, i, $tabs, $expand, tabToShow = 0;
-
-		$tabContainer = $( '<div>' ).addClass( 'cx-stats-tabs-container' );
-		$tabs = $( '<ul>' ).addClass( 'cx-stats-tabs' );
+		var $tabContainer = $( '<div>' ).addClass( 'cx-stats-tabs-container' );
+		var $tabs = $( '<ul>' ).addClass( 'cx-stats-tabs' );
 		$tabContainer.append( $tabs );
 		this.$container.append( $tabContainer );
-		for ( i = 0; i < items.length; i++ ) {
+		var tabToShow;
+		for ( var i = 0; i < items.length; i++ ) {
 			items[ i ].$tab = $( '<li>' )
 				.addClass( 'cx-stats-tabs-tabtitle' )
 				.attr( 'about', tabGroupId + 'tab-' + i )
@@ -399,7 +392,7 @@
 			}
 		} );
 		if ( expandable ) {
-			$expand = $( '<a>' )
+			var $expand = $( '<a>' )
 				.addClass( 'cx-stats-tabs-toggle-all' )
 				.text( mw.msg( 'cx-stats-tabs-expand' ) )
 				.on( 'click', function () {
@@ -435,17 +428,14 @@
 	}
 
 	CXStats.prototype.drawTranslationsChart = function ( direction, status, property ) {
-		var $chart, $bar, translations, $translations, model, i, j, $rows = [],
-			$callout,
-			$row, width, max = 0,
-			$tail, tailWidth = 0,
-			tail, langCode,
-			$langCode, $autonym, $total, $rowLabelContainer,
+		var max = 0,
+			tailWidth = 0,
+			rows = [],
 			fmt = mw.language.convertNumber;
 
-		$chart = $( '<div>' ).addClass( 'cx-stats-chart' );
+		var $chart = $( '<div>' ).addClass( 'cx-stats-chart' );
 
-		model = direction === 'to' ?
+		var model = direction === 'to' ?
 			this.targetSourceModel[ status ].sort(
 				property === 'count' ? sortByCount : sortByTranslators
 			) :
@@ -453,15 +443,15 @@
 				property === 'count' ? sortByCount : sortByTranslators
 			);
 
-		for ( i = 0; i < model.length; i++ ) {
-			$row = $( '<div>' ).addClass( 'cx-stats-chart__row' );
+		for ( var i = 0; i < model.length; i++ ) {
+			var $row = $( '<div>' ).addClass( 'cx-stats-chart__row' );
 
-			$translations = $( '<span>' ).addClass( 'cx-stats-chart__bars' );
-			translations = model[ i ].translations.sort(
+			var $translations = $( '<span>' ).addClass( 'cx-stats-chart__bars' );
+			var translations = model[ i ].translations.sort(
 				property === 'count' ? sortByCount : sortByTranslators
 			);
 
-			tail = false;
+			var tail = false;
 			tailWidth = 0;
 			max = max || model[ 0 ][ property ];
 
@@ -470,19 +460,19 @@
 				max >= 1000
 			) {
 				max = Math.ceil( model[ i ][ property ] / 100 ) * 100;
-				$rows.push( $( '<div>' )
+				rows.push( $( '<div>' )
 					.addClass( 'cx-stats-chart__row cx-separator' )
 					.text( mw.msg( 'cx-stats-grouping-title', fmt( max ) ) ) );
 			}
 
-			$callout = $( '<table>' ).addClass( 'cx-stats-chart__callout' );
-			for ( j = 0; j < translations.length; j++ ) {
-				width = ( translations[ j ][ property ] / max ) * 100;
-				langCode = translations[ j ][ ( direction === 'to' ? 'sourceLanguage' : 'targetLanguage' ) ];
+			var $callout = $( '<table>' ).addClass( 'cx-stats-chart__callout' );
+			for ( var j = 0; j < translations.length; j++ ) {
+				var width = ( translations[ j ][ property ] / max ) * 100;
+				var langCode = translations[ j ][ ( direction === 'to' ? 'sourceLanguage' : 'targetLanguage' ) ];
 
 				if ( width > 2 || j === 0 ) {
 					// languages with more than 2% are represented in chart.
-					$bar = $( '<span>' )
+					var $bar = $( '<span>' )
 						.addClass( 'cx-stats-chart__bar' )
 						.prop( {
 							lang: 'en',
@@ -512,7 +502,7 @@
 			}
 
 			if ( tail ) {
-				$tail = $( '<span>' )
+				var $tail = $( '<span>' )
 					.addClass( 'cx-stats-chart__bar cx-stats-bar-tail' )
 					.text( '…' )
 					.css( 'width', tailWidth + '%' );
@@ -526,7 +516,7 @@
 				content: $callout
 			} );
 
-			$langCode = $( '<span>' )
+			var $langCode = $( '<span>' )
 				.addClass( 'cx-stats-chart__langcode' )
 				// Always Latin (like English).
 				// Make sure it's aligned correctly on all screen sizes.
@@ -536,7 +526,7 @@
 				} )
 				.text( model[ i ].language );
 
-			$autonym = $( '<span>' )
+			var $autonym = $( '<span>' )
 				.addClass( 'cx-stats-chart__autonym' )
 				.prop( {
 					lang: model[ i ].language,
@@ -544,7 +534,7 @@
 				} )
 				.text( $.uls.data.getAutonym( model[ i ].language ) );
 
-			$total = $( '<span>' )
+			var $total = $( '<span>' )
 				.addClass( 'cx-stats-chart__total' )
 				.text( fmt( model[ i ][ property ] ) );
 
@@ -563,16 +553,16 @@
 					.text( fmt( model[ i ][ property ] ) );
 			}
 
-			$rowLabelContainer = $( '<span>' )
+			var $rowLabelContainer = $( '<span>' )
 				.addClass( 'cx-stats-chart__row-label-container' )
 				.append( $langCode, $autonym, $total );
 
 			$row.append( $rowLabelContainer, $translations );
 
-			$rows.push( $row );
+			rows.push( $row );
 		}
 
-		$chart.append( $rows );
+		$chart.append( rows );
 
 		return $chart;
 	};
@@ -609,11 +599,9 @@
 	};
 
 	CXStats.prototype.drawCumulativeGraph = function ( type ) {
-		var data, ctx;
+		var ctx = this.$cumulativeGraph[ 0 ].getContext( '2d' );
 
-		ctx = this.$cumulativeGraph[ 0 ].getContext( '2d' );
-
-		data = {
+		var data = {
 			labels: this.totalTranslationTrend.map( function ( data ) {
 				return data.date;
 			} ),
@@ -654,11 +642,9 @@
 	};
 
 	CXStats.prototype.drawLanguageCumulativeGraph = function ( type ) {
-		var data, ctx;
+		var ctx = this.$languageCumulativeGraph[ 0 ].getContext( '2d' );
 
-		ctx = this.$languageCumulativeGraph[ 0 ].getContext( '2d' );
-
-		data = {
+		var data = {
 			labels: this.languageTranslationTrend.map( function ( data ) {
 				return data.date;
 			} ),
@@ -711,10 +697,10 @@
 	};
 
 	CXStats.prototype.drawTranslationTrend = function () {
-		var data, ctx, type = 'delta';
+		var type = 'delta';
 
-		ctx = this.$translationTrendBarChart[ 0 ].getContext( '2d' );
-		data = {
+		var ctx = this.$translationTrendBarChart[ 0 ].getContext( '2d' );
+		var data = {
 			labels: this.totalTranslationTrend.map( function ( data ) {
 				return data.date;
 			} ),
@@ -749,11 +735,10 @@
 	};
 
 	CXStats.prototype.drawLangTranslationTrend = function () {
-		var ctx, data,
-			type = 'delta';
+		var type = 'delta';
 
-		ctx = this.$langTranslationTrendBarChart[ 0 ].getContext( '2d' );
-		data = {
+		var ctx = this.$langTranslationTrendBarChart[ 0 ].getContext( '2d' );
+		var data = {
 			labels: this.languageTranslationTrend.map( function ( data ) {
 				return data.date;
 			} ),
@@ -797,29 +782,28 @@
 	};
 
 	CXStats.prototype.transformJsonToModel = function ( records ) {
-		var i, record, language, status, count, translators,
-			sourceLanguage, targetLanguage,
-			tempModel,
-			hasOwn = Object.prototype.hasOwnProperty;
+		var hasOwn = Object.prototype.hasOwnProperty;
 
 		this.sourceTargetModel.draft = {};
 		this.targetSourceModel.draft = {};
 		this.sourceTargetModel.published = {};
 		this.targetSourceModel.published = {};
 
+		var i, status;
 		for ( i = 0; i < records.pages.length; i++ ) {
-			record = records.pages[ i ];
+			var record = records.pages[ i ];
 			status = record.status;
-			sourceLanguage = record.sourceLanguage;
-			targetLanguage = record.targetLanguage;
+			var sourceLanguage = record.sourceLanguage;
+			var targetLanguage = record.targetLanguage;
 			this.sourceTargetModel[ status ][ sourceLanguage ] = this.sourceTargetModel[ status ][ sourceLanguage ] || [];
 			this.targetSourceModel[ status ][ targetLanguage ] = this.targetSourceModel[ status ][ targetLanguage ] || [];
 			this.sourceTargetModel[ status ][ sourceLanguage ].push( record );
 			this.targetSourceModel[ status ][ targetLanguage ].push( record );
 		}
 
+		var language, count, translators;
 		for ( status in this.sourceTargetModel ) {
-			tempModel = this.sourceTargetModel[ status ];
+			var tempModel = this.sourceTargetModel[ status ];
 			this.sourceTargetModel[ status ] = [];
 			for ( language in tempModel ) {
 				if ( hasOwn.call( tempModel, language ) ) {
@@ -856,9 +840,7 @@
 	};
 
 	$( function () {
-		var cxLink, cxstats, $container;
-
-		$container = $( '<div>' ).addClass( 'cx-stats-container' );
+		var $container = $( '<div>' ).addClass( 'cx-stats-container' );
 
 		// Set the global siteMapper for code which we cannot inject it
 		mw.cx.siteMapper = new mw.cx.SiteMapper();
@@ -868,7 +850,7 @@
 			)
 		);
 
-		cxstats = new CXStats( $container, {
+		var cxstats = new CXStats( $container, {
 			siteMapper: new mw.cx.SiteMapper()
 		} );
 		cxstats.init();
@@ -877,7 +859,7 @@
 			mw.config.get( 'wgContentTranslationCampaigns' ).cxstats &&
 			mw.user.options.get( 'cx' ) !== '1'
 		) {
-			cxLink = mw.util.getUrl( 'Special:ContentTranslation', {
+			var cxLink = mw.util.getUrl( 'Special:ContentTranslation', {
 				campaign: 'cxstats',
 				to: mw.config.get( 'wgContentLanguage' )
 			} );

modules/cache/mw.cx.ApiResponseCache.js
@@ -119,8 +119,7 @@ mw.cx.ApiResponseCache.prototype.getCached = function ( name ) {
  * @fires add
  */
 mw.cx.ApiResponseCache.prototype.set = function ( entries ) {
-	var name;
-	for ( name in entries ) {
+	for ( var name in entries ) {
 		if ( !Object.prototype.hasOwnProperty.call( this.deferreds, name ) ) {
 			this.deferreds[ name ] = $.Deferred();
 		}
@@ -149,31 +148,27 @@ mw.cx.ApiResponseCache.prototype.getRequestPromise = null;
  * @fires add
  */
 mw.cx.ApiResponseCache.prototype.processQueue = function () {
-	var subqueue, queue,
-		cache = this;
+	var cache = this;
 
 	function rejectSubqueue( rejectQueue ) {
-		var i, len;
-		for ( i = 0, len = rejectQueue.length; i < len; i++ ) {
+		for ( var i = 0, len = rejectQueue.length; i < len; i++ ) {
 			cache.deferreds[ rejectQueue[ i ] ].reject();
 		}
 	}
 
 	function processResult( data ) {
-		var pageid, page, i, processedPage,
-			pages = ( data.query && data.query.pages ) || data.pages,
-			redirects,
+		var pages = ( data.query && data.query.pages ) || data.pages,
 			processed = {};
 
-		redirects = data.query.redirects || {};
+		var redirects = data.query.redirects || {};
 		if ( pages ) {
-			for ( pageid in pages ) {
-				page = pages[ pageid ];
-				processedPage = cache.constructor.static.processPage( page, redirects );
+			for ( var pageid in pages ) {
+				var page = pages[ pageid ];
+				var processedPage = cache.constructor.static.processPage( page, redirects );
 				if ( processedPage !== undefined ) {
 					processed[ page.title ] = processedPage;
 				}
-				for ( i in redirects ) {
+				for ( var i in redirects ) {
 					// Locate the title in redirects, if any.
 					if ( redirects[ i ].to === page.title ) {
 						processed[ redirects[ i ].from ] = processedPage;
@@ -185,10 +180,10 @@ mw.cx.ApiResponseCache.prototype.processQueue = function () {
 		}
 	}
 
-	queue = this.queue;
+	var queue = this.queue;
 	this.queue = [];
 	while ( queue.length ) {
-		subqueue = queue.splice( 0, 50 ).map( this.constructor.static.normalizeTitle );
+		var subqueue = queue.splice( 0, 50 ).map( this.constructor.static.normalizeTitle );
 		this.getRequestPromise( subqueue )
 			.then( processResult )
 

modules/cache/mw.cx.NamespaceCache.js
@@ -34,10 +34,8 @@ mw.cx.NamespaceCache.prototype.get = function ( canonicalNamespace ) {
 };
 
 mw.cx.NamespaceCache.prototype.processResponse = function ( response ) {
-	var namespaceId, namespaceObj;
-
-	for ( namespaceId in response.query.namespaces ) {
-		namespaceObj = response.query.namespaces[ namespaceId ];
+	for ( var namespaceId in response.query.namespaces ) {
+		var namespaceObj = response.query.namespaces[ namespaceId ];
 		this.cacheValues[ namespaceObj.canonical ] = namespaceObj[ '*' ];
 	}
 };

modules/dashboard/ext.cx.recommendtool.client.js
@@ -31,8 +31,7 @@
 	 * @return {jQuery.Promise}
 	 */
 	RecommendTool.prototype.getSeedPages = function () {
-		var params,
-			self = this,
+		var self = this,
 			api = new mw.Api();
 
 		if ( this.seeds ) {
@@ -46,7 +45,7 @@
 		// Effectively, the following request is duplicate. But it does not look like a good idea
 		// to have dependency on translation list for this module. There are some complications as
 		// well with sharing data - we are not sure which module gets initated first.
-		params = {
+		var params = {
 			assert: 'user',
 			list: 'contenttranslation',
 			from: this.sourceLanguage,
@@ -70,10 +69,8 @@
 		var self = this;
 
 		return this.getSeedPages().then( function ( seedPages ) {
-			var algorithms, algorithm;
-
-			algorithms = [ 'morelike', 'related_articles' ];
-			algorithm = algorithms[ Math.floor( Math.random() * algorithms.length ) ];
+			var algorithms = [ 'morelike', 'related_articles' ];
+			var algorithm = algorithms[ Math.floor( Math.random() * algorithms.length ) ];
 			return $.get( mw.config.get( 'wgRecommendToolAPIURL' ), {
 				source: self.sourceLanguage,
 				target: self.targetLanguage,
@@ -94,10 +91,10 @@
 	 * @return {Object}
 	 */
 	RecommendTool.prototype.adapt = function ( articles, algorithm ) {
-		var i, title, suggestions = [];
+		var suggestions = [];
 
-		for ( i = 0; i < articles.length; i++ ) {
-			title = mw.Title.newFromText( articles[ i ].title );
+		for ( var i = 0; i < articles.length; i++ ) {
+			var title = mw.Title.newFromText( articles[ i ].title );
 			if ( !title ) {
 				continue;
 			}

modules/dashboard/mw.cx.DashboardList.js
@@ -119,8 +119,7 @@ mw.cx.DashboardList.prototype.getPageDetails = function ( language, titles ) {
  * @param {Object} list
  */
 mw.cx.DashboardList.prototype.showTitleDetails = function ( list ) {
-	var apply, language, processPageDetails,
-		queries = {},
+	var queries = {},
 		map = {};
 
 	list.forEach( function ( item ) {
@@ -136,7 +135,7 @@ mw.cx.DashboardList.prototype.showTitleDetails = function ( list ) {
 		map[ title ].push( item );
 	}, this );
 
-	apply = function ( page ) {
+	var apply = function ( page ) {
 		if ( !map[ page.title ] ) {
 			return;
 		}
@@ -155,9 +154,8 @@ mw.cx.DashboardList.prototype.showTitleDetails = function ( list ) {
 		} );
 	};
 
-	processPageDetails = function ( response ) {
-		var pageId, page,
-			redirects = response.query.redirects || [],
+	var processPageDetails = function ( response ) {
+		var redirects = response.query.redirects || [],
 			redirectsTo = {},
 			pages = response.query.pages;
 
@@ -165,14 +163,14 @@ mw.cx.DashboardList.prototype.showTitleDetails = function ( list ) {
 			redirectsTo[ redirect.to ] = redirect.from;
 		} );
 
-		for ( pageId in pages ) {
-			page = pages[ pageId ];
+		for ( var pageId in pages ) {
+			var page = pages[ pageId ];
 			page.title = redirectsTo[ page.title ] || page.title;
 			apply( page );
 		}
 	};
 
-	for ( language in queries ) {
+	for ( var language in queries ) {
 		this.getPageDetails( language, queries[ language ] ).done( processPageDetails );
 	}
 };

modules/dashboard/mw.cx.TranslationList.js
@@ -52,7 +52,6 @@ OO.mixinClass( mw.cx.CXTranslationList, OO.EventEmitter );
  */
 mw.cx.CXTranslationList.prototype.getTranslations = function () {
 	var self = this,
-		params,
 		api = new mw.Api();
 
 	if ( this.promise ) {
@@ -64,7 +63,7 @@ mw.cx.CXTranslationList.prototype.getTranslations = function () {
 		return $.Deferred().resolve( [] );
 	}
 
-	params = $.extend( {
+	var params = $.extend( {
 		assert: 'user',
 		list: 'contenttranslation',
 		type: this.type,
@@ -113,7 +112,7 @@ mw.cx.CXTranslationList.prototype.init = function () {
 };
 
 mw.cx.CXTranslationList.prototype.loadItems = function () {
-	var promise, self = this;
+	var self = this;
 
 	if ( this.promise ) {
 		return this.promise;
@@ -128,7 +127,7 @@ mw.cx.CXTranslationList.prototype.loadItems = function () {
 	this.$loadingIndicatorSpinner.show();
 	this.pendingRequests++;
 
-	promise = this.getTranslations();
+	var promise = this.getTranslations();
 	promise.done( function ( translations ) {
 		self.translations = self.translations.concat( translations );
 
@@ -169,7 +168,6 @@ mw.cx.CXTranslationList.prototype.loadItems = function () {
  * Fill source and target language filter with languages for which there are translationlist items
  */
 mw.cx.CXTranslationList.prototype.fillULS = function () {
-	var languageDecorator;
 	// Check if there is only one language combination, e.g. English to Spanish
 	// sourceLanguages - [ 'en' ]
 	// targetLanguages - [ 'es' ]
@@ -182,7 +180,7 @@ mw.cx.CXTranslationList.prototype.fillULS = function () {
 	this.sourceLanguages.unshift( 'x-all' );
 	this.targetLanguages.unshift( 'x-all' );
 
-	languageDecorator = function ( $language, languageCode ) {
+	var languageDecorator = function ( $language, languageCode ) {
 		if ( languageCode === 'x-all' ) {
 			$language.parent().addClass( 'cx-translationlist-uls-all-languages' );
 		}
@@ -247,44 +245,37 @@ mw.cx.CXTranslationList.prototype.continueTranslation = function ( translation )
  * @param {Object[]} translations
  */
 mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations ) {
-	var i, translation, progress, $translation,
-		$lastUpdated, $image, $progressbar,
-		sourceDir, targetDir, $targetTitle,
-		$translationLink,
-		$sourceLanguage, $targetLanguage, $languageContainer,
-		deleteTranslation, $actions,
-		continueTranslation,
-		$titleLanguageBlock,
-		$translations = [];
+	var $translations = [];
 
-	for ( i = 0; i < translations.length; i++ ) {
-		translation = translations[ i ];
+	for ( var i = 0; i < translations.length; i++ ) {
+		var translation = translations[ i ];
 
+		var progress;
 		try {
 			progress = JSON.parse( translation.progress );
 		} catch ( e ) {
 			progress = {};
 		}
 
-		$translation = $( '<div>' )
+		var $translation = $( '<div>' )
 			.addClass( 'cx-tlitem' )
 			.data( 'translation', translation );
-		$lastUpdated = $( '<div>' )
+		var $lastUpdated = $( '<div>' )
 			.addClass( 'cx-last-updated' )
 			.text( moment.utc( translation.lastUpdateTimestamp, 'YYYYMMDDHHmmss' ).local().fromNow() );
-		$image = $( '<div>' )
+		var $image = $( '<div>' )
 			.addClass( 'cx-tlitem__image oo-ui-icon-article' );
-		$progressbar = $( '<div>' )
+		var $progressbar = $( '<div>' )
 			.addClass( 'progressbar' )
 			.cxProgressBar( {
 				weights: progress,
 				version: translation.cxVersion
 			} );
 
-		sourceDir = $.uls.data.getDir( translation.sourceLanguage );
-		targetDir = $.uls.data.getDir( translation.targetLanguage );
+		var sourceDir = $.uls.data.getDir( translation.sourceLanguage );
+		var targetDir = $.uls.data.getDir( translation.targetLanguage );
 
-		$translationLink = $( '<a>' )
+		var $translationLink = $( '<a>' )
 			.addClass( 'cx-translation-link' )
 			// It must be a separate element to ensure
 			// separation from the target title
@@ -300,7 +291,7 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 		// If the translated title is different from the source title,
 		// show it near the source title
 		if ( translation.sourceTitle !== translation.targetTitle ) {
-			$targetTitle = $( '<span>' )
+			var $targetTitle = $( '<span>' )
 				.prop( {
 					lang: translation.targetLanguage,
 					dir: targetDir
@@ -313,7 +304,7 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 			);
 		}
 
-		$sourceLanguage = $( '<div>' )
+		var $sourceLanguage = $( '<div>' )
 			.prop( {
 				lang: translation.sourceLanguage,
 				dir: sourceDir
@@ -321,7 +312,7 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 			.addClass( 'cx-tlitem__languages__language cx-tlitem__languages__language--source' )
 			.text( $.uls.data.getAutonym( translation.sourceLanguage ) );
 
-		$targetLanguage = $( '<div>' )
+		var $targetLanguage = $( '<div>' )
 			.prop( {
 				lang: translation.targetLanguage,
 				dir: targetDir
@@ -329,15 +320,15 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 			.addClass( 'cx-tlitem__languages__language cx-tlitem__languages__language--target' )
 			.text( $.uls.data.getAutonym( translation.targetLanguage ) );
 
-		$languageContainer = $( '<div>' )
+		var $languageContainer = $( '<div>' )
 			.addClass( 'cx-tlitem__languages' )
 			.append( $sourceLanguage, $targetLanguage );
 
-		$actions = $( '<div>' )
+		var $actions = $( '<div>' )
 			.addClass( 'cx-tlitem__actions' );
 		// If the translation is draft, allow deleting it
 		if ( translation.status === 'draft' ) {
-			deleteTranslation = new OO.ui.ButtonWidget( {
+			var deleteTranslation = new OO.ui.ButtonWidget( {
 				framed: false,
 				classes: [ 'cx-discard-translation' ],
 				icon: 'trash',
@@ -345,7 +336,7 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 			} );
 			$actions.append( deleteTranslation.$element );
 		} else if ( translation.status === 'published' ) {
-			continueTranslation = new OO.ui.ButtonWidget( {
+			var continueTranslation = new OO.ui.ButtonWidget( {
 				framed: false,
 				classes: [ 'cx-continue-translation' ],
 				icon: 'edit',
@@ -354,7 +345,7 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 			$actions.append( continueTranslation.$element );
 		}
 
-		$titleLanguageBlock = $( '<div>' )
+		var $titleLanguageBlock = $( '<div>' )
 			.addClass( 'cx-tlitem__details' )
 			.append( $translationLink, $progressbar, $lastUpdated, $languageContainer );
 
@@ -376,17 +367,15 @@ mw.cx.CXTranslationList.prototype.renderTranslations = function ( translations )
 };
 
 mw.cx.CXTranslationList.prototype.buildEmptyTranslationList = function () {
-	var $img, $title, $desc;
-
 	if ( this.$emptyTranslationsList ) {
 		return this.$emptyTranslationsList;
 	}
-	$img = $( '<div>' )
+	var $img = $( '<div>' )
 		.addClass( 'cx-translationlist-empty__img' );
-	$title = $( '<div>' )
+	var $title = $( '<div>' )
 		.addClass( 'cx-translationlist-empty__title' )
 		.text( mw.msg( 'cx-translationlist-empty-title' ) );
-	$desc = $( '<div>' )
+	var $desc = $( '<div>' )
 		.addClass( 'cx-translationlist-empty__desc' )
 		.text( mw.msg( 'cx-translationlist-empty-desc' ) );
 	return $( '<div>' )
@@ -403,11 +392,9 @@ mw.cx.CXTranslationList.prototype.listen = function () {
 	mw.cx.CXTranslationList.super.prototype.listen.apply( this, arguments );
 
 	this.$listContainer.on( 'click', '.cx-discard-translation', function ( e ) {
-		var translation;
-
 		e.stopPropagation();
 		$( this ).find( 'a' ).trigger( 'blur' );
-		translation = $( this ).closest( '.cx-tlitem' ).data( 'translation' );
+		var translation = $( this ).closest( '.cx-tlitem' ).data( 'translation' );
 
 		OO.ui.getWindowManager().openWindow( 'message', $.extend( {
 			message: mw.msg( 'cx-draft-discard-confirmation-message' ),
@@ -439,11 +426,9 @@ mw.cx.CXTranslationList.prototype.listen = function () {
 	} );
 
 	this.$listContainer.on( 'click', '.cx-continue-translation', function ( e ) {
-		var translation;
-
 		e.stopPropagation();
 		$( this ).find( 'a' ).trigger( 'blur' );
-		translation = $( this ).closest( '.cx-tlitem' ).data( 'translation' );
+		var translation = $( this ).closest( '.cx-tlitem' ).data( 'translation' );
 		self.continueTranslation( translation );
 		return false;
 	} );
@@ -507,14 +492,13 @@ mw.cx.CXTranslationList.prototype.discardTranslation = function ( translation ) 
 };
 
 mw.cx.CXTranslationList.prototype.applyFilters = function () {
-	var i, translation, visible,
-		sourceLanguage = this.languageFilter.getSourceLanguage(),
+	var sourceLanguage = this.languageFilter.getSourceLanguage(),
 		targetLanguage = this.languageFilter.getTargetLanguage();
 
-	for ( i = 0; i < this.translations.length; i++ ) {
-		translation = this.translations[ i ];
+	for ( var i = 0; i < this.translations.length; i++ ) {
+		var translation = this.translations[ i ];
 
-		visible = ( !sourceLanguage || translation.sourceLanguage === sourceLanguage ) &&
+		var visible = ( !sourceLanguage || translation.sourceLanguage === sourceLanguage ) &&
 			( !targetLanguage || translation.targetLanguage === targetLanguage );
 
 		if ( visible ) {

modules/entrypoints/ext.cx.contributions.js
@@ -26,12 +26,10 @@
 	};
 
 	CXContributions.prototype.render = function () {
-		var $sectionHeader, contributionButtons, contributionButtonsGroup;
-
-		$sectionHeader = $( '<h1>' )
+		var $sectionHeader = $( '<h1>' )
 			.text( mw.msg( 'cx-contributions-new-contributions' ) );
 
-		contributionButtons = this.getActivities().map( function ( item ) {
+		var contributionButtons = this.getActivities().map( function ( item ) {
 			// eslint-disable-next-line mediawiki/class-doc
 			return new OO.ui.ButtonWidget( {
 				classes: [ 'cx-contributions-item' ].concat( item.classes ),
@@ -41,7 +39,7 @@
 				href: item.url
 			} );
 		} );
-		contributionButtonsGroup = new OO.ui.ButtonGroupWidget( {
+		var contributionButtonsGroup = new OO.ui.ButtonGroupWidget( {
 			classes: [ 'cx-contributions' ],
 			items: contributionButtons
 		} );

commit d8227dc0d1642c392e1b86a8436c54aa3ad13115

stats: Initialize tabToShow with default value
This unbreaks Special:CXStats

Fixes: 0d30ff16e219edf862c81fda1d67cd187165f2f4
Change-Id: Ib91215a23c57b186d8792034f970e3ed1d7f4cdb

modules/stats/ext.cx.stats.js
@@ -342,7 +342,7 @@
 		const $tabs = $( '<ul>' ).addClass( 'cx-stats-tabs' );
 		$tabContainer.append( $tabs );
 		this.$container.append( $tabContainer );
-		let tabToShow;
+		let tabToShow = 0;
 		for ( let i = 0; i < items.length; i++ ) {
 			items[ i ].$tab = $( '<li>' )
 				.addClass( 'cx-stats-tabs-tabtitle' )