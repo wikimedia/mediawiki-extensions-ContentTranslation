d6580fd3cb2a6e7fa78b3cf6278f1f372446ec17
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-04-29 10:36:13 +0000

    newarticle entrypoint: Use new VE hook system
    Bug: T390074
    Change-Id: Ie062adbef87268fd5c4ca2ecf64dca7d1c2dbf00

--- modules/entrypoints/ext.cx.entrypoints.newarticle.veloader.js
+++ modules/entrypoints/ext.cx.entrypoints.newarticle.veloader.js
@@ -7,9 +7,22 @@
 ( function () {
 	'use strict';
 
-	$( () => {
-		mw.hook( 've.activationComplete' ).add( () => {
+	mw.hook( 've.newTarget' ).add( ( target ) => {
+		// Check the target is an article
+		if ( target.constructor.static.name !== 'article' ) {
+			// Other valid targets exist, e.g. 'discussionTools'
+			return;
+		}
+
+		const isDesktop = ve.init.mw.DesktopArticleTarget && target instanceof ve.init.mw.DesktopArticleTarget;
+		if ( !isDesktop ) {
+			// Show only in desktop
+			return;
+		}
+		target.on( 'surfaceReady', () => {
 			mw.loader.load( 'ext.cx.entrypoints.newarticle' );
 		} );
+
 	} );
+
 }() );
