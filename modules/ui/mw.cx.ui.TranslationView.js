aaa9a0ce0a36b7698bdf08d6e23a4ef3b6e74215
NikG <ngkountas@wikimedia.org>
2025-11-05 14:24:11 +0100

    CX: Remove "ext.cx.translation.conflict" module
    This module is only used inside "mw.cx.ui.TranslationView" and there is
    no need for it to be a separate module. This patch removes the
    "ext.cx.translation.conflict" module and moves its functionality inside
    "mw.cx.ui.TranslationView.showConflictWarning" method.
    
    Bug: T203695
    Change-Id: I90e0fddd4b3c53b1029354c2f4de2ed6a7e60b99

--- modules/ui/mw.cx.ui.TranslationView.js
+++ modules/ui/mw.cx.ui.TranslationView.js
@@ -200,9 +200,39 @@ mw.cx.ui.TranslationView.prototype.setStatusMessage = function ( message ) {
 	this.translationHeader.setStatusMessage( message );
 };
 
+/**
+ * Show the information about translation conflict
+ *
+ * @param {string} translatorName
+ * @param {string} translatorGender
+ */
 mw.cx.ui.TranslationView.prototype.showConflictWarning = function ( translatorName, translatorGender ) {
-	mw.loader.using( 'ext.cx.translation.conflict' ).then( () => {
-		mw.hook( 'mw.cx.translation.conflict' ).fire( translatorName, translatorGender );
+	mw.loader.using( 'mediawiki.jqueryMsg' ).then( () => {
+		let message;
+
+		if ( translatorName ) {
+			const $userPage = $( '<a>' )
+				.text( translatorName )
+				.prop( 'href', mw.util.getUrl( 'User:' + translatorName ) );
+
+			message = mw.message( 'cx-translation-already-in-progress', $userPage, translatorGender );
+		} else {
+			message = mw.message( 'cx-translation-already-in-progress-unknown' );
+		}
+
+		OO.ui.getWindowManager().openWindow( 'message', {
+			message: message.parseDom(),
+			actions: [
+				{ action: 'cancel', label: mw.msg( 'cx-create-new-translation' ), flags: 'primary' }
+			]
+		} ).closed.then( ( data ) => {
+			if ( !data || data.action === 'cancel' ) {
+				// Go to dashboard
+				const uri = new URL( location.href );
+				uri.searchParams.delete( 'page' );
+				location.href = uri.toString();
+			}
+		} );
 	} );
 };
 
