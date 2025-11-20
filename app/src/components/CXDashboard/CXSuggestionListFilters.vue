55b27845814989d83fc89723b05465f2d2272de2
Sbisson <sbisson@wikimedia.org>
2025-11-20 11:44:40 -0500

    Let filters wrap on several lines on filters summary bar
    Remove responsible adjustment that limited the number of
    chips on mobile.
    
    Change-Id: I5dfa4c1cba2cd52bd7ca9022e603b5b699a2d89e

--- app/src/components/CXDashboard/CXSuggestionListFilters.vue
+++ app/src/components/CXDashboard/CXSuggestionListFilters.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { cdxIconEllipsis } from "@wikimedia/codex-icons";
-import { ref, computed, watch, inject } from "vue";
+import { ref, watch } from "vue";
 import { useI18n } from "vue-banana-i18n";
 import SxSuggestionsFilters from "./SXSuggestionsFiltersDialog.vue";
 import useSuggestionsFilters from "@/composables/useSuggestionsFilters";
@@ -40,33 +40,6 @@ const logAndSelectFilter = (filter) => {
 };
 
 const filtersSummary = ref(getFiltersSummary());
-const breakpoints = inject("breakpoints");
-
-const responsiveFiltersSummary = computed(() => {
-  if (breakpoints.value.desktopAndUp) {
-    // filters summary can contain up to 4 filters, if featured collection exists and
-    // the currently selected filter is not the edit or the popular filter.
-    // Up to 3 should be displayed
-    return filtersSummary.value.slice(0, 3);
-  } else {
-    const croppedFilters = filtersSummary.value.slice(0, 2);
-    const currentFilterPresentInCropped = croppedFilters.some((filter) =>
-      isFilterSelected(filter)
-    );
-
-    if (currentFilterPresentInCropped) {
-      return croppedFilters;
-    } else {
-      // the first two filters can miss the currently selected filter, which should always be displayed.
-      // Make sure that the current filter is always included
-      const currentFilter = filtersSummary.value.find((filter) =>
-        isFilterSelected(filter)
-      );
-
-      return [filtersSummary.value[0], currentFilter];
-    }
-  }
-});
 
 // Toggling between summary filters should not reset them even when
 // one is a topic and one is automatic. It should only reset when
@@ -90,7 +63,7 @@ watch([waitingForPageCollectionsFetch, targetLanguage], ([newWaitingValue]) => {
   <mw-spinner v-if="waitingForPageCollectionsFetch" />
   <div v-else class="cx-suggestion-list__filters flex mx-4 pb-2">
     <custom-info-chip
-      v-for="filter in responsiveFiltersSummary"
+      v-for="filter in filtersSummary"
       :key="filter.label"
       class="cx-suggestion-list__filter py-1 me-1"
       :class="{
@@ -114,7 +87,6 @@ watch([waitingForPageCollectionsFetch, targetLanguage], ([newWaitingValue]) => {
 @import (reference) "~@wikimedia/codex-design-tokens/theme-wikimedia-ui.less";
 
 .cx-suggestion-list__filters {
-  flex-wrap: nowrap;
   row-gap: @spacing-25;
   overflow-x: auto;
 
